7318104408f1ce47916cf131068b9e29aa10a7ae
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2024-08-30 08:28:31 -0700

    Add home page redirect

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -9,8 +9,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from apps.owasp import views as owasp_views
+
 urlpatterns = [
+    path("", owasp_views.home_page),
     path("a/", admin.site.urls),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
