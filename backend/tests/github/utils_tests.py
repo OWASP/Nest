a065e53517eb10562249b57703ef06989edb9609
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2024-09-05 11:26:04 -0700

    Run pre-commit

--- backend/tests/github/utils_tests.py
+++ backend/tests/github/utils_tests.py
@@ -17,8 +17,6 @@ from apps.github.utils import check_funding_policy_compliance
         ("custom", "https://my-site.com/donate", False),
         ("custom", "https://my-site.com/owasp/donate", False),
         ("patreon", "username", False),
-        
-
     ],
 )
 def test_check_funding_policy_compliance(platform, target, as_expected):
