7eb17d07d5a9ce62dfe6940fc072da38a2e87180
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2024-10-15 10:46:08 -0700

    Update geocoding.py

--- backend/apps/common/geocoding.py
+++ backend/apps/common/geocoding.py
@@ -1,5 +1,6 @@
 """Geocoding utils."""
 
+import logging
 import time
 from functools import lru_cache
 
@@ -8,20 +9,28 @@ from geopy.geocoders import Nominatim
 
 from apps.common.utils import get_nest_user_agent
 
+logger = logging.getLogger(__name__)
+
 
 @lru_cache(maxsize=1024)
 def get_ip_coordinates(ip_address):
     """Return IP address geo coordinates."""
-    latitude, longitude = None, None
-    if ip_address:
+    if not ip_address:
+        return None
+
+    try:
         response = requests.get(f"https://ipapi.co/{ip_address}/latlong/", timeout=5)
         if response.status_code == requests.codes.ok:
-            latitude, longitude = response.text.split(",")
+            return response.text.split(",")
+    except TypeError:
+        logger.exception(
+            "Could not get coordinates for the IP address",
+            extra={"ip_address": ip_address},
+        )
+        return None
 
-    return latitude, longitude
 
-
-def get_location_coordinates(query):
+def get_location_coordinates(query, delay=2):
     """Get location geo coordinates."""
-    time.sleep(2)
+    time.sleep(delay)
     return Nominatim(timeout=3, user_agent=get_nest_user_agent()).geocode(query)
