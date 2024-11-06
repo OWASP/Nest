2b9c1967ed0f7d9b0061c7d7cbf2a92141afe59e
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2024-11-05 17:00:32 -0800

    Update code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello from new OWASP Nest frontend!</h1>
     </>
   );
 }
