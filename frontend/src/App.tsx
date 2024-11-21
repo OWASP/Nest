c34ac1743944685b5adfb9f65ae5746729ebeb80
Abhay <abhaytopno999@gmail.com>
2024-11-22 00:08:29 +0530

    added react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Footer/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
