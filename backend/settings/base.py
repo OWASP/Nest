aa61bdd76893006906bcb11cefc4842b52670058
Harsh Pandey <pandey73harsh@gmail.com>
2024-11-27 11:23:10 +0530

    update pre-commit

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -60,7 +60,7 @@ class Base(Configuration):
         "django.contrib.messages.middleware.MessageMiddleware",
         "django.middleware.clickjacking.XFrameOptionsMiddleware",
     ]
-    
+
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",
         "http://localhost:8000",
