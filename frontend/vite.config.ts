4b08c84d67cd3aa253dafa253ffa631b3cc27888
Yashgupta9330 <yaahg342@gmail.com>
2024-11-28 00:09:38 +0530

    removed os part

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import os from 'os'
 
-const platform = os.platform()
-console.log("platofrom" , platform)
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
-      usePolling: platform === 'win32' || platform === 'linux',
+      usePolling: true,
       interval: 1000,
     },
     host: true,
