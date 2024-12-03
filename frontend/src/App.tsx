bfa62064608e21690e890722e9000fa01a8a81fe
Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>
2024-12-03 13:03:27 -0800

    Update frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
       </Routes>
       <Footer />
     </main>
