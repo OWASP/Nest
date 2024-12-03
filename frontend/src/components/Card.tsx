eb8577033f7dfbcc87bbfd1b39dc2f8cc97ec18c
Harsh Pandey <pandey73harsh@gmail.com>
2024-12-03 23:15:04 +0530

    fix tags

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -8,6 +8,7 @@ import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontaw
 import { CardProps, tooltipStyle } from "../lib/constants";
 import FontAwesomeIconWrapper from "../lib/FontAwesomeIconWrapper";
 import { cn } from "../lib/utils";
+import { useState } from "react";
 
 const Card = ({
     title,
@@ -24,6 +25,25 @@ const Card = ({
     languages,
     social,
 } : CardProps) => {
+
+    const [visibleLanguages, setVisibleLanguages] = useState(18);
+    const [visibleTopics, setVisibleTopics] = useState(18);
+
+    const [toggleLanguages, setToggleLanguages] = useState(true);
+    const [toggleTopics, setToggleTopics] = useState(true);
+
+    const loadMoreLanguages = () => {
+        if(toggleLanguages) setVisibleLanguages(languages?.length as number);
+        else setVisibleLanguages(18);
+        setToggleLanguages(!toggleLanguages);
+    };
+
+    const loadMoreTopics = () => {
+        if(toggleTopics) setVisibleTopics(topics?.length as number);
+        else setVisibleTopics(18);
+        setToggleTopics(!toggleTopics);
+    };
+
   return (
     <div className=" w-full md:max-w-6xl  h-fit flex flex-col justify-normal items-start gap-4 md:gap-2 p-4 px-6 border border-border rounded-md ">
       <div className=" w-full flex justify-between items-center flex-wrap gap-2 ">
@@ -81,22 +101,33 @@ const Card = ({
         <div className=" w-full flex md:flex-row flex-col justify-between items-center ">
             <div className=" flex flex-col justify-normal items-start gap-2 ">
 
-                <div className=" flex justify-normal items-center gap-2 ">
+                <div className=" flex justify-normal items-center gap-2 flex-wrap ">
                     {
-                        languages && languages.map((topic) => (
+                        languages && languages.slice(0, visibleLanguages).map((topic) => (
 
                             <TopicBadge key={topic} topic={topic} tooltipLabel={`This repository uses ${topic}`} />
 
                         ))
 
                     }
+                      {languages && languages.length > 18 && (
+                          <button onClick={loadMoreLanguages} className="">
+                              { toggleLanguages ? "See More" : "See Less" }
+                          </button>
+                      )}
                 </div>
                 <div className=" flex justify-normal items-center gap-2 flex-wrap ">
                     {
-                        topics && topics.map((topic) => (
+                        topics && topics.slice(0, visibleTopics).map((topic) => (
                             <TopicBadge key={topic} topic={topic} tooltipLabel={`This project is labeled as "${topic}"`} />
                         ))
                     }
+
+                      {topics && topics.length > 18 && (
+                          <button onClick={loadMoreTopics} className="">
+                             { toggleTopics ? "See More" : "See Less" }
+                          </button>
+                      )}
                 </div>
                 <div className=" flex justify-normal items-center gap-2 my-2 ">
                     {
@@ -112,6 +143,7 @@ const Card = ({
                             </a>
                         ))
                     }
+
                 </div>
             </div>
             <ActionButton
