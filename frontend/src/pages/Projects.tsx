2b5b403c9047ed055a386dea92348d99df878ebc
Yash Pandey <yash.pandey@agoda.com>
2024-12-09 00:04:36 +0530

    Added indexes to ensure unique key props

--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -24,42 +24,42 @@ export default function Projects() {
   }, [])
 
   return (
-    <div className="w-full min-h-screen flex flex-col justify-normal items-center text-text p-5 md:p-20">
-      <div className="w-full h-fit flex flex-col justify-normal items-center gap-4">
-        {projectData &&
-          projectData.projects.map((project) => {
-            const params: string[] = [
-              'idx_updated_at',
-              'idx_forks_count',
-              'idx_stars_count',
-              'idx_contributors_count',
-            ]
-            const filteredIcons = getFilteredIcons(project, params)
-            const handleButtonClick = () => {
-              window.open(`/projects/contribute?q=${project.idx_name}`, '_blank')
-            }
+      <div className="w-full min-h-screen flex flex-col justify-normal items-center text-text p-5 md:p-20">
+        <div className="w-full h-fit flex flex-col justify-normal items-center gap-4">
+          {projectData &&
+              projectData.projects.map((project, index) => {
+                const params: string[] = [
+                  'idx_updated_at',
+                  'idx_forks_count',
+                  'idx_stars_count',
+                  'idx_contributors_count',
+                ]
+                const filteredIcons = getFilteredIcons(project, params)
+                const handleButtonClick = () => {
+                  window.open(`/projects/contribute?q=${project.idx_name}`, '_blank')
+                }
 
-            const SubmitButton = {
-              label: 'Contribute',
-              icon: <FontAwesomeIconWrapper icon="fa-solid fa-code-fork" />,
-              onclick: handleButtonClick,
-            }
-            return (
-              <Card
-                key={project.objectID}
-                title={project.idx_name}
-                url={project.idx_url}
-                summary={project.idx_summary}
-                level={level[`${project.idx_level as keyof typeof level}`]}
-                icons={filteredIcons}
-                leaders={project.idx_leaders}
-                topContributors={project.idx_top_contributors}
-                topics={project.idx_topics}
-                button={SubmitButton}
-              />
-            )
-          })}
+                const SubmitButton = {
+                  label: 'Contribute',
+                  icon: <FontAwesomeIconWrapper icon="fa-solid fa-code-fork" />,
+                  onclick: handleButtonClick,
+                }
+                return (
+                    <Card
+                        key={project.objectID || `project-${index}`}
+                        title={project.idx_name}
+                        url={project.idx_url}
+                        summary={project.idx_summary}
+                        level={level[`${project.idx_level as keyof typeof level}`]}
+                        icons={filteredIcons}
+                        leaders={project.idx_leaders}
+                        topContributors={project.idx_top_contributors}
+                        topics={project.idx_topics}
+                        button={SubmitButton}
+                    />
+                )
+              })}
+        </div>
       </div>
-    </div>
   )
 }
