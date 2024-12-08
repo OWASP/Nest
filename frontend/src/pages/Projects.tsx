ac222036c63707b5f85b1ac40b1c76136e2792cb
Yash Pandey <97700473+yashpandey06@users.noreply.github.com>
2024-12-09 04:41:55 +0530

    Added indexes to ensure unique key props (#152)
    * Added indexes to ensure unique key props
    
    * fixes in linting
    
    * fixes in linting-2
    
    * Checking node version in job
    
    * fixes in lint
    
    * fixes node-version
    
    * Rebased changes
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Yash Pandey <yash.pandey@agoda.com>
    Co-authored-by: Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>

--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -8,12 +8,12 @@ import { API_URL } from '../utils/credentials.ts'
 
 export default function Projects() {
   const [projectData, setProjectData] = useState<ProjectDataType | null>(null)
+
   useEffect(() => {
     document.title = 'OWASP Projects'
     const fetchApiData = async () => {
       try {
         const response = await fetch(`${API_URL}/owasp/search/project`)
-
         const data = await response.json()
         setProjectData(data)
       } catch (error) {
@@ -24,42 +24,43 @@ export default function Projects() {
   }, [])
 
   return (
-    <div className="w-full min-h-screen flex flex-col justify-normal items-center text-text p-5 md:p-20">
-      <div className="w-full h-fit flex flex-col justify-normal items-center gap-4">
-        {projectData &&
-          projectData.projects.map((project) => {
-            const params: string[] = [
-              'idx_updated_at',
-              'idx_forks_count',
-              'idx_stars_count',
-              'idx_contributors_count',
-            ]
-            const filteredIcons = getFilteredIcons(project, params)
-            const handleButtonClick = () => {
-              window.open(`/projects/contribute?q=${project.idx_name}`, '_blank')
-            }
+      <div className="w-full min-h-screen flex flex-col justify-normal items-center text-text p-5 md:p-20">
+        <div className="w-full h-fit flex flex-col justify-normal items-center gap-4">
+          {projectData &&
+              projectData.projects.map((project, index) => {
+                const params: string[] = [
+                  'idx_updated_at',
+                  'idx_forks_count',
+                  'idx_stars_count',
+                  'idx_contributors_count',
+                ]
+                const filteredIcons = getFilteredIcons(project, params)
+                const handleButtonClick = () => {
+                  window.open(`/projects/contribute?q=${project.idx_name}`, '_blank')
+                }
 
-            const SubmitButton = {
-              label: 'Contribute',
-              icon: <FontAwesomeIconWrapper icon="fa-solid fa-code-fork" />,
-              onclick: handleButtonClick,
-            }
-            return (
-              <Card
-                key={project.objectID}
-                title={project.idx_name}
-                url={project.idx_url}
-                summary={project.idx_summary}
-                level={level[`${project.idx_level as keyof typeof level}`]}
-                icons={filteredIcons}
-                leaders={project.idx_leaders}
-                topContributors={project.idx_top_contributors}
-                topics={project.idx_topics}
-                button={SubmitButton}
-              />
-            )
-          })}
+                const SubmitButton = {
+                  label: 'Contribute',
+                  icon: <FontAwesomeIconWrapper icon="fa-solid fa-code-fork" />,
+                  onclick: handleButtonClick,
+                }
+
+                return (
+                    <Card
+                        key={project.objectID || `project-${index}`}
+                        title={project.idx_name}
+                        url={project.idx_url}
+                        summary={project.idx_summary}
+                        level={level[`${project.idx_level as keyof typeof level}`]}
+                        icons={filteredIcons}
+                        leaders={project.idx_leaders}
+                        topContributors={project.idx_top_contributors}
+                        topics={project.idx_topics}
+                        button={SubmitButton}
+                    />
+                )
+              })}
+        </div>
       </div>
-    </div>
   )
 }
