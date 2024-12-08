a86e37efcdd0665a60af8611e8e11c32b4162add
Kate Golovanova <kate@kgthreads.com>
2024-12-08 11:04:14 -0800

    Update Card component styles (#151)
    * Update Card component styles
    
    * Update Leaders font weight. Move projects data blocks to the card border.

--- frontend/src/components/TopicBadge.tsx
+++ frontend/src/components/TopicBadge.tsx
@@ -1,12 +1,20 @@
 import { Tooltip } from 'react-tooltip'
 import { tooltipStyle } from '../lib/constants'
 
-const TopicBadge = ({ topic, tooltipLabel }: { topic: string; tooltipLabel?: string }) => {
+const TopicBadge = ({
+  topic,
+  tooltipLabel,
+  type,
+}: {
+  topic: string
+  tooltipLabel?: string
+  type: string
+}) => {
   return (
     <div
       data-tooltip-id={`lang-tooltip-${topic}`}
       data-tooltip-content={tooltipLabel}
-      className=" p-1 px-2 text-xs font-bold rounded-md bg-[#6C757D] text-white "
+      className={` p-1 px-2 text-xs font-bold rounded-md bg-[#6C757D] text-white ${type == 'language' ? 'bg-[#868E96]' : 'bg-[#6C757D]'}`}
     >
       {topic}
 
