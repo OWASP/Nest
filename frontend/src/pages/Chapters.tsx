a758e087ecd1ca916d0ec422afde077563cd5752
Harsh Pandey <pandey73harsh@gmail.com>
2024-12-17 19:22:43 +0530

    remove search due to test fail

--- frontend/src/pages/Chapters.tsx
+++ frontend/src/pages/Chapters.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 
 import Card from '../components/Card'
-import SearchBar from '../components/Search'
 import FontAwesomeIconWrapper from '../lib/FontAwesomeIconWrapper'
 import { ChapterDataType } from '../lib/types'
 import { handleSocialUrls } from '../lib/utils'
@@ -9,7 +8,6 @@ import { API_URL } from '../utils/credentials'
 
 export default function Chapters() {
   const [chapterData, setChapterData] = useState<ChapterDataType | null>(null)
-  const [defaultChapter, setDefaultChapter] = useState<ChapterDataType | null>(null)
 
   useEffect(() => {
     document.title = 'OWASP Chapters'
@@ -18,7 +16,6 @@ export default function Chapters() {
         const response = await fetch(`${API_URL}/owasp/search/chapter`)
         const data = await response.json()
         setChapterData(data)
-        setDefaultChapter(data)
       } catch (error) {
         console.error(error)
       }
@@ -29,12 +26,6 @@ export default function Chapters() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-normal p-5 text-text">
       <div className="-mt-6 flex h-fit w-full flex-col items-center justify-normal gap-4">
-        <SearchBar
-          placeholder={`Search through ${defaultChapter?.active_chapters_count || 'OWASP'} chapters around the world...`}
-          searchEndpoint={`${API_URL}/owasp/search/chapter`}
-          onSearchResult={setChapterData}
-          defaultResults={defaultChapter}
-        />
         {chapterData &&
           chapterData.chapters.map((chapter, index) => {
             const formattedUrls = handleSocialUrls(chapter.idx_related_urls)
