237ccf64536ccb825fffb09ada1e7ac635af8327
Rajgupta36 <raj.gupta369@outlook.com>
2024-12-18 13:48:55 +0530

    loader component created

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+interface LoaderProps {
+  imageUrl: string // Single prop for both images
+}
+
+const Loader: React.FC<LoaderProps> = ({ imageUrl }) => {
+  const dark = imageUrl.replace('white', 'black')
+  console.log(dark)
+  return (
+    <div
+      className="relative h-16 w-16 animate-spin rounded-full border-4 border-[#98AFC7] dark:border-white"
+      style={{
+        borderTopColor: 'transparent',
+      }}
+    >
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="animate-fade-in-out">
+          <img
+            src={imageUrl}
+            alt="Loading indicator"
+            className="hidden rounded-full dark:block"
+            width={60}
+            height={60}
+          />
+          <img
+            src={dark}
+            alt="Loading indicator"
+            className="rounded-full dark:hidden"
+            width={60}
+            height={60}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
