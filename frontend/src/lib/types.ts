0548a440e7b0f22b5ca15c0a27c71ee8a3ea5570
Abhay <abhaytopno999@gmail.com>
2024-12-19 12:40:14 +0530

    Addition of Contribute Page

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -84,3 +84,24 @@ export interface CommitteeDataType {
   active_committees_count: number
   committees: CommitteeType[]
 }
+
+export interface ContributeDataType {
+  contributions: ContributionType[]
+}
+
+export interface ContributionType {
+  idx_created_at: number
+  idx_name: string
+  idx_leaders: string[]
+  idx_related_urls: string[]
+  idx_top_contributors: {
+    avatar_url: string
+    contributions_count: number
+    login: string
+    name: string
+  }[]
+  idx_summary: string
+  idx_updated_at: number
+  idx_url: string
+  objectID: string
+}
