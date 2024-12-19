6e8fdc9ef6df8686f419950f40c77dc883d31477
Harsh Pandey <pandey73harsh@gmail.com>
2024-12-19 23:14:45 +0530

    update header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,84 +1,98 @@
-import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState, useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
 
+import ModeToggle from './ModeToggle'
+import { cn } from '../lib/utils'
 import { headerLinks } from '../utils/constants'
 
 export default function Header() {
-  const [dark, setDark] = useState(() => {
-    return localStorage.getItem('theme') === 'dark'
-  })
-  const icon = !dark ? <FontAwesomeIcon icon={faSun} /> : <FontAwesomeIcon icon={faMoon} />
-
-  useEffect(() => {
-    if (dark) {
-      document.body.classList.add('dark')
-    } else {
-      document.body.classList.remove('dark')
-    }
-  }, [dark])
-
-  function toggleTheme() {
-    setDark(!dark)
-    const newTheme = !dark ? 'dark' : 'light'
-    document.body.classList.toggle('dark', !dark)
-    localStorage.setItem('theme', newTheme)
-  }
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const location = useLocation()
+  const toggleMobileMenu = () => setMobileMenuOpen(!mobileMenuOpen)
 
   return (
-    <div>
-      <div className="h-16 bg-owasp-blue dark:bg-slate-800">
-        <div className="flex h-full items-center" id="navbar-sticky">
-          <NavLink to="/">
-            <div className="flex h-full items-center">
-              <img
-                src={
-                  !dark
-                    ? '../public/img/owasp_icon_black_sm.png'
-                    : '../public/img/owasp_icon_white_sm.png'
-                }
-                className="h-16 px-2"
-                alt="OWASP Logo"
-              ></img>
-              <div className="text-2xl text-slate-800 dark:text-slate-300 dark:hover:text-slate-200">
-                Nest
-              </div>
-            </div>
-          </NavLink>
-          <div className="flex-1 justify-between rounded-lg pl-6 font-medium">
-            <div className="flex justify-start pl-6 text-slate-700 dark:text-slate-300">
-              {headerLinks.map((link, i) => (
-                <NavLink
-                  key={i}
-                  to={link.href}
-                  className="navlink px-3 py-2 hover:text-slate-800 dark:hover:text-slate-200"
-                  aria-current="page"
-                >
-                  {link.text}
-                </NavLink>
-              ))}
+    <header className="h-16 bg-owasp-blue dark:bg-slate-800">
+      <div
+        className="flex h-full w-full items-center px-4 max-md:justify-between"
+        id="navbar-sticky"
+      >
+        {/* Logo */}
+        <NavLink to="/">
+          <div className="flex h-full items-center">
+            <img
+              src={'../public/img/owasp_icon_white_sm.png'}
+              className="hidden h-16 dark:block"
+              alt="OWASP Logo"
+            ></img>
+            <img
+              src={'../public/img/owasp_icon_black_sm.png'}
+              className="block h-16 dark:hidden"
+              alt="OWASP Logo"
+            ></img>
+            <div className="text-2xl text-slate-800 dark:text-slate-300 dark:hover:text-slate-200">
+              Nest
             </div>
           </div>
-          <div className="justify-end">
-            <label className="inline-flex cursor-pointer content-center items-center">
-              <span className="ms-3 pr-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                {icon}
-              </span>
-              <input
-                onChange={toggleTheme}
-                type="checkbox"
-                value=""
-                className="peer sr-only"
-              ></input>
-              <div className="peer relative h-5 w-9 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-slate-500 peer-checked:after:translate-x-full peer-checked:after:border-white rtl:peer-checked:after:-translate-x-full dark:border-gray-500 dark:bg-gray-700"></div>
-              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                <i className="fa-regular fa-moon"></i>
-              </span>
-            </label>
+        </NavLink>
+        {/* Desktop Header Links */}
+        <div className="hidden flex-1 justify-between rounded-lg pl-6 font-medium md:block">
+          <div className="flex justify-start pl-6">
+            {headerLinks.map((link, i) => (
+              <NavLink
+                key={i}
+                to={link.href}
+                className={cn(
+                  'px-3 py-2 text-slate-700 hover:text-slate-800 dark:text-slate-300 dark:hover:text-slate-200',
+                  location.pathname === link.href && 'font-bold text-blue-800 dark:text-white'
+                )}
+                aria-current="page"
+              >
+                {link.text}
+              </NavLink>
+            ))}
           </div>
         </div>
+        <div className="flex items-center justify-normal space-x-4">
+          <div className="md:hidden">
+            <button
+              onClick={toggleMobileMenu}
+              className="text-slate-300 hover:text-slate-100 focus:outline-none"
+            >
+              <span className="sr-only">Open main menu</span>
+              {mobileMenuOpen ? (
+                <FontAwesomeIcon icon={faTimes} className="h-6 w-6" />
+              ) : (
+                <FontAwesomeIcon icon={faBars} className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+          <ModeToggle />
+        </div>
       </div>
-    </div>
+      <div
+        className={cn(
+          'fixed inset-y-0 left-0 z-50 w-64 transform bg-owasp-blue shadow-md transition-transform dark:bg-slate-800',
+          mobileMenuOpen ? 'translate-x-0' : '-translate-x-full'
+        )}
+      >
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {headerLinks.map((link, i) => (
+            <NavLink
+              key={i}
+              to={link.href}
+              className={cn(
+                'block px-3 py-2 text-slate-700 hover:text-slate-800 dark:text-slate-300 dark:hover:text-slate-200',
+                location.pathname === link.href && 'font-bold text-blue-800 dark:text-white'
+              )}
+              onClick={toggleMobileMenu}
+            >
+              {link.text}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </header>
   )
 }
