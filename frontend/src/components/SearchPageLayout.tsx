6df50159b8c89f73c41885d79851a0fcf0ff8da3
YASH PANDEY <pandeyyash7052@gmail.com>
2024-12-22 00:22:28 +0530

    rebased

--- frontend/src/components/SearchPageLayout.tsx
+++ frontend/src/components/SearchPageLayout.tsx
@@ -33,14 +33,17 @@ const SearchPageLayout = ({
 }: SearchPageLayoutProps) => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-normal p-5 text-text">
-      <SearchBar onSearch={onSearch} placeholder={searchPlaceholder} initialValue={searchQuery} />
-
       {!isLoaded ? (
         <div className="bg-background/50 fixed inset-0 flex items-center justify-center">
           <LoadingSpinner imageUrl={loadingImageUrl} />
         </div>
       ) : (
         <div className="flex h-fit w-full flex-col items-center justify-normal gap-4">
+          <SearchBar
+            onSearch={onSearch}
+            placeholder={searchPlaceholder}
+            initialValue={searchQuery}
+          />
           {totalPages === 0 && <div className="text bg:text-white m-4 text-xl">{empty}</div>}
           {children}
         </div>
