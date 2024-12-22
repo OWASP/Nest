d107c786a0b1a00bca6e3fab1a28a600682d7f8b
Nitin Awari <nitinawari.dev@gmail.com>
2024-12-22 14:30:56 +0530

    Added temporary redirection from / to /projects

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const Home = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/projects')
+  }, [navigate])
+
   return <div>Hello!</div>
 }
 
