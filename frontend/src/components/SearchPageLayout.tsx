de394b64c3ea14ff069bc06493f1ac243ffe4b15
Rajgupta36 <raj.gupta369@outlook.com>
2024-12-25 03:06:46 +0530

    updated searchBar

--- frontend/src/components/SearchPageLayout.tsx
+++ frontend/src/components/SearchPageLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import LoadingSpinner from '../components/LoadingSpinner'
 import Pagination from '../components/Pagination'
@@ -31,9 +31,17 @@ const SearchPageLayout = ({
   loadingImageUrl = '../public/img/owasp_icon_white_sm.png',
   children,
 }: SearchPageLayoutProps) => {
+  const [isFirstLoad, setIsFirstLoad] = useState(true)
+
+  useEffect(() => {
+    if (isLoaded && isFirstLoad) {
+      setIsFirstLoad(false)
+    }
+  }, [isLoaded, isFirstLoad])
+
   return (
     <div className="mt-16 flex min-h-screen w-full flex-col items-center justify-normal p-5 text-text">
-      {!isLoaded ? (
+      {isFirstLoad ? (
         <div className="bg-background/50 fixed inset-0 flex items-center justify-center">
           <LoadingSpinner imageUrl={loadingImageUrl} />
         </div>
@@ -44,12 +52,20 @@ const SearchPageLayout = ({
             placeholder={searchPlaceholder}
             initialValue={searchQuery}
           />
-          {totalPages === 0 && <div className="text bg:text-white m-4 text-xl">{empty}</div>}
-          {children}
+          {!isLoaded ? (
+            <div className="mt-20 flex h-64 w-full items-center justify-center">
+              <LoadingSpinner imageUrl={loadingImageUrl} />
+            </div>
+          ) : (
+            <>
+              {totalPages === 0 && <div className="text bg:text-white m-4 text-xl">{empty}</div>}
+              {children}
+            </>
+          )}
         </div>
       )}
 
-      {totalPages > 1 && (
+      {!isLoaded && totalPages > 1 && (
         <Pagination
           currentPage={currentPage}
           totalPages={totalPages}
