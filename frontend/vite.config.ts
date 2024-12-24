bcf52f1250233c951d83af059584461e1da65a0c
YASH PANDEY <pandeyyash7052@gmail.com>
2024-12-24 17:17:08 +0530

    fixes on lint--frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import EnvironmentPlugin from 'vite-plugin-environment'
-import path from "path"
+import path from 'path'
 export default defineConfig({
   plugins: [
     react(),
