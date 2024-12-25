8ab76525bea7dcfce99c65efef1a469642f24095
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2024-12-25 11:13:48 -0800

    Update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import EnvironmentPlugin from 'vite-plugin-environment'
+
 export default defineConfig({
   build: {
     sourcemap: true,
@@ -19,8 +20,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      lib: path.resolve(__dirname, 'src/lib'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      'sentry.config': path.resolve(__dirname, 'src/sentry.config.ts'),
       '@tests': path.resolve(__dirname, '__tests__'),
-      '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
