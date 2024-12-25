b0eb54f20b7085e5f14d5738d96a74d9dba03b88
ShashaankS <shashaank.srivastava04@gmail.com>
2024-12-25 23:32:16 +0530

    Resolved linter warnings

--- backend/apps/slack/views.py
+++ backend/apps/slack/views.py
@@ -1,12 +1,13 @@
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt  # noqa: D100
 from slack_bolt.adapter.django import SlackRequestHandler
+
 from apps.slack.apps import SlackConfig
-from apps.slack.commands import *
-from apps.slack.events import *
+from apps.slack.commands import *  # noqa: F403
+from apps.slack.events import *  # noqa: F403
 
 slack_handler = SlackRequestHandler(SlackConfig.app)
 
+
 @csrf_exempt
-def slack_events(request):
+def slack_events(request):  # noqa: D103
     return slack_handler.handle(request)
