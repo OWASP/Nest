598e5f4479c5875f220e505a1fcc7849b2686d7a
Harsh Pandey <pandey73harsh@gmail.com>
2024-12-29 01:07:42 +0530

    update icon for modechange

--- frontend/src/components/ModeToggle.tsx
+++ frontend/src/components/ModeToggle.tsx
@@ -27,15 +27,17 @@ function ModeToggle({ className }: { className?: string }) {
   return (
     <div className={cn('flex items-center space-x-2', className)}>
       <span className="text-sm font-medium text-gray-900 dark:text-gray-100">
-        <FontAwesomeIcon icon={faSun} />
+        <FontAwesomeIcon icon={dark ? faMoon : faSun} />
       </span>
       <label className="relative inline-flex cursor-pointer items-center">
-        <input type="checkbox" className="peer sr-only" checked={dark} onChange={darkModeHandler} />
+        <input
+          type="checkbox"
+          className="peer sr-only"
+          checked={!dark}
+          onChange={darkModeHandler}
+        />
         <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-white dark:border-gray-600 dark:bg-gray-700"></div>
       </label>
-      <span className="text-sm font-medium text-gray-900 dark:text-gray-100">
-        <FontAwesomeIcon icon={faMoon} />
-      </span>
     </div>
   )
 }
