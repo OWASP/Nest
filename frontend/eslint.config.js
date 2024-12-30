3be4523c8a96f9e211e96b857f81827c44142655
Rajgupta36 <raj.gupta369@outlook.com>
2024-12-30 15:29:02 +0530

    added eslint rule

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -16,6 +16,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default [
+  react.configs.flat['jsx-runtime'],
   {
     ignores: ['node_modules', 'build', 'dist', '.cache'],
   },
