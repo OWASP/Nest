f7bfd39ac68e94823f1701ae2311f33b59b0730b
Rajgupta36 <raj.gupta369@outlook.com>
2024-12-30 15:34:58 +0530

    updated code with new lint

--- frontend/__tests__/src/App.test.tsx
+++ frontend/__tests__/src/App.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import App from 'App'
-import React from 'react'
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 
--- frontend/__tests__/src/pages/Home.test.tsx
+++ frontend/__tests__/src/pages/Home.test.tsx
@@ -1,5 +1,4 @@
 import { render } from '@testing-library/react'
-import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Home from 'pages/Home'
 
