752024d415853ca09f2cfa33fd970aacba742689
Harsh Pandey <pandey73harsh@gmail.com>
2024-12-31 00:37:11 +0530

    add google tag manager

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,16 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
 import ErrorWrapper from './ErrorWrapper'
+import TagManager from 'react-gtm-module'
+import { GTM_AUTH, GTM_ID, GTM_PREVIEW } from 'utils/credentials.ts'
+
+const tagManagerArgs = {
+    gtmId: GTM_ID,
+    auth: GTM_AUTH,
+    preview: GTM_PREVIEW
+}
+
+TagManager.initialize(tagManagerArgs)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
