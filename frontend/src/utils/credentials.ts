22509bb3a925fe1a15ae3a81449224bb5c135ef4
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2024-12-31 10:49:27 -0800

    Refactor .env for backend and frontend

--- frontend/src/utils/credentials.ts
+++ frontend/src/utils/credentials.ts
@@ -1,6 +1,5 @@
-export const API_URL = process.env.VITE_NEST_API_URL
-export const APPLICATION_SENTRY_URL = process.env.VITE_NEST_SENTRY_DSN_URL
-export const APPLICATION_ENV = process.env.VITE_NEST_ENV
 export const ALGOLIA_APP_ID = process.env.VITE_ALGOLIA_APP_ID
-export const ALGOLIA_SEARCH_KEY = process.env.VITE_ALGOLIA_SEARCH_KEY
-export const NEST_ENV = process.env.VITE_NEST_ENV
+export const ALGOLIA_SEARCH_API_KEY = process.env.VITE_ALGOLIA_SEARCH_API_KEY
+export const API_URL = process.env.VITE_API_URL
+export const ENVIRONMENT = process.env.VITE_ENVIRONMENT
+export const SENTRY_DSN = process.env.VITE_SENTRY_DSN
