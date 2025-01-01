4209c6a0084246a892536f52d500ffca5b016ef3
Harsh Pandey <pandey73harsh@gmail.com>
2025-01-02 01:30:06 +0530

    fix the footer layout

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,9 +3,9 @@ import { Section } from 'utils/constants'
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t bg-slate-200 dark:bg-slate-800">
-      <div className="container px-4 py-4 text-slate-800 md:py-8 dark:text-slate-200">
-        <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-4">
+    <footer className="container mt-auto w-full border-t bg-slate-200 dark:bg-slate-800">
+      <div className="grid w-full place-content-center gap-12 px-4 py-4 text-slate-800 md:py-8 dark:text-slate-200">
+        <div className="grid w-full gap-20 sm:grid-cols-2 md:grid-cols-4">
           {footerSections.map((section: Section) => (
             <div key={section.title} className="space-y-4">
               <h3 className="text-lg font-semibold">{section.title}</h3>
@@ -29,8 +29,8 @@ export default function Footer() {
             </div>
           ))}
         </div>
-        <div className="pt-8">
-          <div className="flex flex-col gap-4 sm:ml-[52%] sm:flex-row sm:items-center">
+        <div className="grid w-full place-content-center">
+          <div className="flex w-full flex-col items-center gap-4 sm:flex-row">
             <p className="text-sm text-slate-600 dark:text-slate-400">
               © <span id="year">{new Date().getFullYear()}</span> OWASP Nest. All rights reserved.
             </p>
