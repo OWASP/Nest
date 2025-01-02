6bc771e665fc55bb15cdfe744ed87752a192d658
Nitin Awari <nitinawari.dev@gmail.com>
2025-01-02 21:39:54 +0530

    import fix

--- frontend/src/components/ui/toaster.tsx
+++ frontend/src/components/ui/toaster.tsx
@@ -5,8 +5,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from '@/components/ui/toast'
-import { useToast } from '@/lib/hooks/use-toast'
+} from '/components/ui/toast'
+import { useToast } from 'lib/hooks/use-toast'
 
 export function Toaster() {
   const { toasts } = useToast()
