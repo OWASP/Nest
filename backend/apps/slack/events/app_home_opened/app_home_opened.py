3423fa76515f415a3e0fc025c3a988556b42ba31
Rajgupta36 <raj.gupta369@outlook.com>
2025-01-05 01:26:33 +0530

    pre-commit

--- backend/apps/slack/events/app_home_opened/app_home_opened.py
+++ backend/apps/slack/events/app_home_opened/app_home_opened.py
@@ -1,7 +1,10 @@
+"""Slack bot app_home_opened event handler."""
+
 import logging
+
 from django.conf import settings
 from slack_sdk.errors import SlackApiError
-from slack_sdk.models.views import View
+
 from apps.slack.apps import SlackConfig
 from apps.slack.blocks import markdown
 
@@ -9,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 def handler(event, client, ack):
-    """Handles the app_home_opened event to update the Slack Home Tab."""
+    """Handle the app_home_opened event."""
     ack()
 
     if not settings.SLACK_EVENTS_ENABLED:
@@ -22,9 +25,7 @@ def handler(event, client, ack):
         home_view = {
             "type": "home",
             "blocks": [
-                markdown(
-                    "*Quick Actions:*"
-                ),
+                markdown("*Quick Actions:*"),
                 {
                     "type": "actions",
                     "elements": [
@@ -33,46 +34,44 @@ def handler(event, client, ack):
                             "text": {
                                 "type": "plain_text",
                                 "text": "🌟 View Projects",
-                                "emoji": True
+                                "emoji": True,
                             },
                             "value": "view_projects",
-                            "action_id": "view_projects_action"
+                            "action_id": "view_projects_action",
                         },
                         {
                             "type": "button",
                             "text": {
                                 "type": "plain_text",
                                 "text": "👥 View Committees",
-                                "emoji": True
+                                "emoji": True,
                             },
                             "value": "view_committees",
-                            "action_id": "view_committees_action"
+                            "action_id": "view_committees_action",
                         },
                         {
                             "type": "button",
                             "text": {
                                 "type": "plain_text",
                                 "text": "📚 View Chapters",
-                                "emoji": True
+                                "emoji": True,
                             },
                             "value": "view_chapters",
-                            "action_id": "view_chapters_action"
-                        }
-                    ]
+                            "action_id": "view_chapters_action",
+                        },
+                    ],
                 },
                 markdown(
-                    f"""*🏠 Welcome to NestBot, <@{user_id}>!* 
+                    f"""*🏠 Welcome to NestBot, <@{user_id}>!*
                     """
                 ),
-            ]
+            ],
         }
 
         client.views_publish(user_id=user_id, view=home_view)
-        logger.info(f"Home tab updated for user {user_id}")
 
-    except SlackApiError as e:
-        logger.error(
-            f"Error publishing Home Tab for user {user_id}: {e.response['error']}")
+    except SlackApiError:
+        logger.exception("Error publishing Home Tab for user {user_id}: {e.response['error']}")
 
 
 if SlackConfig.app:
--- backend/apps/slack/events/app_home_opened/home_actions.py
+++ backend/apps/slack/events/app_home_opened/home_actions.py
@@ -1,71 +1,71 @@
+"""Slack home actions handler."""
+
 import logging
+
 from slack_sdk.errors import SlackApiError
+
 from apps.slack.apps import SlackConfig
-from slack_sdk.models.views import View
-from apps.slack.blocks import markdown
 
 logger = logging.getLogger(__name__)
 
 
 def handle_home_actions(ack, body, client):
-    from apps.owasp.api.search.project import get_projects
+    """Handle actions triggered in the Slack Home Tab."""
     from apps.owasp.api.search.chapter import get_chapters
     from apps.owasp.api.search.committee import get_committees
+    from apps.owasp.api.search.project import get_projects
 
-    """Handles button click actions."""
     ack()
 
-    action_id = body['actions'][0]['action_id']
-    user_id = body['user']['id']
+    action_id = body["actions"][0]["action_id"]
+    user_id = body["user"]["id"]
 
     try:
-        if action_id == 'view_projects_action':
+        if action_id == "view_projects_action":
             project_details = get_projects(query="", limit=10, page=1)
             view_title = "*Top Projects*"
             blocks = []
 
-            if project_details['hits']:
-                content = f"Here are the top {len(project_details['hits'])} projects you can explore:\n"
-                for project in project_details['hits']:
+            if project_details["hits"]:
+                content = (
+                    f"Here are the top {len(project_details['hits'])} projects you can explore:\n"
+                )
+                for project in project_details["hits"]:
                     project_card = {
                         "type": "section",
                         "text": {
                             "type": "mrkdwn",
                             "text": (
                                 f"*<{project['idx_url']}|{project['idx_name']}>*\n"
-                                f"• :bust_in_silhouette: *Contributors:* {project['idx_contributors_count']}\n"
+                                f"• :bust_in_silhouette: *Contributors:*"
+                                "{project['idx_contributors_count']}\n"
                                 f"• :fork_and_knife: *Forks:* {project['idx_forks_count']}\n"
                                 f"• :star2: *Stars:* {project['idx_stars_count']}\n"
-                            )
+                            ),
                         },
                     }
-                    blocks.append({
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "━━━━━━━━━━━━━━━━━━━━"
+                    blocks.append(
+                        {
+                            "type": "section",
+                            "text": {"type": "mrkdwn", "text": "━━━━━━━━━━━━━━━━━━━━"},
                         }
-                    })
+                    )
                     blocks.append(project_card)
 
             else:
                 content = "No projects found based on your search query."
-                blocks.append({
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": content
-                    }
-                })
+                blocks.append({"type": "section", "text": {"type": "mrkdwn", "text": content}})
 
-        elif action_id == 'view_committees_action':
+        elif action_id == "view_committees_action":
             committee_details = get_committees(query="", limit=10, page=1)
             view_title = "*Top Committees*"
             blocks = []
 
-            if committee_details['hits']:
-                content = f"Here are the top {len(committee_details['hits'])} committees you can join:\n"
-                for committee in committee_details['hits']:
+            if committee_details["hits"]:
+                content = (
+                    f"Here are the top {len(committee_details['hits'])} committees you can join:\n"
+                )
+                for committee in committee_details["hits"]:
                     committee_card = {
                         "type": "section",
                         "text": {
@@ -73,37 +73,30 @@ def handle_home_actions(ack, body, client):
                             "text": (
                                 f"*<{committee['idx_url']}|{committee['idx_name']}>*\n"
                                 f"• :star2: *Summary:* {committee['idx_summary']}\n"
-                            )
+                            ),
                         },
                     }
-                    blocks.append({
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "━━━━━━━━━━━━━━━━━━━━"
+                    blocks.append(
+                        {
+                            "type": "section",
+                            "text": {"type": "mrkdwn", "text": "━━━━━━━━━━━━━━━━━━━━"},
                         }
-                    })
+                    )
                     blocks.append(committee_card)
 
             else:
                 content = "No committees found based on your search query."
-                blocks.append({
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": content
-                    }
-                })
+                blocks.append({"type": "section", "text": {"type": "mrkdwn", "text": content}})
 
-        elif action_id == 'view_chapters_action':
-            # Fetch chapters
+        elif action_id == "view_chapters_action":
             chapter_details = get_chapters(query="", limit=10, page=1)
             view_title = "*Top Chapters*"
             blocks = []
 
-            if chapter_details['hits']:
-                content = f"Here are the top {len(chapter_details['hits'])} chapters you can be a part of:\n"
-                for chapter in chapter_details['hits']:
+            if chapter_details["hits"]:
+                content = f"Here are the top {len(chapter_details['hits'])} chapters"
+                "you can be a part of:\n"
+                for chapter in chapter_details["hits"]:
                     chapter_card = {
                         "type": "section",
                         "text": {
@@ -111,27 +104,20 @@ def handle_home_actions(ack, body, client):
                             "text": (
                                 f"*<{chapter['idx_url']}|{chapter['idx_name']}>*\n"
                                 f"• :star2: *Summary:* {chapter['idx_summary']}\n"
-                            )
+                            ),
                         },
                     }
-                    blocks.append({
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "━━━━━━━━━━━━━━━━━━━━"
+                    blocks.append(
+                        {
+                            "type": "section",
+                            "text": {"type": "mrkdwn", "text": "━━━━━━━━━━━━━━━━━━━━"},
                         }
-                    })
+                    )
                     blocks.append(chapter_card)
 
             else:
                 content = "No chapters found based on your search query."
-                blocks.append({
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": content
-                    }
-                })
+                blocks.append({"type": "section", "text": {"type": "mrkdwn", "text": content}})
 
         else:
             view_title = "*Unknown Action*"
@@ -140,25 +126,15 @@ def handle_home_actions(ack, body, client):
         new_home_view = {
             "type": "home",
             "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": view_title
-                    }
-                },
+                {"type": "section", "text": {"type": "mrkdwn", "text": view_title}},
                 *blocks,
-            ]
+            ],
         }
 
-        # Update the Home Tab
         client.views_publish(user_id=user_id, view=new_home_view)
-        logger.info(
-            f"Home tab updated for user {user_id} after clicking button {action_id}")
 
     except SlackApiError as e:
-        logger.error(
-            f"Error updating Home Tab for user {user_id}: {e.response['error']}")
+        logger.exception("Error publishing Home Tab for user %s: %s", user_id, e.response["error"])
 
 
 # actions
