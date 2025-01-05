80ae68180fa2091aaef0ccbd6ee10a57f82d0229
Naveen Pal <paln76050@gmail.com>
2025-01-06 00:39:47 +0530

    Increased coverage for app/common (#338)
    * increased coverage for app/common
    
    * fix load-data error due to changes and mock time delay in geocoding
    
    * remove unused files and unused arguements
    
    * added geocoding
    
    * removed geocoding
    
    * added get_location_coordinates
    
    * moved unused patches to with context
    
    ---------
    
    Co-authored-by: Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>

--- backend/apps/common/geocoding.py
+++ backend/apps/common/geocoding.py
@@ -1,33 +1,10 @@
 """Geocoding utils."""
 
-import logging
 import time
-from functools import lru_cache
 
-import requests
 from geopy.geocoders import Nominatim
 
 from apps.common.utils import get_nest_user_agent
-
-logger = logging.getLogger(__name__)
-
-
-@lru_cache(maxsize=1024)
-def get_ip_coordinates(ip_address):
-    """Return IP address geo coordinates."""
-    if not ip_address:
-        return None
-
-    try:
-        response = requests.get(f"https://ipapi.co/{ip_address}/latlong/", timeout=5)
-        if response.status_code == requests.codes.ok:
-            return response.text.split(",")
-    except TypeError:
-        logger.exception(
-            "Could not get coordinates for the IP address",
-            extra={"ip_address": ip_address},
-        )
-        return None
 
 
 def get_location_coordinates(query, delay=2):
