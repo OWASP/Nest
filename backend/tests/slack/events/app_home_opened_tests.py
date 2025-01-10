642eafc8eb68ca78a5d006d407a6fe260d0b0165
Rajgupta36 <raj.gupta369@outlook.com>
2025-01-10 23:26:44 +0530

    fixes

--- backend/tests/slack/events/app_home_opened_tests.py
+++ backend/tests/slack/events/app_home_opened_tests.py
@@ -21,9 +21,10 @@ class TestSlackHandler:
 
         mock_client.views_publish.return_value = {"ok": True}
 
-        with patch("apps.slack.events.app_home_opened.get_header") as mock_get_header, patch(
-            "apps.slack.events.app_home_opened.markdown"
-        ) as mock_markdown:
+        with (
+            patch("apps.slack.events.app_home_opened.get_header") as mock_get_header,
+            patch("apps.slack.events.app_home_opened.markdown") as mock_markdown,
+        ):
             mock_get_header.return_value = [
                 {"type": "section", "text": {"type": "mrkdwn", "text": "Header"}}
             ]
@@ -40,24 +41,24 @@ class TestSlackHandler:
 
             handler(mock_event, mock_client, mock_ack)
 
-        mock_ack.assert_called_once()
+            mock_ack.assert_called_once()
 
-        mock_client.views_publish.assert_called_once_with(
-            user_id="U12345",
-            view={
-                "type": "home",
-                "blocks": [
-                    {"type": "section", "text": {"type": "mrkdwn", "text": "Header"}},
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": (
-                                f"*Hi <@{mock_event['user']}>!* "
-                                "Welcome to the OWASP Slack Community!"
-                            ),
+            mock_client.views_publish.assert_called_once_with(
+                user_id="U12345",
+                view={
+                    "type": "home",
+                    "blocks": [
+                        {"type": "section", "text": {"type": "mrkdwn", "text": "Header"}},
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": (
+                                    f"*Hi <@{mock_event['user']}>!* "
+                                    "Welcome to the OWASP Slack Community!"
+                                ),
+                            },
                         },
-                    },
-                ],
-            },
-        )
+                    ],
+                },
+            )
