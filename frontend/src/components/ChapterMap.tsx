5ea83c55b82e57b4ee6a40bbdebcd04530368d3d
AbhayMishra <grabhaymishra@gmail.com>
2025-01-14 02:08:13 +0530

    added leaflet.markercluster

--- frontend/src/components/ChapterMap.tsx
+++ frontend/src/components/ChapterMap.tsx
@@ -1,6 +1,9 @@
 import L from 'leaflet'
 import { useEffect, useRef } from 'react'
 import 'leaflet/dist/leaflet.css'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import 'leaflet.markercluster'
 
 const ChapterMap = ({ geoLocData }) => {
   const mapRef = useRef<L.Map | null>(null)
@@ -23,18 +26,24 @@ const ChapterMap = ({ geoLocData }) => {
     const map = mapRef.current
 
     map.eachLayer((layer) => {
-      if (layer instanceof L.Marker) {
+      if (layer instanceof L.Marker || layer instanceof L.LayerGroup) {
         map.removeLayer(layer)
       }
     })
 
+    const markerClusterGroup = L.markerClusterGroup()
+
     const bounds: [number, number][] = []
     geoLocData.forEach((chapter) => {
       if (chapter._geoloc) {
-        L.marker([chapter._geoloc.lat, chapter._geoloc.lng]).addTo(map).bindPopup(chapter.idx_name)
+        const marker = L.marker([chapter._geoloc.lat, chapter._geoloc.lng])
+        marker.bindPopup(chapter.idx_name)
+        markerClusterGroup.addLayer(marker)
         bounds.push([chapter._geoloc.lat, chapter._geoloc.lng])
       }
     })
+
+    map.addLayer(markerClusterGroup)
 
     if (bounds.length > 0) {
       map.fitBounds(bounds as L.LatLngBoundsExpression)
