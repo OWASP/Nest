2198ce57ee2dc7abe28a6038e1c917abef06826d
Satwik Sai Prakash Sahoo <satwikspsahoo@gmail.com>
2025-01-16 21:03:27 +0530

    Fix: Fallback to username in ContributorAvatar component

--- frontend/src/components/ContributorAvatar.tsx
+++ frontend/src/components/ContributorAvatar.tsx
@@ -1,22 +1,26 @@
-import { Tooltip } from 'react-tooltip'
-import { topContributorsType } from 'types/contributor'
-import { tooltipStyle } from 'utils/constants'
+import { Tooltip } from 'react-tooltip';
+import { topContributorsType } from 'types/contributor';
+import { tooltipStyle } from 'utils/constants';
 
 const ContributorAvatar = ({ contributor }: { contributor: topContributorsType }) => {
+  // Fallback to username if name is unavailable
+  const displayName = contributor.name || contributor.login || 'Anonymous';
+
   return (
     <a
       data-tooltip-id={`avatar-tooltip-${contributor.login}`}
-      data-tooltip-content={`${contributor.contributions_count} contributions by ${contributor.name}`}
+      data-tooltip-content={`${contributor.contributions_count} contributions by ${displayName}`}
       href={`/community/users/${contributor.login}`}
       target="_blank"
     >
       <img
         className="h-[30px] w-[30px] rounded-full grayscale hover:grayscale-0"
         src={contributor.avatar_url}
+        alt={`${displayName}'s avatar`}
       />
       <Tooltip id={`avatar-tooltip-${contributor.login}`} style={tooltipStyle} />
     </a>
-  )
-}
+  );
+};
 
-export default ContributorAvatar
+export default ContributorAvatar;
