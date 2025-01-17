760db5c544173d7e1cf3c58047925f02827840fa
Naveen Pal <paln76050@gmail.com>
2025-01-17 22:05:29 +0530

    updated github/mixins entities tests (#533)
    Co-authored-by: Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>

--- backend/tests/github/models/mixins/release_tests.py
+++ backend/tests/github/models/mixins/release_tests.py
@@ -6,24 +6,26 @@ import pytest
 from apps.github.models.mixins.release import ReleaseIndexMixin
 
 
-class MockModel(ReleaseIndexMixin):
-    def __init__(self):
-        self.author = MagicMock()
+@pytest.fixture()
+def release_index_mixin_instance():
+    instance = ReleaseIndexMixin()
+    instance.author = MagicMock()
 
-        self.author.avatar_url = "https://example.com/avatar.png"
-        self.author.login = "test_user"
-        self.author.name = "Test User"
+    instance.author.avatar_url = "https://example.com/avatar.png"
+    instance.author.login = "test_user"
+    instance.author.name = "Test User"
 
-        self.repository = MagicMock(
-            path="mock/repository",
-            project=MagicMock(nest_key="mock/project"),
-        )
-        self.created_at = datetime(2023, 1, 1, tzinfo=timezone.utc)
-        self.published_at = datetime(2023, 6, 1, tzinfo=timezone.utc)
-        self.description = "This is a long description"
-        self.is_pre_release = True
-        self.name = "Release v1.0.0"
-        self.tag_name = "v1.0.0"
+    instance.repository = MagicMock(
+        path="mock/repository",
+        project=MagicMock(nest_key="mock/project"),
+    )
+    instance.created_at = datetime(2023, 1, 1, tzinfo=timezone.utc)
+    instance.published_at = datetime(2023, 6, 1, tzinfo=timezone.utc)
+    instance.description = "This is a long description"
+    instance.is_pre_release = True
+    instance.name = "Release v1.0.0"
+    instance.tag_name = "v1.0.0"
+    return instance
 
 
 @pytest.mark.parametrize(
@@ -55,12 +57,11 @@ class MockModel(ReleaseIndexMixin):
         ("idx_published_at", None),
     ],
 )
-def test_release_index(attr, expected):
-    mock_instance = MockModel()
+def test_release_index(release_index_mixin_instance, attr, expected):
     if attr == "idx_author" and not expected:
-        mock_instance.author = None
+        release_index_mixin_instance.author = None
     elif attr == "idx_project" and expected == "":
-        mock_instance.repository.project = None
+        release_index_mixin_instance.repository.project = None
     elif attr == "idx_published_at" and expected is None:
-        mock_instance.published_at = None
-    assert getattr(mock_instance, attr) == expected
+        release_index_mixin_instance.published_at = None
+    assert getattr(release_index_mixin_instance, attr) == expected
