b943274d9be14e5f776966be9b14d4e5442c5147
yash goyal <yg364550@gmail.com>
2025-01-19 03:53:57 +0530

    removed heading hover effect

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -58,14 +58,6 @@ const Card = ({
     setShowAll(!toggleLanguagesAndTopics)
   }
 
-  const handleHeadingColor = (e, isMouseEnter) => {
-    if (isMouseEnter) {
-      e.currentTarget.style.color = level?.color || ''
-    } else {
-      e.currentTarget.style.color = ''
-    }
-  }
-
   return (
     <div className="mb-2 mt-4 flex w-full flex-col items-start rounded-md border border-border bg-white pb-4 pl-4 transition-colors duration-300 ease-linear dark:bg-[#212529] md:max-w-6xl">
       <div className="flex w-full flex-col items-start gap-4 pt-2 sm:flex-row sm:items-center sm:gap-6 md:pt-0">
@@ -88,8 +80,6 @@ const Card = ({
               style={{
                 transition: 'color 0.3s ease',
               }}
-              onMouseEnter={(e) => handleHeadingColor(e, true)}
-              onMouseLeave={(e) => handleHeadingColor(e, false)}
             >
               {title}
             </h1>
