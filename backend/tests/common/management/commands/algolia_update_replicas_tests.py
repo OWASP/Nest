2a5694a2c778eba5cdba50247fc22c2362784005
Rajgupta36 <raj.gupta369@outlook.com>
2025-01-23 04:35:14 +0530

    pre-commit

--- backend/tests/common/management/commands/algolia_update_replicas_tests.py
+++ backend/tests/common/management/commands/algolia_update_replicas_tests.py
@@ -4,6 +4,7 @@ from io import StringIO
 from unittest.mock import patch
 
 import pytest
+from algoliasearch.http.exceptions import AlgoliaException
 from django.core.management import call_command
 
 
@@ -22,20 +23,21 @@ class TestUpdateReplicasCommand:
         """Test successful replica configuration."""
         with patch("sys.stdout", new=StringIO()) as fake_out:
             call_command("algolia_update_replicas")
-            
-            expected_output = "\n Starting replica configuration...\n" \
-                            "\n Replica have been Successfully created.\n"
+
+            expected_output = (
+                "\n Starting replica configuration...\n"
+                "\n Replica have been Successfully created.\n"
+            )
             assert fake_out.getvalue() == expected_output
             self.mock_replica_update.assert_called_once()
 
     def test_handle_exception(self):
         """Test handling of exceptions during replica configuration."""
         error_message = "Failed to configure replicas"
-        self.mock_replica_update.side_effect = Exception(error_message)
+        self.mock_replica_update.side_effect = AlgoliaException(error_message)
 
-        with pytest.raises(Exception) as exc_info:
+        with pytest.raises(AlgoliaException) as exc_info:
             call_command("algolia_update_replicas")
 
         assert str(exc_info.value) == error_message
         self.mock_replica_update.assert_called_once()
-
