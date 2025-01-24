f7561dd371806f4f7eeda9efa86f9655b4c7943b
Rajgupta36 <raj.gupta369@outlook.com>
2025-01-24 17:45:29 +0530

    fix

--- frontend/src/utils/urlIconMappings.ts
+++ frontend/src/utils/urlIconMappings.ts
@@ -12,13 +12,18 @@ export const getSocialIcon = (url) => {
   if (!/^https?:\/\//i.test(url)) {
     url = 'http://' + url
   }
+
   const hostname = new URL(url).hostname.toLowerCase()
 
   if (hostname.includes('discord')) return faDiscord
   if (hostname.includes('instagram')) return faInstagram
   if (hostname.includes('linkedin')) return faLinkedin
   if (hostname.includes('youtube')) return faYoutube
-  if (hostname.includes('x.com') || hostname.includes('twitter')) return faXTwitter
   if (hostname.includes('meetup')) return faMeetup
+
+  if (hostname === 'x.com' || hostname.endsWith('.x.com') || hostname.includes('twitter')) {
+    return faXTwitter
+  }
+
   return faGlobe
 }
