a2ec8df4ec9da89d7bb3350b0c0b15d6472b23c8
YourBroCode <kr.shyamiit@gmail.com>
2025-01-26 03:07:13 +0530

    fixed-the alignment of project-details and statistics blocks

--- frontend/src/pages/ProjectDetails.tsx
+++ frontend/src/pages/ProjectDetails.tsx
@@ -80,33 +80,31 @@ const ProjectDetailsPage = () => {
         <SecondaryCard title="Summary">
           <p>{project.summary}</p>
         </SecondaryCard>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          <SecondaryCard title="Project Details" className="gap-2 flex flex-col justify-between p-4">
-            <p>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+          <SecondaryCard title="Project Details" className="gap-2 md:col-span-2">
+            <p className="pb-1" >
               <strong>Type:</strong> {project.type[0].toUpperCase() + project.type.slice(1)}
             </p>
-            <p>
+            <p className="pb-1" >
               <strong>Level:</strong> {project.level[0].toUpperCase() + project.level.slice(1)}
             </p>
-            <p>
-              <strong>Organization:</strong> {project.organizations}
-            </p>
-            <p>
+          
+            <p className="pb-1" >
               <strong>Project Leaders:</strong> {project.leaders.join(', ')}
             </p>
-            <p>
+            <p className="pb-1" >
               <strong>Last Updated:</strong> {formatDate(project.updated_at)}
             </p>
-            <p>
+            <p className="pb-1" >
               <strong>URL:</strong>{' '}
               <a href={project.url} className="hover:underline dark:text-sky-600">
                 {project.url}
               </a>
             </p>
           </SecondaryCard>
+          <SecondaryCard title="Statistics">
 
-          <SecondaryCard title="Statistics" className="flex flex-col justify-between p-4">
-            <InfoBlock className="pb-1" icon={faUsers}  value={`${project.contributors_count} Contributors`}/>
+            <InfoBlock className="pb-1"  icon={faUsers} value={`${project.contributors_count} Contributors`}/>
             <InfoBlock className="pb-1" icon={faCodeFork} value={`${project.forks_count} Forks`} />
             <InfoBlock className="pb-1" icon={faStar} value={`${project.stars_count} Stars`} />
             <InfoBlock className="pb-1" icon={faBook} value={`${project.issues_count} Issues`} />
