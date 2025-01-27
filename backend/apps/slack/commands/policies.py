9d26c33171e047166cf67001506ffa28fd8ddff4
yashgoyal0110 <yg364550@gmail.com>
2025-01-27 10:37:07 +0530

    nestbot-policies

--- backend/apps/slack/commands/policies.py
+++ backend/apps/slack/commands/policies.py
@@ -17,6 +17,21 @@ def policies_handler(ack, command, client):
         return
 
     blocks = [
+        markdown(f"- <https://owasp.org/www-policy/operational/awards-and-scholarships|Awards and Scholarships Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/board-code-of-conduct|Board Code of Conduct>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/branding|Branding Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/chapters|Chapters Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/code-of-conduct|Code of Conduct Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/committees|Committees Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/community-review-process|Community Review Process Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/conferences-events|Conferences and Events Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/conflict-resolution|Conflict Resolution Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/conflict-of-interest|Conflict of Interest Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/election|Election Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/events|Events Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/expense-reimbursement|Expense Reimbursement Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/force-majeure-sanctions|Force Majeure and Sanctions Policy>\n"),
+        markdown(f"- <https://owasp.org/www-policy/operational/general-disclaimer|Force General Disclaimer Policy>\n"),
         markdown(f"Please visit <https://owasp.org/www-policy/|OWASP policies> page{NL}"),
     ]
 
