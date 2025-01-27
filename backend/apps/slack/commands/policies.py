137e9111ea4664a378338f9e29d24ff7b2440369
yashgoyal0110 <yg364550@gmail.com>
2025-01-27 10:46:17 +0530

    commit

--- backend/apps/slack/commands/policies.py
+++ backend/apps/slack/commands/policies.py
@@ -18,39 +18,49 @@ def policies_handler(ack, command, client):
 
     blocks = [
         markdown(
-            "- <https://owasp.org/www-policy/operational/awards-and-scholarships|Awards and Scholarships Policy>\n"
+            "- <https://owasp.org/www-policy/operational/awards-and-scholarships|"
+            "Awards and Scholarships Policy>\n"
         ),
         markdown(
-            "- <https://owasp.org/www-policy/operational/board-code-of-conduct|Board Code of Conduct>\n"
+            "- <https://owasp.org/www-policy/operational/board-code-of-conduct|"
+            "Board Code of Conduct>\n"
         ),
         markdown("- <https://owasp.org/www-policy/operational/branding|Branding Policy>\n"),
         markdown("- <https://owasp.org/www-policy/operational/chapters|Chapters Policy>\n"),
         markdown(
-            "- <https://owasp.org/www-policy/operational/code-of-conduct|Code of Conduct Policy>\n"
+            "- <https://owasp.org/www-policy/operational/code-of-conduct|"
+            "Code of Conduct Policy>\n"
         ),
         markdown("- <https://owasp.org/www-policy/operational/committees|Committees Policy>\n"),
         markdown(
-            "- <https://owasp.org/www-policy/operational/community-review-process|Community Review Process Policy>\n"
+            "- <https://owasp.org/www-policy/operational/community-review-process|"
+            "Community Review Process Policy>\n"
         ),
         markdown(
-            "- <https://owasp.org/www-policy/operational/conferences-events|Conferences and Events Policy>\n"
+            "- <https://owasp.org/www-policy/operational/conferences-events|"
+            "Conferences and Events Policy>\n"
         ),
         markdown(
-            "- <https://owasp.org/www-policy/operational/conflict-resolution|Conflict Resolution Policy>\n"
+            "- <https://owasp.org/www-policy/operational/conflict-resolution|"
+            "Conflict Resolution Policy>\n"
         ),
         markdown(
-            "- <https://owasp.org/www-policy/operational/conflict-of-interest|Conflict of Interest Policy>\n"
+            "- <https://owasp.org/www-policy/operational/conflict-of-interest|"
+            "Conflict of Interest Policy>\n"
         ),
         markdown("- <https://owasp.org/www-policy/operational/election|Election Policy>\n"),
         markdown("- <https://owasp.org/www-policy/operational/events|Events Policy>\n"),
         markdown(
-            "- <https://owasp.org/www-policy/operational/expense-reimbursement|Expense Reimbursement Policy>\n"
+            "- <https://owasp.org/www-policy/operational/expense-reimbursement|"
+            "Expense Reimbursement Policy>\n"
         ),
         markdown(
-            "- <https://owasp.org/www-policy/operational/force-majeure-sanctions|Force Majeure and Sanctions Policy>\n"
+            "- <https://owasp.org/www-policy/operational/force-majeure-sanctions|"
+            "Force Majeure and Sanctions Policy>\n"
         ),
         markdown(
-            "- <https://owasp.org/www-policy/operational/general-disclaimer|Force General Disclaimer Policy>\n"
+            "- <https://owasp.org/www-policy/operational/general-disclaimer|"
+            "General Disclaimer Policy>\n"
         ),
         markdown(f"Please visit <https://owasp.org/www-policy/|OWASP policies> page{NL}"),
     ]
