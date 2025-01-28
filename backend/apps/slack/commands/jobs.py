a6f3951c891a66f062b4f02d71fb4169fc6a08ab
YourBroCode <kr.shyamiit@gmail.com>
2025-01-29 02:59:43 +0530

    update NestBot_/jobs command

--- backend/apps/slack/commands/jobs.py
+++ backend/apps/slack/commands/jobs.py
@@ -2,7 +2,7 @@
 
 from django.conf import settings
 
-from apps.common.constants import NL
+from apps.common.constants import NL, OWASP_WEBSITE_URL
 from apps.slack.apps import SlackConfig
 from apps.slack.blocks import markdown
 from apps.slack.constants import OWASP_JOBS_CHANNEL_ID
@@ -19,6 +19,20 @@ def jobs_handler(ack, command, client):
 
     blocks = [
         markdown(f"Please join <{OWASP_JOBS_CHANNEL_ID}> channel{NL}"),
+        markdown(
+            "This Slack channel contains community"
+            "-driven posts about job opportunities, "
+            "professional networking, and career advice"
+            " in cybersecurity and related fields. "
+            "\n\n⚠️ *Important Disclaimer:* This is *NOT* an"
+            " official OWASP channel, and its contents "
+            "have *NOT* been endorsed, reviewed, or approved by"
+            " OWASP. The information shared here is "
+            "community-generated and not connected to the "
+            f"<{OWASP_WEBSITE_URL}/careers/|official OWASP Careers page>. "
+            "For official OWASP resources, visit the OWASP website."
+            f"please visit the <{OWASP_WEBSITE_URL}/|OWASP website>."
+        ),
     ]
 
     conversation = client.conversations_open(users=command["user_id"])
