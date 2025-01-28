0753a0bf1fe090345c226040cc2c10505c258700
AbhayTopno <143319154+AbhayTopno@users.noreply.github.com>
2025-01-28 13:38:40 +0530

    Update urls.py

--- backend/apps/owasp/api/urls.py
+++ backend/apps/owasp/api/urls.py
@@ -2,7 +2,6 @@
 
 from rest_framework import routers
 
-from apps.github.api.issue import IssueViewSet
 from apps.owasp.api.chapter import ChapterViewSet
 from apps.owasp.api.committee import CommitteeViewSet
 from apps.owasp.api.event import EventViewSet
@@ -12,6 +11,5 @@ router = routers.SimpleRouter()
 
 router.register(r"owasp/chapters", ChapterViewSet)
 router.register(r"owasp/committees", CommitteeViewSet)
-router.register(r"owasp/contribute", IssueViewSet)
 router.register(r"owasp/events", EventViewSet)
 router.register(r"owasp/projects", ProjectViewSet)
