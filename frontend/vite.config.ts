f3da55e7d20829b091c7670a89d4874121954131
Dishant Miyani <dishantmiyani1804@gmail.com>
2025-01-29 00:16:55 +0530

    removed tsconfig paths to make the imports consistent

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import EnvironmentPlugin from 'vite-plugin-environment'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   optimizeDeps: {
@@ -22,7 +21,6 @@ export default defineConfig({
       VITE_GRAPHQL_URL: process.env.VITE_GRAPHQL_URL,
       VITE_SENTRY_DSN: process.env.VITE_SENTRY_DSN || '',
     }),
-    tsconfigPaths(),
   ],
   resolve: {
     alias: {
