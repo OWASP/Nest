c0eb02dac35ca527c7f99aa886ad62ee52b496d7
Harsh Pandey <pandey73harsh@gmail.com>
2025-01-29 15:08:24 +0530

    update placement and icon

--- frontend/src/components/FeedbackFormModal.tsx
+++ frontend/src/components/FeedbackFormModal.tsx
@@ -1,4 +1,7 @@
 import { Button } from '@chakra-ui/react'
+import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
+import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   DialogBody,
   DialogCloseTrigger,
@@ -6,19 +9,31 @@ import {
   DialogRoot,
   DialogTrigger,
 } from 'components/ui/Dialog'
+import { Tooltip } from "components/ui/tooltip"
 import { FeedbackForm } from './FeedbackForm'
 
 export const FeedbackFormModal = () => {
   return (
     <DialogRoot motionPreset="slide-in-bottom" placement="center">
-      <DialogTrigger asChild>
-        <Button size="sm">Feedback</Button>
-      </DialogTrigger>
+      <Tooltip
+        showArrow
+        content="Give Feedback"
+        openDelay={500}
+        closeDelay={100}
+      >
+        <DialogTrigger asChild>
+
+          <Button size="sm" variant={'outline'} colorScheme="blue" className="group flex items-center border">
+            <FontAwesomeIcon icon={faCircleQuestion} className="group-hover:block hidden transition-all ease-linear" />
+            <FontAwesomeIcon icon={faQuestionCircle} className="group-hover:hidden block transition-all ease-linear" />
+          </Button>
+        </DialogTrigger>
+      </Tooltip>
       <DialogContent>
         <DialogBody>
           <FeedbackForm />
         </DialogBody>
-        <DialogCloseTrigger top="0" insetEnd="-12" bg="bg" />
+        <DialogCloseTrigger />
       </DialogContent>
     </DialogRoot>
   )
