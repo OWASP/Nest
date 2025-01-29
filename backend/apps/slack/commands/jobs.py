7d80b75ae84819185505f26bd108d1112f646ef3
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2025-01-29 14:32:42 -0800

    Update code

--- backend/apps/slack/commands/jobs.py
+++ backend/apps/slack/commands/jobs.py
@@ -2,10 +2,10 @@
 
 from django.conf import settings
 
-from apps.common.constants import NL, OWASP_WEBSITE_URL
+from apps.common.constants import NL
 from apps.slack.apps import SlackConfig
 from apps.slack.blocks import markdown
-from apps.slack.constants import OWASP_JOBS_CHANNEL_ID
+from apps.slack.constants import FEEDBACK_CHANNEL_MESSAGE, OWASP_JOBS_CHANNEL_ID
 
 COMMAND = "/jobs"
 
@@ -18,21 +18,16 @@ def jobs_handler(ack, command, client):
         return
 
     blocks = [
-        markdown(f"Please join <{OWASP_JOBS_CHANNEL_ID}> channel{NL}"),
         markdown(
-            "This Slack channel contains community"
-            "-driven posts about job opportunities, "
-            "professional networking, and career advice"
-            " in cybersecurity and related fields. "
-            "\n\n⚠️ *Important Disclaimer:* This is *NOT* an"
-            " official OWASP channel, and its contents "
-            "have *NOT* been endorsed, reviewed, or approved by"
-            " OWASP. The information shared here is "
-            "community-generated and not connected to the "
-            f"<{OWASP_WEBSITE_URL}/careers/|official OWASP Careers page>. "
-            "For official OWASP resources, visit the OWASP website."
-            f"please visit the <{OWASP_WEBSITE_URL}/|OWASP website>."
+            f"Please join <{OWASP_JOBS_CHANNEL_ID}> channel{NL}"
+            "This Slack channel shares community-driven job opportunities, networking, "
+            "and career advice in cybersecurity and related fields."
         ),
+        markdown(
+            "⚠️ *Disclaimer: This is not an official OWASP channel and its content is "
+            "not endorsed, reviewed, or approved by OWASP*."
+        ),
+        markdown(f"{FEEDBACK_CHANNEL_MESSAGE}"),
     ]
 
     conversation = client.conversations_open(users=command["user_id"])
