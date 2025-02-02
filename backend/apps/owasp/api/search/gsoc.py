5df0727fdf34cc7a25f49c96b9949f3bfe2ecdde
Dishant1804 <dishantmiyani1804@gmail.com>
2025-02-02 14:31:07 +0530

    pre-commit and format checks

--- backend/apps/owasp/api/search/gsoc.py
+++ backend/apps/owasp/api/search/gsoc.py
@@ -1,13 +1,15 @@
-"""OWASP app project gsoc search API """
+"""OWASP app project gsoc search API."""
 
 from functools import lru_cache
 
 from algoliasearch_django import raw_search
 
+
 @lru_cache
 def get_gsoc_projects(year, attributes=None):
     """Return GSoC projects from Algolia."""
     from apps.owasp.models.project import Project
+
     query = f"gsoc{year}"
     searchable_attributes = ["idx_custom_tags"]
 
@@ -15,11 +17,9 @@ def get_gsoc_projects(year, attributes=None):
         "attributesToHighlight": [],
         "restrictSearchableAttributes": searchable_attributes,
     }
-    
+
     if attributes:
         params["attributesToRetrieve"] = attributes
 
     results = raw_search(Project, query, params)
-    projects = results.get('hits', [])
-
-    return projects
+    return results.get("hits", [])
