8e29b8f35cf1707977b5ff5dde973ad06ab0b46f
Abhay Mishra <grabhaymishra@gmail.com>
2025-02-02 23:17:21 +0530

    Added license schema in project

--- schema/tests/project_test.py
+++ schema/tests/project_test.py
@@ -2,9 +2,8 @@ from pathlib import Path
 
 import pytest
 import yaml
-from utils.validators import validate_data
-
 from tests.conftest import tests_data_dir
+from utils.validators import validate_data
 
 
 def test_positive(project_schema):
@@ -26,6 +25,12 @@ def test_positive(project_schema):
         ("level-invalid.yaml", "2.5 is not one of [2, 3, 3.5, 4]"),
         ("name-empty.yaml", "'' is too short"),
         ("name-none.yaml", "None is not of type 'string'"),
+        (
+            "license-invalid-spdx.yaml",
+            "'INVALID-LICENSE' is not one of ['Apache-2.0', 'BSD-2-Clause', 'BSD-3-Clause', 'CC-BY-4.0', 'CC-BY-SA-4.0', 'CC0-1.0', 'EUPL-1.2', 'AGPL-3.0', 'GPL-2.0', 'GPL-3.0', 'LGPL-2.1', 'LGPL-3.0', 'MIT', 'MPL-2.0', 'OTHER']",
+        ),
+        ("license-missing-name.yaml", "'name' is a required property"),
+        ("license-missing-spdx.yaml", "'spdx_id' is a required property"),
     ],
 )
 def test_negative(project_schema, file_path, error_message):
