9a448785b81d677c4c2645085516ec30ae1f3c9c
Abhay Topno <abhaytopno999@gmail.com>
2025-02-03 14:25:17 +0000

    Addition of email to leader structure

--- schema/tests/project_test.py
+++ schema/tests/project_test.py
@@ -32,6 +32,9 @@ def test_positive(project_schema):
         ("level-invalid.yaml", "2.5 is not one of [2, 3, 3.5, 4]"),
         ("name-empty.yaml", "'' is too short"),
         ("name-none.yaml", "None is not of type 'string'"),
+        ("email-invalid.yaml", "'invalid-email' is not a 'email'"),
+        ("email-missing-at.yaml", "'userexample.com' is not a 'email'"),
+        ("email-invalid-domain.yaml", "'user@com' is not a 'email'"),
     ],
 )
 def test_negative(project_schema, file_path, error_message):
