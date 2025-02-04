061ad19595d16787f65cb2f894fdbfdb0243c449
AbhayTopno <143319154+AbhayTopno@users.noreply.github.com>
2025-02-04 18:52:01 +0530

    Update project_test.py

--- schema/tests/project_test.py
+++ schema/tests/project_test.py
@@ -58,7 +58,6 @@ def test_positive(project_schema):
         ("sponsors-empty-list.yaml", "[] should be non-empty"),
         ("sponsors-name-missing.yaml", "'name' is a required property"),
         ("sponsors-url-missing.yaml", "'url' is a required property"),
-        ("website-empty.yaml", "'' is too short"),
         ("website-none.yaml", "None is not of type 'string'"),
     ],
 )
