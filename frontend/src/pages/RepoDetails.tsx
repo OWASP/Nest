6f7373e88c3430211a0587a47c5e4805671b5dce
Rajgupta36 <raj.gupta369@outlook.com>
2025-02-05 14:23:27 +0530

    repo_details_page

---
+++ frontend/src/pages/RepoDetails.tsx
@@ -0,0 +1,104 @@
+import { useQuery } from '@apollo/client'
+import {
+  faCodeFork,
+  faExclamationCircle,
+  faHistory,
+  faStar,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons'
+import { GET_REPOSITORY_DATA } from 'api/queries/repositoryQueries'
+import { toast } from 'hooks/useToast'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { formatDate } from 'utils/dateFormatter'
+import { ErrorDisplay } from 'wrappers/ErrorWrapper'
+import DetailsCard from 'components/CardDetailsPage'
+import LoadingSpinner from 'components/LoadingSpinner'
+
+const RepoDetailsPage = () => {
+  const { repositoryKey } = useParams()
+  const [repository, setRepository] = useState(null)
+
+  const {
+    data,
+    loading: isGraphQlDataLoading,
+    error: graphQLRequestError,
+  } = useQuery(GET_REPOSITORY_DATA, {
+    variables: { key: repositoryKey },
+  })
+
+  useEffect(() => {
+    if (data) {
+      setRepository(data.repository || null)
+    }
+    if (graphQLRequestError) {
+      toast({
+        variant: 'destructive',
+        title: 'GraphQL Request Failed',
+        description: 'Unable to complete the requested operation.',
+      })
+    }
+  }, [data, graphQLRequestError])
+
+  if (isGraphQlDataLoading)
+    return (
+      <div className="flex min-h-[60vh] items-center justify-center">
+        <LoadingSpinner imageUrl="/img/owasp_icon_white_sm.png" />
+      </div>
+    )
+
+  if (!repository)
+    return (
+      <ErrorDisplay
+        statusCode={404}
+        title="Repository not found"
+        message="Sorry, the Repository you're looking for doesn't exist"
+      />
+    )
+
+  const repositoryDetails = [
+    {
+      label: 'Last Updated',
+      value: formatDate(repository.updatedAt),
+    },
+    {
+      label: 'Lincese',
+      value: repository.license,
+    },
+    {
+      label: 'Size',
+      value: `${repository.size} KB`,
+    },
+    {
+      label: 'URL',
+      value: (
+        <a href={repository.url} className="hover:underline dark:text-sky-600">
+          {repository.url}
+        </a>
+      ),
+    },
+  ]
+
+  const RepoStats = [
+    { icon: faHistory, value: `${repository?.commitsCount || 'No'} Commits` },
+    { icon: faUsers, value: `${repository?.contributorsCount || 'No'} Contributors` },
+    { icon: faCodeFork, value: `${repository?.forksCount || 'No'} Forks` },
+    { icon: faExclamationCircle, value: `${repository?.openIssuesCount || 'No'} Issues` },
+    { icon: faStar, value: `${repository?.starsCount || 'No'} Stars` },
+  ]
+  return (
+    <DetailsCard
+      title={repository.name}
+      details={repositoryDetails}
+      summary={repository.description}
+      type="repository"
+      stats={RepoStats}
+      topContributors={repository.topContributors}
+      languages={repository.languages}
+      topics={repository.topics}
+      recentIssues={repository.issues}
+      recentReleases={repository.releases}
+    />
+  )
+}
+export default RepoDetailsPage
