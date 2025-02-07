a902fc654f6a3bd18d665eaaf1c70599e389f725
yashgoyal0110 <yg364550@gmail.com>
2025-02-08 00:47:58 +0530

    uri validatn

--- schema/utils/validators.py
+++ schema/utils/validators.py
@@ -1,9 +1,16 @@
-from jsonschema import validate
+from jsonschema import validate, FormatChecker
 from jsonschema.exceptions import ValidationError
-
+from utils.helpers import is_valid_uri
 
 def validate_data(schema, data):
+    """Validate data against schema."""
+    format_checker = FormatChecker()
+    
+    @format_checker.checks('uri')
+    def check_uri_format(value):
+        return is_valid_uri(value)
+    
     try:
-        validate(schema=schema, instance=data)
+        validate(schema=schema, instance=data, format_checker=format_checker)
     except ValidationError as e:
-        return e.message
+        return e.message 
