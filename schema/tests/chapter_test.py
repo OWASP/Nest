3c813e9d8081e66ffc9bf2b3e4d0b6da08bd1f67
Oluwatobi Olajide <tobiolajide01@gmail.com>
2025-02-11 19:39:27 +0000

    fixedformatting

--- schema/tests/chapter_test.py
+++ schema/tests/chapter_test.py
@@ -6,6 +6,7 @@ from utils.schema_validators import validate_data
 
 from tests.conftest import tests_data_dir
 
+
 def test_positive(chapter_schema):
     for file_path in Path(tests_data_dir / "chapter/positive").rglob("*.yaml"):
         assert (
@@ -17,6 +18,7 @@ def test_positive(chapter_schema):
             )
             is None
         )
+
 
 @pytest.mark.parametrize(
     ("file_path", "error_message"),
@@ -49,7 +51,6 @@ def test_positive(chapter_schema):
         ("website-null.yaml", "None is not of type 'string'"),
     ],
 )
-
 def test_negative(chapter_schema, file_path, error_message):
     assert (
         validate_data(
--- schema/utils/schema_validators.py
+++ schema/utils/schema_validators.py
@@ -2,6 +2,7 @@ from jsonschema import validate, FormatChecker
 from jsonschema.exceptions import ValidationError
 import validators
 
+
 def validate_data(schema, data):
     """Validate data against schema."""
     format_checker = FormatChecker()
@@ -9,15 +10,12 @@ def validate_data(schema, data):
     @format_checker.checks("uri")
     def check_uri_format(value):
         return validators.url(value)
-    
+
     @format_checker.checks("email")
     def check_email_format(value):
         return validators.email(value)
-
 
     try:
         validate(schema=schema, instance=data, format_checker=format_checker)
     except ValidationError as e:
         return e.message
-
-
