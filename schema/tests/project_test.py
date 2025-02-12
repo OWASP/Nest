b7c58425527db22d79a9e8af07196384e6810310
shining-bluemoon-11 <sneha08115@gmail.com>
2025-02-12 15:02:03 +0530

    update fix

--- schema/tests/project_test.py
+++ schema/tests/project_test.py
@@ -35,8 +35,14 @@ def test_positive(project_schema):
         ("demo-invalid.yaml", "'https://invalid/' is not a 'uri'"),
         ("demo-null.yaml", "None is not a 'uri'"),
         ("documentation-empty.yaml", "[] should be non-empty"),
-        ("documentation-invalid.yaml", "'xyz-abc' is not a 'uri'",),
-        ("documentation-non-unique.yaml", " ['https://example.com/docs' , 'https://example.com/docs'] has non-unique elements"),
+        (
+            "documentation-invalid.yaml",
+            "'xyz-abc' is not a 'uri'",
+        ),
+        (
+            "documentation-non-unique.yaml",
+            " ['https://example.com/docs' , 'https://example.com/docs'] has non-unique elements",
+        ),
         ("downloads-empty.yaml", "[] should be non-empty"),
         (
             "downloads-invalid.yaml",
