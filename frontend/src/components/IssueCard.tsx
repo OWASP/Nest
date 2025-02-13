03e267d3d03f060250ea5533c251961fee7c92ab
moodyadi2006 <moodyadi30@gmail.com>
2025-02-13 23:20:52 +0530

    Add Feature : Improved Contribution Guide Formatting 🚀

--- frontend/src/components/IssueCard.tsx
+++ frontend/src/components/IssueCard.tsx
@@ -1,6 +1,5 @@
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Link } from 'react-router-dom'
 import { Issue } from 'types/user'
 
 export function IssueCard({ issue }: { issue: Issue }) {
@@ -8,14 +7,14 @@ export function IssueCard({ issue }: { issue: Issue }) {
     <div className="cursor-pointer rounded-lg border border-gray-300 p-4 transition-colors hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-800">
       <div className="flex justify-between">
         <div>
-          <Link
-            to={`https://github.com/${issue.repository.ownerKey}/${issue.repository.key}/issues/${issue.number}`}
+          <a
+            href={`https://github.com/${issue.repository.ownerKey}/${issue.repository.key}/issues/${issue.number}`}
             target="_blank"
             rel="noopener noreferrer"
-            className="font-bold text-black underline decoration-dotted dark:text-white"
+            className="font-bold text-black underline decoration-dotted dark:text-white hover:cursor-pointer"
           >
             {issue.title}
-          </Link>
+          </a>
 
           <div className="mt-1 flex items-center gap-2 text-gray-600 dark:text-gray-400">
             <span className="rounded-full bg-gray-200 px-2 py-1 text-sm dark:bg-gray-700">
