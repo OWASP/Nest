6d558a7ef4e98d052a6f2d8567cc59245ac393d6
shining-bluemoon-11 <sneha08115@gmail.com>
2025-02-15 00:34:27 +0530

    update fix

--- schema/tests/project_test.py
+++ schema/tests/project_test.py
@@ -33,7 +33,10 @@ def test_positive(project_schema):
         ("blog-invalid.yaml", "'https://invalid/' is not a 'uri'"),
         ("blog-null.yaml", "None is not a 'uri'"),
         ("community-empty.yaml", "[] should be non-empty"),
-        ("community-invalid.yaml", "Additional properties are not allowed ('description' was unexpected)"),
+        (
+            "community-invalid.yaml",
+            "Additional properties are not allowed ('description' was unexpected)",
+        ),
         ("community-null.yaml", "None is not of type 'array'"),
         ("demo-invalid.yaml", "'https://invalid/' is not a 'uri'"),
         ("demo-null.yaml", "None is not a 'uri'"),
