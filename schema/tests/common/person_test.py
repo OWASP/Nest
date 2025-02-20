0145b064631e5f8c77b518f987697cec57b3291d
AbhayTopno <143319154+AbhayTopno@users.noreply.github.com>
2025-02-20 20:23:57 +0530

    Update person_test.py

--- schema/tests/common/person_test.py
+++ schema/tests/common/person_test.py
@@ -8,12 +8,12 @@ from tests.conftest import common_negative_test, common_positive_test
 @pytest.mark.parametrize(
     ("file_path", "error_message"),
     [
+        ("email-empty.yaml", "'' is not an 'email'"),
+        ("email-null.yaml", "None is not of type 'string'"),
         ("github-empty.yaml", "'' does not match '^[a-zA-Z0-9-]{1,39}$'"),
         ("github-invalid.yaml", "'user@name' does not match '^[a-zA-Z0-9-]{1,39}$'"),
         ("github-null.yaml", "None is not of type 'string'"),
         ("github-undefined.yaml", "'github' is a required property"),
-        ("email-empty.yaml", "'' is not an 'email'"),
-        ("email-null.yaml", "None is not of type 'string'"),
         ("name-empty.yaml", "'' is not an 'name'"),
         ("name-null.yaml", "None is not of type 'string'"),
         ("slack-empty.yaml", "'' does not match '^[a-zA-Z0-9._-]{1,21}$'"),
