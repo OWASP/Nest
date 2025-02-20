8512e779a38da6eab17994bef9c6407cddbd5ab6
Akash Jaiswal <akashjaiswal3846@gmail.com>
2025-02-20 22:38:56 +0530

    readme: centered icons, improved design and fixed build status

--- schema/tests/common/social_media_test.py
+++ schema/tests/common/social_media_test.py
@@ -1,6 +1,10 @@
+"""Social media schema tests."""
+
 import pytest
 
 from tests.conftest import common_negative_test, common_positive_test
+
+SCHEMA_NAME = "social_media"
 
 
 @pytest.mark.parametrize(
@@ -26,8 +30,8 @@ from tests.conftest import common_negative_test, common_positive_test
     ],
 )
 def test_negative(common_schema, file_path, error_message):
-    common_negative_test(common_schema, "social-media", file_path, error_message)
+    common_negative_test(common_schema, SCHEMA_NAME, file_path, error_message)
 
 
 def test_positive(common_schema):
-    common_positive_test(common_schema, "social-media")
+    common_positive_test(common_schema, SCHEMA_NAME)
