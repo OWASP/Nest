eced19f9d6d601a49e4a823469a254a290885253
AbhayTopno <143319154+AbhayTopno@users.noreply.github.com>
2025-02-20 13:57:10 +0530

    Update chapter_test.py

--- schema/tests/chapter_test.py
+++ schema/tests/chapter_test.py
@@ -10,10 +10,16 @@ from utils.schema_validators import validate_data
 
 
 def test_positive(chapter_schema):
-    """Test valid chapter schema cases."""
     for file_path in Path(tests_data_dir / "chapter/positive").rglob("*.yaml"):
-        yaml_data = yaml.safe_load(file_path.read_text())
-        assert validate_data(chapter_schema, yaml_data) is None
+        assert (
+            validate_data(
+                chapter_schema,
+                yaml.safe_load(
+                    file_path.read_text(),
+                ),
+            )
+            is None
+        )
 
 
 @pytest.mark.parametrize(
@@ -22,13 +28,22 @@ def test_positive(chapter_schema):
         ("blog-invalid.yaml", "'invalid-blog-uri' is not a 'uri'"),
         ("blog-null.yaml", "None is not a 'uri'"),
         ("events-empty.yaml", "[] should be non-empty"),
-        ("events-non-unique.yaml", "events are non-unique"),
+        (
+            "events-non-unique.yaml",
+            "['https://example.com/event1', 'https://example.com/event1'] has non-unique elements",
+        ),
         ("name-empty.yaml", "'' is too short"),
         ("name-none.yaml", "None is not of type 'string'"),
         ("website-null.yaml", "None is not of type 'string'"),
     ],
 )
 def test_negative(chapter_schema, file_path, error_message):
-    """Test invalid chapter schema cases."""
-    yaml_data = yaml.safe_load(Path(tests_data_dir / "chapter/negative" / file_path).read_text())
-    assert validate_data(chapter_schema, yaml_data) == error_message
+    assert (
+        validate_data(
+            chapter_schema,
+            yaml.safe_load(
+                Path(tests_data_dir / "chapter/negative" / file_path).read_text(),
+            ),
+        )
+        == error_message
+    )
