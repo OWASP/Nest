1362d176bc7139dab17ba84122bbd2061f9c9aca
AbhayTopno <143319154+AbhayTopno@users.noreply.github.com>
2025-02-20 13:55:24 +0530

    Update project_test.py

--- schema/tests/project_test.py
+++ schema/tests/project_test.py
@@ -1,4 +1,4 @@
-"""Test cases for the project schema validation."""
+"""Project schema tests."""
 
 from pathlib import Path
 
@@ -10,7 +10,6 @@ from utils.schema_validators import validate_data
 
 
 def test_positive(project_schema):
-    """Test valid cases for the project schema."""
     for file_path in Path(tests_data_dir / "project/positive").rglob("*.yaml"):
         assert (
             validate_data(
@@ -26,27 +25,52 @@ def test_positive(project_schema):
 @pytest.mark.parametrize(
     ("file_path", "error_message"),
     [
-        ("audience-invalid.yaml", "'hacker' is not one of ['breaker', 'builder', 'defender']"),
+        (
+            "audience-invalid.yaml",
+            "'hacker' is not one of ['breaker', 'builder', 'defender']",
+        ),
         ("audience-empty.yaml", "'' is not one of ['breaker', 'builder', 'defender']"),
         ("audience-null.yaml", "None is not one of ['breaker', 'builder', 'defender']"),
         ("audience-undefined.yaml", "'audience' is a required property"),
         ("blog-invalid.yaml", "'https://invalid/' is not a 'uri'"),
         ("blog-null.yaml", "None is not a 'uri'"),
+        ("demo-empty.yaml", "[] should be non-empty"),
         ("demo-invalid.yaml", "'https://invalid/' is not a 'uri'"),
-        ("demo-null.yaml", "None is not a 'uri'"),
+        (
+            "demo-non-unique.yaml",
+            "['https://example.com/', 'https://example.com/'] has non-unique elements",
+        ),
+        ("demo-null.yaml", "None is not of type 'array'"),
         ("documentation-empty.yaml", "[] should be non-empty"),
-        ("documentation-invalid.yaml", "'xyz-abc' is not a 'uri'"),
+        (
+            "documentation-invalid.yaml",
+            "'xyz-abc' is not a 'uri'",
+        ),
         ("documentation-null.yaml", "None is not of type 'array'"),
         ("downloads-empty.yaml", "[] should be non-empty"),
-        ("downloads-invalid.yaml", "'xyz-abc' is not a 'uri'"),
-        ("downloads-non-unique.yaml", "has non-unique elements"),
+        (
+            "downloads-invalid.yaml",
+            "'xyz-abc' is not a 'uri'",
+        ),
+        (
+            "downloads-non-unique.yaml",
+            "['https://abc.com/download', 'https://abc.com/download'] has non-unique elements",
+        ),
         ("downloads-null.yaml", "None is not of type 'array'"),
         ("events-empty.yaml", "[] should be non-empty"),
-        ("events-non-unique.yaml", "has non-unique elements"),
+        (
+            "events-non-unique.yaml",
+            "['https://example.com/event1', 'https://example.com/event1'] has non-unique elements",
+        ),
         ("events-invalid.yaml", "'xyz-abc' is not a 'uri'"),
         ("events-null.yaml", "None is not of type 'array'"),
         ("level-invalid.yaml", "2.5 is not one of [2, 3, 3.5, 4]"),
-        ("license-invalid-value.yaml", "'INVALID-LICENSE-VALUE' is not valid license value"),
+        (
+            "license-invalid-value.yaml",
+            "'INVALID-LICENSE-VALUE' is not one of ['AGPL-3.0', 'Apache-2.0', 'BSD-2-Clause', "
+            "'BSD-3-Clause', 'CC-BY-4.0', 'CC-BY-SA-4.0', 'CC0-1.0', 'EUPL-1.2', 'GPL-2.0', "
+            "'GPL-3.0', 'LGPL-2.1', 'LGPL-3.0', 'MIT', 'MPL-2.0', 'OTHER']",
+        ),
         ("mailing-list-empty.yaml", "'' is not a 'uri'"),
         ("mailing-list-invalid.yaml", "'https://xyz' is not a 'uri'"),
         ("mailing-list-null.yaml", "None is not a 'uri'"),
@@ -57,7 +81,6 @@ def test_positive(project_schema):
     ],
 )
 def test_negative(project_schema, file_path, error_message):
-    """Test invalid cases for the project schema."""
     assert (
         validate_data(
             project_schema,
