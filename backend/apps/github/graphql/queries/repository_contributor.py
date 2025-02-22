9dd10355382ece63daa0908abe8577534bc0581f
Abhay <grabhaymishra@gmail.com>
2025-02-23 02:00:26 +0530

    Added main page (#885)
    * Main Page Addded
    
    * updated code
    
    * Added searchbar
    
    * fixed minor bugs
    
    * Added improvements
    
    * Added testcase
    
    * spell check
    
    * fixed some queries
    
    * fixed test
    
    * removed comments
    
    * removed owasp foundation login
    
    * Update frontend/src/pages/Home.tsx
    
    Co-authored-by: Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>
    
    * Added required changes
    
    * pre-commit
    
    * added user suggestion improved types
    
    * added function for logo and fix project type
    
    * testcase fixed
    
    * added default fafilecode
    
    * added close on esc button press
    
    * added a call to action button
    
    ---------
    
    Co-authored-by: Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>

---
+++ backend/apps/github/graphql/queries/repository_contributor.py
@@ -0,0 +1,47 @@
+"""OWASP repository contributor GraphQL queries."""
+
+import graphene
+from django.db.models import Sum
+
+from apps.common.graphql.queries import BaseQuery
+from apps.github.constants import OWASP_FOUNDATION_LOGIN
+from apps.github.graphql.nodes.repository_contributor import RepositoryContributorNode
+from apps.github.models.organization import Organization
+from apps.github.models.repository_contributor import RepositoryContributor
+
+
+class RepositoryContributorQuery(BaseQuery):
+    """Repository contributor queries."""
+
+    top_contributors = graphene.List(
+        RepositoryContributorNode, limit=graphene.Int(default_value=15)
+    )
+
+    def resolve_top_contributors(root, info, limit):
+        """Resolve top contributors."""
+        non_indexable_logins = ["ghost", OWASP_FOUNDATION_LOGIN, *list(Organization.get_logins())]
+
+        top_contributors_data = (
+            RepositoryContributor.objects.exclude(user__login__in=non_indexable_logins)
+            .values(
+                "user__avatar_url",
+                "user__login",
+                "user__name",
+                "repository__name",
+                "repository__owner__login",
+            )
+            .annotate(total_contributions=Sum("contributions_count"))
+            .order_by("-total_contributions")[:limit]
+        )
+
+        return [
+            RepositoryContributorNode(
+                avatar_url=contrib["user__avatar_url"],
+                contributions_count=contrib["total_contributions"],
+                login=contrib["user__login"],
+                name=contrib["user__name"],
+                repository_name=contrib["repository__name"],
+                repository_url=f"https://github.com/{contrib['repository__owner__login']}/{contrib['repository__name']}",
+            )
+            for contrib in top_contributors_data
+        ]
