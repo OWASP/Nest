023a2c2e2f6e7d4f776cfd38a22f5877a0d60c74
yashgoyal0110 <yg364550@gmail.com>
2025-03-03 01:04:01 +0530

    Commit

--- backend/manage.py
+++ backend/manage.py
@@ -6,8 +6,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
-    os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.getenv("DJANGO_SETTINGS_MODULE", "settings.test"))
+    os.environ.setdefault("DJANGO_CONFIGURATION", os.getenv("DJANGO_CONFIGURATION", "Test"))
 
     from configurations.management import execute_from_command_line
 
