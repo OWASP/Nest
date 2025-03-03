7bbeaa17fd2d3f485d290085ef4639d701d27e23
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2025-03-03 09:42:03 -0800

    Update code

--- frontend/src/utils/sortingOptions.ts
+++ frontend/src/utils/sortingOptions.ts
@@ -5,8 +5,8 @@ export const sortOptionsProject = createListCollection({
     { label: 'Relevancy', value: 'default' },
     { label: 'Contributors', value: 'contributors_count' },
     { label: 'Forks', value: 'forks_count' },
+    { label: 'Last Updated', value: 'updated_at' },
     { label: 'Name', value: 'name' },
     { label: 'Stars', value: 'stars_count' },
-    { label: 'Last Updated', value: 'updated_at' },
   ],
 })
