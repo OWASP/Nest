c379fad6b467cadc318b7ac67174d5ce14f48a6e
Tamara Lazerka <lazerkatamara@gmail.com>
2025-03-03 20:34:09 -0800

    remve import react

--- frontend/src/components/HeatMap.tsx
+++ frontend/src/components/HeatMap.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchHeatmapData, drawContributions, HeatmapData } from 'utils/helpers/githubHeatmap'
 
