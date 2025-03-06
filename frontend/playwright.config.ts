ef0888d7776a2c76a7b5c4999cc648dd7ceadb02
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2025-03-05 17:58:56 -0800

    Update e2e tests config

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,32 +1,23 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   fullyParallel: true,
-  reporter: [['list', { printSteps: true }]],
-  testDir: './__tests__/e2e',
-
-  use: {
-    baseURL: 'http://localhost:3000',
-    trace: 'off',
-  },
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
     },
   ],
+  reporter: [['list', { printSteps: true }]],
+  testDir: './__tests__/e2e',
+  use: {
+    baseURL: 'http://localhost:3000',
+    headless: true,
+    trace: 'off',
+  },
   webServer: {
     command: 'npm run dev',
     url: 'http://localhost:3000',
   },
+  workers: process.env.GITHUB_ACTIONS ? 1 : undefined, // Use 1 worker on GitHub
 })
