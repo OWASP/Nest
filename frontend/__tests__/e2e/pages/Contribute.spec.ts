b8e70d70081d01e4b04e9c7978f67345e32db12a
Karan Negi <142792314+KaranNegi20Feb@users.noreply.github.com>
2025-03-12 02:45:16 +0530

    more cases added

--- frontend/__tests__/e2e/pages/Contribute.spec.ts
+++ frontend/__tests__/e2e/pages/Contribute.spec.ts
@@ -17,9 +17,7 @@ test.describe('Contribute Page', () => {
 
   test('renders issue data correctly', async ({ page }) => {
     await expect(page.getByRole('link', { name: 'Contribution 1' })).toBeVisible()
-    await expect(
-      page.locator('text=/\\d+ months? ago|a month ago|a day ago|\\d+ days? ago/')
-    ).toBeVisible()
+    await expect(page.locator('text=/\\b(\\d+|a) (day|week|month|year)s? ago\\b/')).toBeVisible()
     await expect(page.getByRole('link', { name: 'Owasp Nest' })).toBeVisible()
     await expect(page.getByText('This is a summary of Contribution 1')).toBeVisible()
     await expect(page.getByRole('button', { name: 'Read More' })).toBeVisible()
