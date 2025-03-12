11633899fba50a46af223d1c2b21702623a8db16
Dishant1804 <dishantmiyani1804@gmail.com>
2025-03-12 12:47:45 +0530

    checks and lint

--- backend/apps/owasp/migrations/0026_post.py
+++ backend/apps/owasp/migrations/0026_post.py
@@ -4,27 +4,34 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('owasp', '0025_remove_event_country_remove_event_postal_code_and_more'),
+        ("owasp", "0025_remove_event_country_remove_event_postal_code_and_more"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Post',
+            name="Post",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nest_created_at', models.DateTimeField(auto_now_add=True)),
-                ('nest_updated_at', models.DateTimeField(auto_now=True)),
-                ('author_name', models.CharField(max_length=100, verbose_name='Author name')),
-                ('author_image_url', models.URLField(blank=True, default='', verbose_name='Author image URL')),
-                ('published_at', models.DateTimeField(verbose_name='Publication date')),
-                ('title', models.CharField(max_length=200, verbose_name='Title')),
-                ('url', models.URLField(verbose_name='URL')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("nest_created_at", models.DateTimeField(auto_now_add=True)),
+                ("nest_updated_at", models.DateTimeField(auto_now=True)),
+                ("author_name", models.CharField(max_length=100, verbose_name="Author name")),
+                (
+                    "author_image_url",
+                    models.URLField(blank=True, default="", verbose_name="Author image URL"),
+                ),
+                ("published_at", models.DateTimeField(verbose_name="Publication date")),
+                ("title", models.CharField(max_length=200, verbose_name="Title")),
+                ("url", models.URLField(verbose_name="URL")),
             ],
             options={
-                'verbose_name_plural': 'Posts',
-                'db_table': 'owasp_posts',
+                "verbose_name_plural": "Posts",
+                "db_table": "owasp_posts",
             },
         ),
     ]
