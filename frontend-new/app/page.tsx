a70b4234e646f6aa1663d105f70c17d8bd2eec0b
Rajgupta36 <raj.gupta369@outlook.com>
2025-03-16 19:25:44 +0530

    tailwind fix

--- frontend-new/app/page.tsx
+++ frontend-new/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="just flex h-screen w-screen items-center justify-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      Hii
+      <p className="mt-12 text-9xl">hii </p>
     </div>
   )
 }
