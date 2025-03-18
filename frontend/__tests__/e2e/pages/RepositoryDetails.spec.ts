436000642673b5d9047d757a2cbb9180f50a83cf
Karan Negi <142792314+KaranNegi20Feb@users.noreply.github.com>
2025-03-18 07:38:00 +0530

    changes made fix sonar duplicates

--- frontend/__tests__/e2e/pages/RepositoryDetails.spec.ts
+++ frontend/__tests__/e2e/pages/RepositoryDetails.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test'
 import { mockRepositoryData } from '@unit/data/mockRepositoryData'
-import { expectContributorVisible } from '../utils/contributor-utils';
-
 
 test.describe('Repository Details Page', () => {
   test.beforeEach(async ({ page }) => {
@@ -48,8 +46,12 @@ test.describe('Repository Details Page', () => {
   })
   test('should have top contributors', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Top Contributors' })).toBeVisible()
-    await expectContributorVisible(page, 'Contributor 1', '30 Contributions');
-    await expectContributorVisible(page, 'Contributor 2', '29 Contributions');
+    await expect(page.getByRole('img', { name: 'Contributor 1' })).toBeVisible()
+    await expect(page.getByText('Contributor 1')).toBeVisible()
+    await expect(page.getByText('30 Contributions')).toBeVisible()
+    await expect(page.getByRole('img', { name: 'Contributor 2' })).toBeVisible()
+    await expect(page.getByText('Contributor 2')).toBeVisible()
+    await expect(page.getByText('29 Contributions')).toBeVisible()
   })
 
   test('toggle top contributors', async ({ page }) => {
