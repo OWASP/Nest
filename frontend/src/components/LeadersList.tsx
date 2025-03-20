81143f01cf2ac2ff5231b2f6e58ed4dc51949c8f
nipunh <nipunhedaoo@gmail.com>
2025-03-19 23:26:02 -0400

    code quality

--- frontend/src/components/LeadersList.tsx
+++ frontend/src/components/LeadersList.tsx
@@ -3,23 +3,20 @@ import { Link } from 'react-router-dom'
 import { LeadersListProps } from 'types/leaders'
 
 const LeadersList = ({ leaders }: LeadersListProps) => {
+  if (!leaders || leaders.trim() === '') return <>Unknown</>
 
-    if (!leaders || leaders.trim() === "") return <>Unknown</>;
+  const leadersArray = leaders.split(',').map((leader) => leader.trim())
 
-    const leadersArray = leaders.split(',').map((leader) => leader.trim());
-  
-    return (
-      <>
-        {leadersArray.map((leader, index) => (
-          <span key={leader}>
-            <Link to={`/community/users?q=${encodeURIComponent(leader)}`}>
-              {leader}
-            </Link>
-            {index < leadersArray.length - 1 && ", "}
-          </span>
-        ))}
-      </>
-    );
+  return (
+    <>
+      {leadersArray.map((leader, index) => (
+        <span key={leader}>
+          <Link to={`/community/users?q=${encodeURIComponent(leader)}`}>{leader}</Link>
+          {index < leadersArray.length - 1 && ', '}
+        </span>
+      ))}
+    </>
+  )
 }
 
 export default LeadersList
