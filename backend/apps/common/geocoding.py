cd2ea8b476ce286567037350369a922fcbf8bc18
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2025-03-30 19:44:21 -0700

    Update code

--- backend/apps/common/geocoding.py
+++ backend/apps/common/geocoding.py
@@ -11,12 +11,10 @@ def get_location_coordinates(query, delay=2):
     """Get location geo coordinates.
 
     Args:
-    ----
         query (str): The location query string.
         delay (int, optional): Delay in seconds before making the request.
 
     Returns:
-    -------
         Location|None: The geopy Location object or None if not found.
 
     """
