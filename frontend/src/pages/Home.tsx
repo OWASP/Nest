b10a2f710be7f11f946226d973180459b64c2ee3
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2025-03-31 09:37:26 -0700

    Update code

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -201,7 +201,7 @@ export default function Home() {
 
                   {chapter.leaders.length > 0 && (
                     <div className="mr-4 mt-1 flex items-center gap-x-2 text-sm [&_a]:text-gray-600 dark:[&_a]:text-gray-400">
-                      <FontAwesomeIcon icon={faUsers} className="mr-2 h-4 w-4" />
+                      <FontAwesomeIcon icon={faUsers} className="h-4 w-4" />
                       <LeadersList leaders={String(chapter.leaders)} />
                     </div>
                   )}
@@ -234,7 +234,7 @@ export default function Home() {
 
                   {project.leaders.length > 0 && (
                     <div className="mr-4 mt-1 flex items-center gap-x-2 text-sm [&_a]:text-gray-600 dark:[&_a]:text-gray-400">
-                      <FontAwesomeIcon icon={faUsers} className="mr-2 h-4 w-4" />
+                      <FontAwesomeIcon icon={faUsers} className="h-4 w-4" />
                       <LeadersList leaders={String(project.leaders)} />
                     </div>
                   )}
