64bac4306a9d6f768a9dc49d9385979fa5ed785b
Abhay <grabhaymishra@gmail.com>
2025-04-02 05:00:23 +0530

    Secured the idx/graphQL endpoints by csrf (#1255)
    * Secured the idx/graphQL endpoints by csrf
    
    * Update code
    
    * Added testcases
    
    * fixcodeQL
    
    * Update code
    
    ---------
    
    Co-authored-by: Arkadii Yakovets <arkadii.yakovets@owasp.org>

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -18,6 +18,7 @@ class Base(Configuration):
         load_dotenv(BASE_DIR / ".env.example")
 
     ALLOWED_HOSTS = values.ListValue()
+    CORS_ALLOW_CREDENTIALS = True
     DEBUG = False
     RELEASE_VERSION = values.Value(environ_name="RELEASE_VERSION")
     SENTRY_DSN = values.SecretValue(environ_name="SENTRY_DSN")
