5f79854f7e43a2e17e30f676fc22caea8230ecc3
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-04-04 07:13:49 +0200

    Refactor fuzz configuration by reorganizing imports and cleaning up whitespace

--- backend/settings/fuzz.py
+++ backend/settings/fuzz.py
@@ -1,22 +1,23 @@
 """OWASP Nest Fuzz configuration."""
 
+from configurations import values
+
 from settings.base import Base
 
-from configurations import values
 
 class Fuzz(Base):
     """Fuzz configuration."""
+
     BASE_DIR = Base.BASE_DIR
     DEBUG = False
     APP_NAME = "OWASP Nest Fuzz"
-    
+
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-    
+
     IP_ADDRESS = values.Value()
     ALLOWED_HOSTS = ["*"]
-    
