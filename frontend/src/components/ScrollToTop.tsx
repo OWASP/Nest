5873a102ffb0f244c5066b0d5c6f9e67513c5b56
Abhay Mishra <grabhaymishra@gmail.com>
2025-04-07 04:49:48 +0530

    added useclient

--- frontend/src/components/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState, useEffect, useCallback } from 'react'
