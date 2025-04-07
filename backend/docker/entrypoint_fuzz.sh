ccc28c8007f72453cf3123c8f3c65a0619aae03a
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-04-07 18:01:28 +0200

    Load data from nest.json and add graphqler to cspell dict.

--- backend/docker/entrypoint_fuzz.sh
+++ backend/docker/entrypoint_fuzz.sh
@@ -5,6 +5,9 @@ set -e
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+# Load initial data
+python manage.py load_data
+
 # Added true temporarily to allow the script to continue when the test coverage fails
 pytest fuzz_tests || true
 
