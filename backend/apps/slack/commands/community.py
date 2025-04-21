aafa78ab6eb98ae77f8b91438431edd628ae2684
L. C. <146425272+Li-ChangC@users.noreply.github.com>
2025-04-21 08:33:15 -0700

    Feature: NestBot block message template system (#1301)
    * feat: Add jinja2 and markupsafe dependencies to project
    
    * feat: Add Jinja2 templates for Slack commands and implement template rendering
    
    * refactor: inherit from base command and clean up deprecated files
    
    * refactor: streamline command configuration and enhance command retrieval
    
    * feat: implement template system to NestBot and update unit tests
    
    * refactor code as suggested
    
    * Fix code as suggested; remove files mistakenly added in last commit
    
    * fix: update community URL in template context
    
    * fix: improve template loading error handling and command registration
    
    * fix: enhance error logging for template loading failures
    
    * refactor: streamline template context retrieval in command classes
    
    * Update code
    
    * Update code
    
    ---------
    
    Co-authored-by: Arkadii Yakovets <arkadii.yakovets@owasp.org>
    Co-authored-by: Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>

--- backend/apps/slack/commands/community.py
+++ backend/apps/slack/commands/community.py
@@ -1,40 +1,20 @@
 """Slack bot community command."""
 
-from django.conf import settings
-
-from apps.common.constants import NL
-from apps.slack.apps import SlackConfig
-from apps.slack.blocks import markdown
-from apps.slack.utils import get_text
-
-COMMAND = "/community"
+from apps.common.utils import get_absolute_url
+from apps.slack.commands.command import CommandBase
 
 
-def community_handler(ack, command, client):
-    """Handle the Slack /community command.
+class Community(CommandBase):
+    """Slack bot /community command."""
 
-    Args:
-        ack (function): Acknowledge the Slack command request.
-        command (dict): The Slack command payload.
-        client (slack_sdk.WebClient): The Slack WebClient instance for API calls.
+    def get_template_file_name(self):
+        """Get the template file name."""
+        return "navigate.template"
 
-    """
-    ack()
-
-    if not settings.SLACK_COMMANDS_ENABLED:
-        return
-
-    blocks = [
-        markdown(f"Please visit <https://nest.owasp.dev/members/|OWASP community> page{NL}"),
-    ]
-
-    conversation = client.conversations_open(users=command["user_id"])
-    client.chat_postMessage(
-        blocks=blocks,
-        channel=conversation["channel"]["id"],
-        text=get_text(blocks),
-    )
-
-
-if SlackConfig.app:
-    community_handler = SlackConfig.app.command(COMMAND)(community_handler)
+    def get_template_context(self, command):
+        """Get the template context."""
+        return {
+            **super().get_template_context(command),
+            "name": "OWASP community",
+            "url": get_absolute_url("members"),
+        }
