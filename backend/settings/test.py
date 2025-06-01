73df65dac77ee514563675d69ee72263ffcab547
Bandhan Majumder <133476557+bandhan-majumder@users.noreply.github.com>
2025-06-02 00:17:14 +0530

    Add test: test for clearing cache (#1522)
    * Add test: test for clearing cache
    
    Signed-off-by: bandhan-majumder <bandhanmajumder16@gmail.com>
    
    * rabbit suggestions
    
    Signed-off-by: bandhan-majumder <bandhanmajumder16@gmail.com>
    
    * make check applied
    
    * update into class
    
    * Update code
    
    * Apply suggestions
    
    ---------
    
    Signed-off-by: bandhan-majumder <bandhanmajumder16@gmail.com>
    Co-authored-by: Arkadii Yakovets <arkadii.yakovets@owasp.org>

--- backend/settings/test.py
+++ backend/settings/test.py
@@ -6,4 +6,10 @@ from settings.base import Base
 class Test(Base):
     """Test configuration."""
 
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+            "LOCATION": "test-cache",
+        },
+    }
     DEBUG = False
