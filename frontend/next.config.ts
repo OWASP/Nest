ceb01c5799e06ff7a506219b0abb9c8d7740ec2e
rishyym0297 <rishyym0927@gmail.com>
2025-06-08 03:21:16 +0530

    Fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,8 @@ import type { NextConfig } from 'next'
 const isLocal = process.env.NEXT_PUBLIC_ENVIRONMENT === 'local'
 
 const nextConfig: NextConfig = {
+   productionBrowserSourceMaps: true, // Enable source maps for production
+  // ...existing config...
   devIndicators: false,
   images: {
     // This is a list of remote patterns that Next.js will use to determine
