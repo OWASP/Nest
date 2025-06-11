9205597251f66db502e1398c812e00db7d9061a7
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-06-11 19:32:02 +0300

    Update filtering and add fields to models

--- backend/apps/owasp/graphql/queries/project_health_metrics.py
+++ backend/apps/owasp/graphql/queries/project_health_metrics.py
@@ -1,18 +1,12 @@
 """OWASP Project Health Metrics Queries."""
 
 import strawberry
-from django.utils import timezone
 
 from apps.owasp.graphql.nodes.project_health_metrics import ProjectHealthMetricsNode
 from apps.owasp.models.project_health_metrics import ProjectHealthMetrics
 
 # It is stated in issue #711
 CONTRIBUTORS_COUNT_REQUIREMENT = 2
-
-# These numbers are just for testing purposes, they can be adjusted later
-OPEN_ISSUES_LIMIT = 40
-LONG_UNANSWERED_ISSUES_LIMIT = 30
-LONG_UNASSIGNED_ISSUES_LIMIT = 30
 
 
 @strawberry.type
@@ -23,62 +17,66 @@ class ProjectHealthMetricsQuery:
     def unhealthy_projects(
         self,
         *,
-        contributors_count_requirement_compliant: bool = True,
-        funding_requirement_compliant: bool = True,
+        contributors_count_requirement_compliant: bool = False,
+        # Set the default of the `funding_requirement_compliant`
+        # `leaders_requirement_compliant`, `long_open_issues`, `long_unanswered_issues`,
+        # and `long_unassigned_issues` parameters to None,
+        # to allow retrieving projects with or without these requirements
+        funding_requirement_compliant: bool | None = None,
         no_recent_commits: bool = False,
         no_recent_releases: bool = False,
-        leaders_requirement_compliant: bool = True,
+        leaders_requirement_compliant: bool | None = None,
         limit: int = 20,
-        long_open_issues: bool = False,
-        long_unanswered_issues: bool = False,
-        long_unassigned_issues: bool = False,
+        long_open_issues: bool | None = None,
+        long_unanswered_issues: bool | None = None,
+        long_unassigned_issues: bool | None = None,
+        # Because the default behavior is to return unhealthy projects with low scores,
+        # we set `low_score` to True by default.
+        # We may return projects with high scores to indicate issues that need attention,
+        # like a lack of contributors, recent commits, or otherwise.
+        # We set the default of other parameters to False,
+        # to allow retrieving all unhealthy projects without any filters.
         low_score: bool = True,
     ) -> list[ProjectHealthMetricsNode]:
         """Resolve unhealthy projects."""
         filters = {}
 
-        if not contributors_count_requirement_compliant:
+        if contributors_count_requirement_compliant:
             filters["contributors_count__lt"] = CONTRIBUTORS_COUNT_REQUIREMENT
 
         if no_recent_releases:
             filters["recent_releases_count"] = 0
 
-        if long_open_issues:
-            filters["open_issues_count__gt"] = OPEN_ISSUES_LIMIT
+        if no_recent_commits:
+            filters["recent_commits_count"] = 0
 
-        if long_unanswered_issues:
-            filters["unanswered_issues_count__gt"] = LONG_UNANSWERED_ISSUES_LIMIT
+        if long_open_issues is not None:
+            filters["has_long_open_issues"] = long_open_issues
 
-        if long_unassigned_issues:
-            filters["unassigned_issues_count__gt"] = LONG_UNASSIGNED_ISSUES_LIMIT
+        if long_unanswered_issues is not None:
+            filters["has_long_unanswered_issues"] = long_unanswered_issues
+
+        if long_unassigned_issues is not None:
+            filters["has_long_unassigned_issues"] = long_unassigned_issues
+
+        if leaders_requirement_compliant is not None:
+            filters["is_project_leaders_requirements_compliant"] = leaders_requirement_compliant
+
+        if funding_requirement_compliant is not None:
+            filters["is_funding_requirements_compliant"] = funding_requirement_compliant
 
         if low_score:
             filters["score__lt"] = 50
 
-        filters["is_project_leaders_requirements_compliant"] = leaders_requirement_compliant
-
-        filters["is_funding_requirements_compliant"] = funding_requirement_compliant
-
         # Get the last created metrics (one for each project)
         queryset = (
             ProjectHealthMetrics.objects.select_related("project")
-            .order_by("project__key", "-nest_created_at", "-score")
+            .order_by("project__key", "-nest_created_at")
             .distinct("project__key")
         )
 
         # Apply filters
         if filters:
             queryset = queryset.filter(**filters)
-
-        if no_recent_commits:
-            from django.db.models import Q
-
-            # Again, this is just for testing purposes, it can be adjusted later
-            # Define a recent period (e.g., 120 days)
-            recent_period = timezone.now() - timezone.timedelta(days=120)
-            no_commits_filter = Q(last_committed_at__isnull=True) | Q(
-                last_committed_at__lt=recent_period
-            )
-            queryset = queryset.filter(no_commits_filter)
 
         return queryset.select_related("project")[:limit]
