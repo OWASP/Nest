1db50baad22e827f819d44ec4b472e8495938d93
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-06-18 03:42:14 +0300

    Add remaining line charts and initial support for the radial bar charts

--- frontend/src/components/GeneralChart.tsx
+++ frontend/src/components/GeneralChart.tsx
@@ -1,3 +1,4 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import dynamic from 'next/dynamic'
 import React from 'react'
 import type { ApexChartSeries } from 'types/healthMetrics'
@@ -12,20 +13,41 @@ const GeneralChart: React.FC<{
   title: string
   series: ApexChartSeries[]
   xAxis?: string[]
-}> = ({ title, series, xAxis }) => {
+  icon?: IconProp
+  type: 'line' | 'radialBar'
+}> = ({ title, series, xAxis, icon, type }) => {
+  let plotOptions = {}
+  if (type == 'radialBar') {
+    plotOptions = {
+      radialBar: {
+        size: 150,
+        hollow: {
+          margin: 5,
+          size: '70%',
+          background: 'transparent',
+        },
+        track: {
+          background: '#f2f2f2',
+        },
+        dataLabels: {
+          show: true,
+        },
+      },
+    }
+  }
   return (
-    <SecondaryCard title={title}>
+    <SecondaryCard title={title} icon={icon}>
       <Chart
         options={{
-          chart: {
-            type: 'line',
-          },
+          plotOptions: plotOptions,
+          labels: series.map((s) => s.name),
           xaxis: {
             categories: xAxis.map((dayNum) => `Day ${dayNum}`),
           },
         }}
         series={series}
         height={450}
+        type={type}
       />
     </SecondaryCard>
   )
--- frontend/src/components/HealthMetricsCharts.tsx
+++ frontend/src/components/HealthMetricsCharts.tsx
@@ -1,41 +1,122 @@
+import {
+  faExclamationCircle,
+  faCodeCommit,
+  faCodeFork,
+  faCodePullRequest,
+  faQuestionCircle,
+  faStar,
+  faTag,
+} from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 import type { HealthMetricsProps } from 'types/healthMetrics'
 import GeneralChart from 'components/GeneralChart'
 
 const HealthMetricsCharts: React.FC<{ data: HealthMetricsProps[] }> = ({ data }) => {
   const openIssuesCountArray = data.map((item) => item.openIssuesCount)
-  const xAxisArray = data.map((item, index) => (data.length - index).toString())
+  const xAxisArray = data.map((item, index) => (index + 1).toString())
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-      <GeneralChart
-        title="Issues Health Trend"
-        series={[
-          {
-            name: 'Open Issues',
-            data: openIssuesCountArray,
-          },
-          {
-            name: 'Unassigned Issues',
-            data: data.map((item) => item.unassignedIssuesCount),
-          },
-        ]}
-        xAxis={xAxisArray}
-      />
-      <GeneralChart
-        title="Unanswered Issues Trend"
-        series={[
-          {
-            name: 'Open Issues',
-            data: openIssuesCountArray,
-          },
-          {
-            name: 'Unanswered Issues',
-            data: data.map((item) => item.unansweredIssuesCount),
-          },
-        ]}
-        xAxis={xAxisArray}
-      />
-    </div>
+    <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <GeneralChart
+          title="Issues Health Trend"
+          type="line"
+          series={[
+            {
+              name: 'Open Issues',
+              data: openIssuesCountArray,
+            },
+            {
+              name: 'Unassigned Issues',
+              data: data.map((item) => item.unassignedIssuesCount),
+            },
+          ]}
+          xAxis={xAxisArray}
+          icon={faExclamationCircle}
+        />
+        <GeneralChart
+          title="Unanswered Issues Trend"
+          type="line"
+          series={[
+            {
+              name: 'Open Issues',
+              data: openIssuesCountArray,
+            },
+            {
+              name: 'Unanswered Issues',
+              data: data.map((item) => item.unansweredIssuesCount),
+            },
+          ]}
+          xAxis={xAxisArray}
+          icon={faQuestionCircle}
+        />
+      </div>
+      <div className="grid grid-cols-1">
+        <GeneralChart
+          title="Activity Trend"
+          type="line"
+          series={[
+            {
+              name: 'Open Pull Requests',
+              data: data.map((item) => item.openPullRequestsCount),
+            },
+          ]}
+          xAxis={xAxisArray}
+          icon={faCodePullRequest}
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <GeneralChart
+          title="Stars Trend"
+          type="line"
+          series={[
+            {
+              name: 'Stars',
+              data: data.map((item) => item.starsCount),
+            },
+          ]}
+          xAxis={xAxisArray}
+          icon={faStar}
+        />
+        <GeneralChart
+          title="Forks Trend"
+          type="line"
+          series={[
+            {
+              name: 'Forks',
+              data: data.map((item) => item.forksCount),
+            },
+          ]}
+          xAxis={xAxisArray}
+          icon={faCodeFork}
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <GeneralChart
+          title="Days Since Last Commit"
+          type="radialBar"
+          series={[
+            {
+              name: 'Last Commit Days',
+              data: data.map((item) => item.lastCommitDays),
+            },
+          ]}
+          xAxis={xAxisArray}
+          icon={faCodeCommit}
+        />
+        <GeneralChart
+          title="Days Since Last Release"
+          type="radialBar"
+          series={[
+            {
+              name: 'Last Release Days',
+              data: data.map((item) => item.lastReleaseDays),
+            },
+          ]}
+          xAxis={xAxisArray}
+          icon={faTag}
+        />
+      </div>
+    </>
   )
 }
 
