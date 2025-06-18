7aa9581b627d95f4adb52a4c1b4c72035117c6e1
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-06-18 19:59:22 +0300

    Add colors for Radial Chart

--- frontend/src/components/GeneralCharts.tsx
+++ frontend/src/components/GeneralCharts.tsx
@@ -36,18 +36,39 @@ const RadialChart: React.FC<{
   labels: string[]
   series: number[]
 }> = ({ title, series, icon, labels }) => {
+  const colors = series.map((days) => {
+    if (days < 3)
+      return '#00E396' // Green
+    else if (days <= 20)
+      return '#FFEA00' // Yellow
+    else return '#FF4560' // Red
+  })
   return (
     <SecondaryCard title={title} icon={icon}>
       <Chart
         options={{
           labels: labels,
+          colors: colors,
           plotOptions: {
             radialBar: {
               dataLabels: {
                 show: true,
+                value: {
+                  formatter: (val) => {
+                    return `${val} days`
+                  },
+                },
+                name: {
+                  show: true,
+                },
                 total: {
                   show: true,
-                  label: 'Total',
+                  label: 'Average Days',
+                  color: '#FF4560',
+                  formatter: (w) => {
+                    const total = w.globals.series.reduce((a, b) => a + b, 0)
+                    return `${(total / w.globals.series.length).toFixed(0)} days`
+                  },
                 },
               },
             },
