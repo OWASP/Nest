d8e089ea5fd6ca85bbb13926f80fe592639806cf
Abhay Mishra <grabhaymishra@gmail.com>
2025-06-22 12:16:17 +0530

    fixed links

--- backend/apps/github/api/urls.py
+++ backend/apps/github/api/urls.py
@@ -11,9 +11,9 @@ from apps.github.api.user import router as user_router
 
 router = Router()
 
-router.add_router("/issues", issue_router)
-router.add_router("/labels", label_router)
-router.add_router("/organizations", organization_router)
-router.add_router("/releases", release_router)
-router.add_router("/repositories", repository_router)
-router.add_router("/users", user_router)
+router.add_router(r"/issues", issue_router)
+router.add_router(r"/labels", label_router)
+router.add_router(r"/organizations", organization_router)
+router.add_router(r"/releases", release_router)
+router.add_router(r"/repositories", repository_router)
+router.add_router(r"/users", user_router)
