f0cb274c4e86869757576daf4077c8cd9c02a3c2
Abhay Mishra <grabhaymishra@gmail.com>
2025-06-27 00:24:27 +0530

    added throttling

--- backend/settings/api_v1.py
+++ backend/settings/api_v1.py
@@ -1,6 +1,7 @@
 """OWASP Nest API v1 configuration."""
 
 from ninja import NinjaAPI
+from ninja.throttling import AnonRateThrottle, AuthRateThrottle
 
 from apps.github.api.v1.urls import router as github_router
 from apps.owasp.api.v1.urls import router as owasp_router
@@ -9,6 +10,10 @@ api = NinjaAPI(
     description="API for OWASP related entities",
     title="OWASP Nest API",
     version="1.0.0",
+    throttle=[
+        AnonRateThrottle("1/s"),
+        AuthRateThrottle("10/s"),
+    ],
 )
 
 api.add_router("github", github_router)
