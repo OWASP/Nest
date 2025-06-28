80b7f4b0d37948b7137ddc27665c3cdd46153434
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-06-29 02:00:54 +0300

    Add project name and filters

--- backend/apps/owasp/graphql/queries/project_health_metrics.py
+++ backend/apps/owasp/graphql/queries/project_health_metrics.py
@@ -1,14 +1,24 @@
 """OWASP Project Health Metrics Queries."""
 
 import strawberry
+import strawberry_django
 
 from apps.owasp.graphql.nodes.health_stats import HealthStatsNode
+from apps.owasp.graphql.nodes.project_health_metrics import (
+    ProjectHealthMetricsFilter,
+    ProjectHealthMetricsNode,
+)
 from apps.owasp.models.project_health_metrics import ProjectHealthMetrics
 
 
 @strawberry.type
 class ProjectHealthMetricsQuery:
     """Project health metrics queries."""
+
+    project_health_metrics: list[ProjectHealthMetricsNode] = strawberry_django.field(
+        filters=ProjectHealthMetricsFilter,
+        description="List of project health metrics.",
+    )
 
     @strawberry.field
     def health_stats(self) -> HealthStatsNode:
