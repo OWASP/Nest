0d03f1518cda6cb4ae1a9f6768c6b9999eccc874
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-06-29 17:34:59 +0300

    fix importing

--- backend/apps/owasp/graphql/queries/project_health_metrics.py
+++ backend/apps/owasp/graphql/queries/project_health_metrics.py
@@ -3,7 +3,9 @@
 import strawberry
 import strawberry_django
 
+from apps.owasp.graphql.filters.project_health_metrics import ProjectHealthMetricsFilter
 from apps.owasp.graphql.nodes.project_health_stats import ProjectHealthStatsNode
+from apps.owasp.graphql.nodes.project_health_metrics import ProjectHealthMetricsNode
 from apps.owasp.models.project_health_metrics import ProjectHealthMetrics
 
 
