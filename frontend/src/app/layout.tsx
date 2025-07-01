45a2cdca31536c8eec37ae2cd8c0334d24a94d26
Kate Golovanova <kate@kgthreads.com>
2025-07-01 10:37:10 -0700

    Add auto-scroll to top on pathname change (#1686)
    * Add auto-scroll to top on pathname change
    
    * Add tests
    
    * Update code
    
    ---------
    
    Co-authored-by: Arkadii Yakovets <arkadii.yakovets@owasp.org>

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import React from 'react'
 import { Providers } from 'wrappers/provider'
 import { GTM_ID, IS_GITHUB_AUTH_ENABLED } from 'utils/credentials'
+import AutoScrollToTop from 'components/AutoScrollToTop'
 import BreadCrumbs from 'components/BreadCrumbs'
 import Footer from 'components/Footer'
 import Header from 'components/Header'
@@ -67,6 +68,7 @@ export default function RootLayout({
         style={{ minHeight: '100vh' }}
       >
         <Providers>
+          <AutoScrollToTop />
           <Header isGitHubAuthEnabled={IS_GITHUB_AUTH_ENABLED} />
           <BreadCrumbs />
           {children}
