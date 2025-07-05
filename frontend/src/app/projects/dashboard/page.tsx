bb5c39489b9735558231a3ea99d25a1318399f06
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-07-05 07:29:03 +0300

    Update overview page with metrics

--- frontend/src/app/projects/dashboard/page.tsx
+++ frontend/src/app/projects/dashboard/page.tsx
@@ -1,40 +1,82 @@
 'use client'
 import { useQuery } from '@apollo/client'
+import {
+  faCheck,
+  faWarning,
+  faRectangleXmark,
+  faChartLine,
+  faUsers,
+  faStar,
+  faCodeBranch,
+} from '@fortawesome/free-solid-svg-icons'
+import millify from 'millify'
+import { useState, useEffect } from 'react'
+import { handleAppError } from 'app/global-error'
 import { GET_PROJECT_HEALTH_STATS } from 'server/queries/projectsHealthDashboardQueries'
+import type { ProjectHealthStats } from 'types/projectHealthStats'
+import DashboardCard from 'components/DashboardCard'
+import LoadingSpinner from 'components/LoadingSpinner'
+import SecondaryCard from 'components/SecondaryCard'
 
 export default function ProjectsDashboardPage() {
-  const { data, loading, error } = useQuery(GET_PROJECT_HEALTH_STATS)
+  const [stats, setStats] = useState<ProjectHealthStats | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const { data, error: graphQLRequestError } = useQuery(GET_PROJECT_HEALTH_STATS)
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
+  useEffect(() => {
+    if (data) {
+      setStats(data.projectHealthStats)
+      setIsLoading(false)
+    }
+    if (graphQLRequestError) {
+      handleAppError(graphQLRequestError)
+      setIsLoading(false)
+    }
+  }, [data, graphQLRequestError])
 
-  const stats = data.projectHealthStats
-
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
   return (
-    <div>
-      <h1>Projects Dashboard</h1>
-      <div>
-        <h2>Overall Statistics</h2>
-        <p>
-          Total Projects:{' '}
-          {stats.projectsCountHealthy +
-            stats.projectsCountNeedAttention +
-            stats.projectsCountUnhealthy}
-        </p>
-        <p>
-          Healthy Projects: {stats.projectsCountHealthy} ({stats.projectsPercentageHealthy}%)
-        </p>
-        <p>
-          Projects Needing Attention: {stats.projectsCountNeedAttention} (
-          {stats.projectsPercentageNeedAttention}%)
-        </p>
-        <p>
-          Unhealthy Projects: {stats.projectsCountUnhealthy} ({stats.projectsPercentageUnhealthy}%)
-        </p>
-        <p>Total Stars: {stats.totalStars}</p>
-        <p>Total Forks: {stats.totalForks}</p>
-        <p>Total Contributors: {stats.totalContributors}</p>
+    <>
+      <h1 className="font-semibold">Project Health Dashboard Overview</h1>
+      <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-3">
+        <SecondaryCard
+          title="Healthy Projects"
+          icon={faCheck}
+          className="bg-green-100 bg-opacity-30 text-green-800 transition-colors duration-300 hover:bg-green-200 dark:bg-green-800 dark:bg-opacity-30 dark:text-green-400 dark:hover:bg-green-700"
+        >
+          <p className="text-3xl font-bold">{stats.projectsCountHealthy}</p>
+        </SecondaryCard>
+        <SecondaryCard
+          title="Projects Needing Attention"
+          icon={faWarning}
+          className="bg-yellow-100 bg-opacity-30 text-yellow-800 transition-colors duration-300 hover:bg-yellow-200 dark:bg-yellow-800 dark:bg-opacity-30 dark:text-yellow-400 dark:hover:bg-yellow-700"
+        >
+          <p className="text-3xl font-bold">{stats.projectsCountNeedAttention}</p>
+        </SecondaryCard>
+        <SecondaryCard
+          title="Unhealthy Projects"
+          icon={faRectangleXmark}
+          className="bg-red-100 bg-opacity-30 text-red-800 transition-colors duration-300 hover:bg-red-200 dark:bg-red-800 dark:bg-opacity-30 dark:text-red-400 dark:hover:bg-red-700"
+        >
+          <p className="text-3xl font-bold">{stats.projectsCountUnhealthy}</p>
+        </SecondaryCard>
       </div>
-    </div>
+      <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-4">
+        <DashboardCard
+          title="Average Score"
+          icon={faChartLine}
+          stats={`%${stats.averageScore.toFixed(0)}`}
+        />
+        <DashboardCard
+          title="Total Contributors"
+          icon={faUsers}
+          stats={millify(stats.totalContributors)}
+        />
+        <DashboardCard title="Total Forks" icon={faCodeBranch} stats={millify(stats.totalForks)} />
+        <DashboardCard title="Total Stars" icon={faStar} stats={millify(stats.totalStars)} />
+      </div>
+    </>
   )
 }
---
+++ frontend/src/types/projectHealthStats.ts
@@ -0,0 +1,13 @@
+export type ProjectHealthStats = {
+  averageScore: number
+  monthlyOverallScores: number[]
+  projectsCountHealthy: number
+  projectsCountNeedAttention: number
+  projectsCountUnhealthy: number
+  projectsPercentageHealthy: number
+  projectsPercentageNeedAttention: number
+  projectsPercentageUnhealthy: number
+  totalContributors: number
+  totalForks: number
+  totalStars: number
+}
