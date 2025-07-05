54c564c81e5ec047d1ea9352d824cbd87df43a66
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-07-05 06:40:00 +0300

    Update navigation-bar

---
+++ frontend/src/components/ProjectsDashboardNavBar.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { faChartLine, faChartBar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Navbar, NavbarItem, NavbarContent } from '@heroui/react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export default function ProjectsDashboardNavBar() {
+  const pathname = usePathname()
+  const isActive = (path: string) => pathname === path
+  return (
+    <Navbar
+      classNames={{
+        base: 'h-full w-64 flex-col items-start justify-start py-4',
+        item: [
+          'data-[active=true]:bg-gray-200',
+          'data-[active=true]:dark:bg-gray-800',
+          'data-[active=true]:text-align-left',
+          'w-full',
+          'data-[active=true]:rounded',
+        ],
+      }}
+    >
+      <NavbarContent className="flex h-full flex-col items-start justify-start">
+        <NavbarItem isActive={isActive('/projects/dashboard')}>
+          <Link href="/projects/dashboard">
+            <div className="flex w-full items-center gap-2 rounded p-2 hover:bg-gray-200 dark:hover:bg-gray-800">
+              <FontAwesomeIcon icon={faChartLine} className="text-xl" />
+              <span className="text-sm font-semibold">Overview</span>
+            </div>
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={isActive('/projects/dashboard/metrics')}>
+          <Link href="/projects/dashboard/metrics">
+            <div className="flex w-full items-center gap-2 rounded p-2 hover:bg-gray-200 dark:hover:bg-gray-800">
+              <FontAwesomeIcon icon={faChartBar} className="text-xl" />
+              <span className="text-sm font-semibold">Metrics</span>
+            </div>
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
+}
