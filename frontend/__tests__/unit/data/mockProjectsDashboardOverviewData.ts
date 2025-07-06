25397839a04cf55c5b4630167056890c762f23d4
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-07-06 08:15:52 +0300

    Update mocking

--- frontend/__tests__/unit/data/mockProjectsDashboardOverviewData.ts
+++ frontend/__tests__/unit/data/mockProjectsDashboardOverviewData.ts
@@ -1,12 +1,12 @@
 export const mockProjectsDashboardOverviewData = {
   projectHealthStats: {
-    projectsCountHealthy: 5,
-    projectsCountNeedAttention: 3,
-    projectsCountUnhealthy: 2,
+    projectsCountHealthy: 542,
+    projectsCountNeedAttention: 164,
+    projectsCountUnhealthy: 252,
     averageScore: 75,
-    totalContributors: 100,
-    totalForks: 50,
-    totalStars: 200,
+    totalContributors: 95848,
+    totalForks: 48428,
+    totalStars: 123456,
     monthlyOverallScores: [70, 75, 80, 85, 90],
     monthlyOverallScoresMonths: [1, 2, 3, 4, 5],
     projectsPercentageHealthy: 50,
