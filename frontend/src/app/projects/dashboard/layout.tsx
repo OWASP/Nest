17491001f4530d2c2c162a471def78d18642c6a9
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-07-08 13:54:34 +0300

    Fix navbar

--- frontend/src/app/projects/dashboard/layout.tsx
+++ frontend/src/app/projects/dashboard/layout.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import FontLoaderWrapper from 'components/FontLoaderWrapper'
 import ProjectsDashboardNavBar from 'components/ProjectsDashboardNavBar'
 
 const ProjectsHealthDashboardLayout: React.FC<{ readonly children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <div className="flex flex-col md:flex-row">
-      <ProjectsDashboardNavBar />
-      <main className="flex-1 p-4" role="main">
-        {children}
-      </main>
-    </div>
+    <FontLoaderWrapper>
+      <div className="flex flex-col md:flex-row">
+        <ProjectsDashboardNavBar />
+        <main className="flex-1 p-4" role="main">
+          {children}
+        </main>
+      </div>
+    </FontLoaderWrapper>
   )
 }
 
