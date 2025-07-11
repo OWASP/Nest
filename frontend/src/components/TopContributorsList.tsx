e95d006dd1aa2aec1813bf68a3cc4298e044ba72
Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>
2025-07-10 18:42:05 -0700

    Update frontend/src/components/TopContributorsList.tsx
    Co-authored-by: Kate Golovanova <kate@kgthreads.com>

--- frontend/src/components/TopContributorsList.tsx
+++ frontend/src/components/TopContributorsList.tsx
@@ -43,7 +43,7 @@ const TopContributorsList = ({
         </div>
       }
     >
-      <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-4">
+      <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
         {displayContributors.map((item, index) => (
           <div key={index} className="overflow-hidden rounded-lg bg-gray-200 p-4 dark:bg-gray-700">
             <div className="flex w-full items-center gap-2">
