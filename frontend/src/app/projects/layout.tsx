c1d67ec15b751f2b03d73860cfb5f9ad90b77169
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-07-23 16:46:36 +0300

    Add dashboard button

--- frontend/src/app/projects/layout.tsx
+++ frontend/src/app/projects/layout.tsx
@@ -1,9 +1,19 @@
 import { Metadata } from 'next'
 import React from 'react'
+import { IS_GITHUB_AUTH_ENABLED } from 'utils/credentials'
 import { getStaticMetadata } from 'utils/metaconfig'
-
+import AccessProjectDashboardButton from 'components/AccessProjectDashboardButton'
 export const metadata: Metadata = getStaticMetadata('projects', '/projects')
 
+// TODO(ahmed): Create a project wrapper to show loading spinner for the whole page
+
 export default function ProjectsLayout({ children }: { children: React.ReactNode }) {
-  return children
+  return IS_GITHUB_AUTH_ENABLED ? (
+    <div className="flex flex-col items-center justify-center">
+      <AccessProjectDashboardButton />
+      {children}
+    </div>
+  ) : (
+    children
+  )
 }
