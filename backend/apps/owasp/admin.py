805c2aef3fc4eb742e19f775c0d6f7ee80fa730d
pavan <pavanvanam890@gmail.com>
2025-07-24 17:14:40 +0530

    Add Badge model to owasp app #1762

--- backend/apps/owasp/admin.py
+++ backend/apps/owasp/admin.py
@@ -264,6 +264,27 @@ class SponsorAdmin(admin.ModelAdmin):
     )
 
 
+class BadgeAdmin(admin.ModelAdmin):
+    """Admin for Badge model."""
+
+    list_display = (
+        "name",
+        "description",
+        "weight",
+        "css_class",
+        "nest_created_at",
+        "nest_updated_at",
+    )
+    search_fields = ("name", "description", "css_class")
+    list_filter = ("weight",)
+    ordering = ("weight", "name")
+    fieldsets = (
+        ("Basic Information", {"fields": ("name", "description", "weight")}),
+        ("Display Settings", {"fields": ("css_class",)}),
+        ("Timestamps", {"fields": ("nest_created_at", "nest_updated_at")}),
+    )
+
+
 admin.site.register(Chapter, ChapterAdmin)
 admin.site.register(Committee, CommitteeAdmin)
 admin.site.register(Event, EventAdmin)
