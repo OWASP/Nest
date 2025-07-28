69230422f623f73ad84e2464a844fa6647e508cf
adithya-naik <jatothadithyanaik@gmail.com>
2025-07-28 22:17:55 +0530

    fix/by-creating-a-common-component-#1329

--- frontend/src/components/ToggleButton.tsx
+++ frontend/src/components/ToggleButton.tsx
@@ -2,14 +2,22 @@ import { Button } from '@heroui/button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 
-const ToggleButton = ({ 
-  isExpanded, 
-  onToggle, 
-  showMoreText = "Show more", 
-  showLessText = "Show less" 
-}) => (
+interface ToggleButtonProps {
+  isExpanded: boolean
+  onToggle: () => void
+  showMoreText?: string
+  showLessText?: string
+}
+
+const ToggleButton = ({
+  isExpanded,
+  onToggle,
+  showMoreText = "Show more",
+  showLessText = "Show less",
+}: ToggleButtonProps) => (
   <div className="mt-4 flex justify-start">
     <Button
+      type="button"
       disableAnimation
       onPress={onToggle}
       className="bg-blue-50 hover:bg-blue-100 dark:bg-blue-900/20 dark:hover:bg-blue-900/30 text-blue-600 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 border border-blue-200 dark:border-blue-800 rounded-md px-4 py-2 font-medium transition-colors"
@@ -26,3 +34,4 @@ const ToggleButton = ({
     </Button>
   </div>
 )
+export default ToggleButton
