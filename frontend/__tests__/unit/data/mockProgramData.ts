4b7d5dce7a849c04fc33aab6f73ce8e3ffd529dc
Rajgupta36 <raj.gupta369@outlook.com>
2025-07-29 01:35:03 +0530

    added unit test case for program page

---
+++ frontend/__tests__/unit/data/mockProgramData.ts
@@ -0,0 +1,11 @@
+export const mockPrograms = [
+  {
+    key: 'program_1',
+    name: 'Program 1',
+    description: 'This is a summary of Program 1.',
+    startedAt: '2025-01-01',
+    endedAt: '2025-12-31',
+    status: 'published',
+    modules: ['Module A', 'Module B'],
+  },
+]
