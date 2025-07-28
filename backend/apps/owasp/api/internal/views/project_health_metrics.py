bfbede144f2a46b77350b3b5213003af870ca486
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-07-28 07:54:54 +0300

    Add require GET

--- backend/apps/owasp/api/internal/views/project_health_metrics.py
+++ backend/apps/owasp/api/internal/views/project_health_metrics.py
@@ -1,10 +1,12 @@
 """Views for OWASP project health metrics."""
 
 from django.http import FileResponse
+from django.views.decorators.http import require_GET
 
 from apps.owasp.models.project_health_metrics import ProjectHealthMetrics
 
 
+@require_GET
 def generate_overview_pdf(_request):
     """Generate a PDF overview of OWASP project health metrics."""
     pdf = ProjectHealthMetrics.generate_overview_pdf()
