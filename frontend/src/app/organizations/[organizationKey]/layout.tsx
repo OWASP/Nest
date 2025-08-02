68378a3d7fb2cb189e35d5e7307fe8445e98cd4a
Rudransh Shrivastava <rudransh.shr@gmail.com>
2025-08-02 21:25:32 +0530

    format imports

--- frontend/src/app/organizations/[organizationKey]/layout.tsx
+++ frontend/src/app/organizations/[organizationKey]/layout.tsx
@@ -2,7 +2,10 @@ import { Metadata } from 'next'
 import Script from 'next/script'
 import React from 'react'
 import { apolloClient } from 'server/apolloClient'
-import { GET_ORGANIZATION_METADATA, GET_ORGANIZATION_DATA } from 'server/queries/organizationQueries'
+import {
+  GET_ORGANIZATION_METADATA,
+  GET_ORGANIZATION_DATA,
+} from 'server/queries/organizationQueries'
 import { generateSeoMetadata } from 'utils/metaconfig'
 
 export async function generateMetadata({
