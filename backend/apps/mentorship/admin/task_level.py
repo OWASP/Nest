17d1a7b43212ab342ef6e606aaa5292117e8c25e
Rajgupta36 <raj.gupta369@outlook.com>
2025-08-04 21:53:46 +0530

    update code

---
+++ backend/apps/mentorship/admin/task_level.py
@@ -0,0 +1,23 @@
+"""Mentorship app Program model admin."""
+
+from django.contrib import admin
+from apps.mentorship.models.task_level import TaskLevel
+
+
+class TaskLevelAdmin(admin.ModelAdmin):
+    """Admin view for TaskLevel model."""
+
+    list_display = (
+        "description",
+        "labels",
+        "module",
+        "name",
+    )
+
+    search_fields = (
+        "name",
+        "module__name",
+    )
+
+
+admin.site.register(TaskLevel, TaskLevelAdmin)
