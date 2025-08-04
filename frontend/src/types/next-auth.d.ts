addece7a1f85e82bb68062c1989204d20c6cd890
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-08-04 16:25:07 +0300

    Update Session.user type

--- frontend/src/types/next-auth.d.ts
+++ frontend/src/types/next-auth.d.ts
@@ -3,10 +3,10 @@ import 'next-auth'
 declare module 'next-auth' {
   interface Session {
     user: {
-      id: string
-      name: string
-      email: string
-      login: string
+      id?: string
+      name?: string
+      email?: string
+      login?: string
       image?: string
     }
   }
