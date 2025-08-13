e2fc2370ee389acbb34d03ccd3e35a3f74a3c9ef
Raj gupta <raj.gupta369@outlook.com>
2025-08-13 08:32:48 +0530

    Created Module and program pages (#1717)
    * program mutation and node
    
    * UI for program create , view and edit
    
    * backend completed
    
    * create module edit and view page
    
    * type fix
    
    * update components
    
    * updated backend
    
    * slugify urls
    
    * pre commit
    
    * fix components
    
    * update suggestions
    
    * update pnpm-lock file
    
    * fix spell check
    
    * fix publish program
    
    * fix unit test cases
    
    * fix pre-commits
    
    * update frontend logic
    
    * format
    
    * refactor code
    
    * fix terminal
    
    * suggestion fixes
    
    * added unit test cases
    
    * fix test cases
    
    * optimization
    
    * generated code
    
    * update code
    
    * update code
    
    * update naming,validation logic
    
    * cleanup
    
    * fix project suggestion bug
    
    * update code
    
    * fixes
    
    * apply suggestions
    
    * update suggestion
    
    * update code
    
    * update code
    
    * update frozen-lockfile
    
    * update code and make program page view only
    
    * updated migration and add query
    
    * added index
    
    * updated components, mutaitons and nodes
    
    * update code
    
    * added constraints
    
    * fix test case
    
    * added unit test case for program page
    
    * added remaining unit test cases
    
    * fix bug
    
    * update code
    
    * updated auth and projectleader code
    
    * update test cases
    
    * update code
    
    * revert test case
    
    * fix unit test cases
    
    * migration fix
    
    * increase  test coverage
    
    * update suggestions
    
    * added function for cache clear by index_name
    
    * created component for single module
    
    * fix typo
    
    * fix some frontend test cases
    
    * added post save signal
    
    * added more test cases
    
    * pnpm updated
    
    * increase coverage
    
    * update suggestion
    
    * updated backend suggestions
    
    * added isMentor query
    
    * fix page loading
    
    * Update module description styling
    
    * Update code
    
    ---------
    
    Co-authored-by: Kate Golovanova <kate@kgthreads.com>
    Co-authored-by: Arkadii Yakovets <arkadii.yakovets@owasp.org>

--- frontend/src/components/ToggleableList.tsx
+++ frontend/src/components/ToggleableList.tsx
@@ -10,11 +10,13 @@ const ToggleableList = ({
   label,
   icon,
   limit = 10,
+  isDisabled = false,
 }: {
   items: string[]
   label: React.ReactNode
   limit?: number
   icon?: IconDefinition
+  isDisabled?: boolean
 }) => {
   const [showAll, setShowAll] = useState(false)
   const router = useRouter()
@@ -38,7 +40,7 @@ const ToggleableList = ({
           <button
             key={index}
             className="rounded-lg border border-gray-400 px-3 py-1 text-sm hover:bg-gray-200 dark:border-gray-300 dark:hover:bg-gray-700"
-            onClick={() => handleButtonClick({ item })}
+            onClick={() => !isDisabled && handleButtonClick({ item })}
           >
             {item}
           </button>
