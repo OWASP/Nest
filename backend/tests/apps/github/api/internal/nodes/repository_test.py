5892607d4738d5dbba9e65e0719fa795536f50d5
Rudransh Shrivastava <125893371+rudransh-shrivastava@users.noreply.github.com>
2025-08-14 08:17:23 +0530

    Add id field to graphql entities (#2045)
    * add id field
    
    * update code
    
    * add id field by inheriting from strawberry.relay.Node

--- backend/tests/apps/github/api/internal/nodes/repository_test.py
+++ backend/tests/apps/github/api/internal/nodes/repository_test.py
@@ -18,7 +18,6 @@ class TestRepositoryNode:
         field_names = {field.name for field in RepositoryNode.__strawberry_definition__.fields}
         expected_field_names = {
             "_id",
-            "commits_count",
             "contributors_count",
             "created_at",
             "description",
