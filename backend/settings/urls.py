9ea6936e87c91c69ec9690b6cb6b08d3c5f4503a
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-08-17 19:27:59 +0300

    Add google callback

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -16,6 +16,7 @@ from apps.core.api.internal.csrf import get_csrf_token
 from apps.core.api.internal.status import get_status
 from apps.owasp.api.internal.views.urls import urlpatterns as owasp_urls
 from apps.slack.apps import SlackConfig
+from apps.slack.views import google_auth_callback
 from settings.api.v1 import api as api_v1
 from settings.graphql import schema
 
@@ -37,6 +38,7 @@ if SlackConfig.app:
         path("integrations/slack/commands/", slack_request_handler),
         path("integrations/slack/events/", slack_request_handler),
         path("integrations/slack/interactivity/", slack_request_handler),
+        path("integrations/slack/oauth2/callback/", google_auth_callback),
     ]
 
 if settings.DEBUG:
