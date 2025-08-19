3789e3d5f8583895c246e9786be4052311a5db3d
OUMIMANDAL <oumimandal9@gmail.com>
2025-08-20 03:15:35 +0530

    fix: correctly mark RootLayout props as read-only

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -58,9 +58,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}:Readonly {
-  children: React.ReactNode
-}) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
