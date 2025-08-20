c617cf3e9016ad2c3762ad8f5170cdc4461a798f
Rajgupta36 <raj.gupta369@outlook.com>
2025-08-21 00:25:29 +0530

    clean

--- backend/manage.py
+++ backend/manage.py
@@ -5,10 +5,6 @@
 import os
 import sys
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
