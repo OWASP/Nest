02ccee845066c85c134ef68d1b808cad0aca1990
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-08-21 18:12:01 +0300

    Add handler test

---
+++ backend/tests/apps/slack/common/handlers/google_sign_in_test.py
@@ -0,0 +1,28 @@
+"""Test cases for google sign in handler."""
+
+from unittest.mock import Mock, patch
+
+from apps.slack.blocks import markdown
+from apps.slack.common.handlers.google_sign_in import get_blocks
+
+
+class TestGoogleSignInHandler:
+    """Test cases for GoogleSignInHandler."""
+
+    @patch("apps.nest.models.google_account_authorization.GoogleAccountAuthorization.authenticate")
+    def test_handle_google_sign_in_when_not_authenticated(self, mock_auth):
+        """Test handle_google_sign_in when not authenticated."""
+        mock_auth.return_value = ("test_url", "test_state")
+        blocks = get_blocks("test_slack_user_id")
+        mock_auth.assert_called_once_with("test_slack_user_id")
+        assert blocks == [
+            markdown("Please sign in to Google: <test_url|Click here>"),
+        ]
+
+    @patch("apps.nest.models.google_account_authorization.GoogleAccountAuthorization.authenticate")
+    def test_handle_google_sign_in_when_authenticated(self, mock_auth):
+        """Test handle_google_sign_in when authenticated."""
+        mock_auth.return_value = Mock()
+        blocks = get_blocks("test_slack_user_id")
+        mock_auth.assert_called_once_with("test_slack_user_id")
+        assert blocks == [markdown("✅ You are already signed in to Google.")]
