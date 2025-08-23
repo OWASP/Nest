508b0cd5503a32202aaafc1ee46dec127fd64983
Rudransh Shrivastava <rudransh.shr@gmail.com>
2025-08-23 16:31:54 +0530

    use environment variables instead of flags, upload to artifacts

--- frontend/lighthouserc.js
+++ frontend/lighthouserc.js
@@ -1,3 +1,9 @@
+const LHCI_CHROME_FLAGS =
+  process.env.LHCI_CHROME_FLAGS || '--disable-dev-shm-usage --headless --no-sandbox'
+const LHCI_OUTPUT_DIR = process.env.LHCI_OUTPUT_DIR || '.lighthouseci/'
+const LHCI_UPLOAD_TARGET = process.env.LHCI_UPLOAD_TARGET || 'filesystem'
+const LHCI_URL = process.env.LHCI_URL || 'http://localhost:3000/'
+
 module.exports = {
   ci: {
     assert: {
@@ -11,12 +17,14 @@ module.exports = {
     collect: {
       numberOfRuns: 3,
       settings: {
-        chromeFlags: '--disable-dev-shm-usage --headless --no-sandbox',
+        chromeFlags: LHCI_CHROME_FLAGS,
       },
-      url: ['http://localhost:3000/'],
+      url: [LHCI_URL],
     },
     upload: {
-      target: 'temporary-public-storage',
+      outputDir: LHCI_OUTPUT_DIR,
+      reportFilenamePattern: '%%DATETIME%%-%%HOSTNAME%%%%PATHNAME%%.%%EXTENSION%%',
+      target: LHCI_UPLOAD_TARGET,
     },
   },
 }
