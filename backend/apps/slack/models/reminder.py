f68b28159f59bdc59d437f8824f441408d0440cf
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-08-28 20:19:35 +0300

    Apply rabbit suggestions

--- backend/apps/slack/models/reminder.py
+++ backend/apps/slack/models/reminder.py
@@ -1,32 +1,33 @@
-"""Slack remainder model."""
+"""Slack reminder model."""
 
 from django.db import models
 
 
-class Remainder(models.Model):
-    """Model representing a slack channel remainder."""
+class Reminder(models.Model):
+    """Model representing a slack channel reminder."""
 
     class Meta:
-        db_table = "slack_remainders"
-        verbose_name = "Slack Remainder"
-        verbose_name_plural = "Slack Remainders"
+        db_table = "slack_reminders"
+        verbose_name = "Slack Reminder"
+        verbose_name_plural = "Slack Reminders"
 
     member = models.ForeignKey(
         "slack.Member",
         verbose_name="Slack Member",
         on_delete=models.SET_NULL,
+        related_name="reminders",
         null=True,
     )
     channel = models.ForeignKey(
         "slack.EntityChannel",
         verbose_name="Slack Channel",
+        related_name="reminders",
         on_delete=models.CASCADE,
-        null=True,
     )
     message = models.TextField(verbose_name="Reminder Message")
     remind_at = models.DateTimeField(verbose_name="Reminder Time")
     periodic = models.BooleanField(default=False, verbose_name="Is Periodic")
 
     def __str__(self) -> str:
-        """Remainder human readable representation."""
+        """Reminder human readable representation."""
         return f"Reminder for {self.member} in {self.channel}: {self.message} at {self.remind_at}"
