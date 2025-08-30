06b05278b7d29c7488ca647074e48ac1f94bbd45
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2025-08-29 20:37:00 -0700

    Run pnpm format (looks like a nondeterministic behavior bug)

--- frontend/src/components/ItemCardList.tsx
+++ frontend/src/components/ItemCardList.tsx
@@ -71,7 +71,7 @@ const ItemCardList = ({
                     </Link>
                   </Tooltip>
                 )}
-                <h3 className="min-w-0 flex-1 overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
+                <h3 className="min-w-0 flex-1 overflow-hidden font-semibold text-ellipsis whitespace-nowrap">
                   <Link
                     className="text-blue-400 hover:underline"
                     href={item?.url || ''}
