c57b4bf72dabdb099930bae24bbf42b73f30dbbd
Ahmed Gouda <62152210+ahmedxgouda@users.noreply.github.com>
2025-08-30 06:47:58 +0300

    Integrate slack bot with google OAuth (#2089)
    * Add google libs
    
    * Add model
    
    * Update logic and add tests
    
    * Update tests, apply make-check and suggestions
    
    * Skip sonar and apply rabbit suggestion
    
    * Update poetry lock
    
    * Add migrations
    
    * Change user to member
    
    * Update env.example
    
    * Update poetry.lock
    
    * Update tokens to binary field
    
    * Improve auth logic
    
    * Apply migrations
    
    * Apply rabbit's suggestions
    
    * Separate google client from slack.GoogleAuth model
    
    * Convert singleton to factory
    
    * Update auth_uri
    
    * Apply suggestions
    
    * Add meta class
    
    * Update refresh logic and tests
    
    * Make google auth credentials secret
    
    * Update code
    
    * Apply suggestions
    
    * Update tests and member related name
    
    * Clean up migrations
    
    * Add kms key id to env
    
    * Add AWS to settings
    
    * Add KMS client, encryption and decryption
    
    * Separate kms client from GoogleAuth model
    
    * Update kms client
    
    * Update tests
    
    * Update refresh and tests
    
    * Apply rabbit suggestion
    
    * Apply check-spelling
    
    * Apply rabbit suggestion and add google client test
    
    * Add custom field for kms
    
    * Update tests
    
    * Apply suggestions
    
    * Update tests
    
    * Guard authenticate method with KMS
    
    * Update tests
    
    * Add migrations
    
    * Update settings
    
    * Clean up migrations
    
    * Add google callback
    
    * Add google sign in button
    
    * Apply suggestions
    
    * Apply suggestion
    
    * Update tests
    
    * Change name
    
    * Add migrations
    
    * Refactor google and kms clients
    
    * Update tests
    
    * Add sign in command
    
    * Update tests
    
    * Add handler test
    
    * Clean up migrations
    
    * Apply suggestions
    
    * Update backend/tests/apps/nest/models/google_account_authorization_test.py
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Apply rabbit's suggestion
    
    * Update poetry lock
    
    * Update code
    
    * Apply suggestions
    
    ---------
    
    Co-authored-by: Arkadii Yakovets <arkadii.yakovets@owasp.org>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ backend/tests/apps/slack/common/handlers/google_sign_in_test.py
@@ -0,0 +1,28 @@
+"""Test cases for google sign in handler."""
+
+from unittest.mock import Mock, patch
+
+from apps.slack.blocks import markdown
+from apps.slack.common.handlers.google_sign_in import get_blocks
+
+
+class TestGoogleSignInHandler:
+    """Test cases for GoogleSignInHandler."""
+
+    @patch("apps.nest.models.google_account_authorization.GoogleAccountAuthorization.authorize")
+    def test_handle_google_sign_in_when_not_authorized(self, mock_auth):
+        """Test handle_google_sign_in when not authorized."""
+        mock_auth.return_value = ("test_url", "test_state")
+        blocks = get_blocks("test_slack_user_id")
+        mock_auth.assert_called_once_with("test_slack_user_id")
+        assert blocks == [
+            markdown("Please sign in with Google on <test_url|this page>"),
+        ]
+
+    @patch("apps.nest.models.google_account_authorization.GoogleAccountAuthorization.authorize")
+    def test_handle_google_sign_in_when_authorized(self, mock_auth):
+        """Test handle_google_sign_in when authorized."""
+        mock_auth.return_value = Mock()
+        blocks = get_blocks("test_slack_user_id")
+        mock_auth.assert_called_once_with("test_slack_user_id")
+        assert blocks == [markdown("✅ You are already signed in.")]
