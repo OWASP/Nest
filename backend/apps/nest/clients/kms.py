5a85dd3f983d33016261349508b02bf99819b7e1
Ahmed Gouda <62152210+ahmedxgouda@users.noreply.github.com>
2025-09-02 02:28:20 +0300

    Use singleton pattern in kms client (#2158)
    * Use singleton
    
    * Add thread locking

--- backend/apps/nest/clients/kms.py
+++ backend/apps/nest/clients/kms.py
@@ -1,5 +1,7 @@
 """AWS KMS client."""
 
+from threading import Lock
+
 import boto3
 from django.conf import settings
 
@@ -7,8 +9,20 @@ from django.conf import settings
 class KmsClient:
     """AWS KMS Client."""
 
+    _lock = Lock()
+
+    def __new__(cls):
+        """Create a new instance of KmsClient."""
+        if not hasattr(cls, "instance"):
+            with cls._lock:
+                if not hasattr(cls, "instance"):
+                    cls.instance = super().__new__(cls)
+        return cls.instance
+
     def __init__(self):
         """Initialize the KMS client."""
+        if getattr(self, "client", None) is not None:
+            return
         self.client = boto3.client(
             "kms",
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
@@ -28,6 +42,3 @@ class KmsClient:
             KeyId=settings.AWS_KMS_KEY_ID,
             Plaintext=text.encode("utf-8"),
         )["CiphertextBlob"]
-
-
-kms_client = KmsClient()
