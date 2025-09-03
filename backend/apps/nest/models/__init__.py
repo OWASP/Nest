76cbcfbf4dd15c4f9da403b77a4b9e4b42830f8d
Ahmed Gouda <62152210+ahmedxgouda@users.noreply.github.com>
2025-09-03 05:29:50 +0300

    Update owasp app event model with calendar attributes and add reminder models (#2128)
    * Update event
    
    * Add slack member and channel id fields
    
    * Apply rabbit's suggestion
    
    * Apply suggestions
    
    * Add null possibility to channel in remainder
    
    * Apply rabbit suggestions
    
    * Use channel id and add tests
    
    * Apply suggestions and add reminder shedule
    
    ---------
    
    Co-authored-by: Kate Golovanova <kate@kgthreads.com>

--- backend/apps/nest/models/__init__.py
+++ backend/apps/nest/models/__init__.py
@@ -1,4 +1,6 @@
 from .badge import Badge
 from .google_account_authorization import GoogleAccountAuthorization
+from .reminder import Reminder
+from .reminder_schedule import ReminderSchedule
 from .user import User
 from .user_badge import UserBadge
