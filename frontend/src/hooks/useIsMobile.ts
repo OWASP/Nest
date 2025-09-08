eb32f880e02601944cae486fd7f18d53b52b655c
Ishan Bahuguna <ishanbahuguna1@gmail.com>
2025-09-08 17:45:10 +0530

    made the useIsMobile hook compatible with old browsers

--- frontend/src/hooks/useIsMobile.ts
+++ frontend/src/hooks/useIsMobile.ts
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
+import { desktopViewMinWidth } from 'utils/constants'
 
 export const useIsMobile = () => {
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    // Tailwind md breakpoint starts at 768px (min-width:768px),
-    // so mobile should be max-width:767px
-    const mediaQuery = window.matchMedia('(max-width: 767px)')
+
+    // check whether the browser supports matchMedia API
+    if (typeof window.matchMedia !== 'function') return
+    
+    const mediaQuery = window.matchMedia(`(max-width: ${desktopViewMinWidth - 1}px)`)
 
     const updateMatch = (e: MediaQueryListEvent | MediaQueryList) => {
       setIsMobile(e.matches)
@@ -14,14 +17,13 @@ export const useIsMobile = () => {
 
     updateMatch(mediaQuery)
 
-    if (mediaQuery.addEventListener) {
+    if (typeof mediaQuery.addEventListener === 'function') { // check for modern browser so it doesn't throw an error
       mediaQuery.addEventListener('change', updateMatch)
       return () => mediaQuery.removeEventListener('change', updateMatch)
-    } else {
-      // Safari browser < 14 fallback
+    } else if(typeof mediaQuery.addListener === 'function') { // check for old browser
       mediaQuery.addListener(updateMatch)
       return () => mediaQuery.removeListener(updateMatch)
-    }
+    } else return;
   }, [])
 
   return isMobile
