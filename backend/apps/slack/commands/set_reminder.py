37ea7c46d7c9a9c942fce7e140105f31c22321d6
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-09-08 09:17:02 +0300

    Refactor

--- backend/apps/slack/commands/set_reminder.py
+++ backend/apps/slack/commands/set_reminder.py
@@ -1,5 +1,6 @@
 """Slack Set Reminder Command."""
 
+from apps.slack.blocks import markdown
 from apps.slack.commands.command import CommandBase
 from apps.slack.common.handlers.calendar_events import get_reminder_blocks
 from apps.slack.utils import parse_slack_reminder_args
@@ -15,5 +16,9 @@ class SetReminder(CommandBase):
 
     def render_blocks(self, command):
         """Render the blocks for the command."""
-        args = parse_slack_reminder_args(command["text"])
-        return get_reminder_blocks(args, self.get_user_id(command))
+        try:
+            args = parse_slack_reminder_args(command["text"])
+        except SystemExit:
+            return [markdown("*Invalid command format. Please check your input and try again.*")]
+        else:
+            return get_reminder_blocks(args, self.get_user_id(command))
