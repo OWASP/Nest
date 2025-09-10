ca115b2bfa54b498e1255d10066dceac2fed941b
Anurag Yadav <143180737+anurag2787@users.noreply.github.com>
2025-09-11 04:53:36 +0530

    Unify user summary card (#2208)
    * Maked Unified UserSummaryCard
    
    * Removed unused code
    
    * fixed code
    
    * updated the heatmap to bigger size
    
    * updated size of heatmap
    
    * added test
    
    * fix
    
    ---------
    
    Co-authored-by: Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>

--- frontend/src/app/members/[memberKey]/page.tsx
+++ frontend/src/app/members/[memberKey]/page.tsx
@@ -35,10 +35,7 @@ const UserDetailsPage: React.FC = () => {
   const [data, setData] = useState<HeatmapData>({} as HeatmapData)
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [username, setUsername] = useState('')
-  const [imageLink, setImageLink] = useState('')
   const [isPrivateContributor, setIsPrivateContributor] = useState(false)
-  const canvasRef = useRef<HTMLCanvasElement | null>(null)
-  const theme = 'blue'
 
   const { data: graphQLData, error: graphQLRequestError } = useQuery(GET_USER_DATA, {
     variables: { key: memberKey },
@@ -74,14 +71,6 @@ const UserDetailsPage: React.FC = () => {
     }
     fetchData()
   }, [memberKey, user])
-
-  useEffect(() => {
-    if (canvasRef.current && data && data.years && data.years.length > 0) {
-      drawContributions(canvasRef.current, { data, username, theme })
-      const imageURL = canvasRef.current.toDataURL()
-      setImageLink(imageURL)
-    }
-  }, [username, data])
 
   const formattedBio = user?.bio?.split(' ').map((word, index) => {
     // Regex to match GitHub usernames, but if last character is not a word character or @, it's a punctuation
@@ -142,6 +131,7 @@ const UserDetailsPage: React.FC = () => {
 
   const Heatmap = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null)
+    const [imgSrc, setImgSrc] = useState('')
     const { resolvedTheme } = useTheme()
     const isDarkMode = (resolvedTheme ?? 'light') === 'dark'
 
@@ -153,61 +143,71 @@ const UserDetailsPage: React.FC = () => {
           themeName: isDarkMode ? 'dark' : 'light',
         })
         const imageURL = canvasRef.current.toDataURL()
-        setImageLink(imageURL)
+        setImgSrc(imageURL)
+      } else {
+        setImgSrc('')
       }
     }, [isDarkMode])
 
     return (
-      <div className="flex flex-col gap-4">
-        <div className="overflow-hidden rounded-lg bg-white dark:bg-gray-800">
-          <div className="relative">
-            <canvas ref={canvasRef} style={{ display: 'none' }} aria-hidden="true"></canvas>
-            {imageLink ? (
-              <div className="h-40">
-                <Image
-                  width={100}
-                  height={100}
-                  src={imageLink}
-                  className="h-full w-full object-cover object-[54%_60%]"
-                  alt="Contribution Heatmap"
-                />
-              </div>
-            ) : (
-              <div className="relative h-40 items-center justify-center">
-                <Image
-                  height={100}
-                  width={100}
-                  src={
-                    isDarkMode
-                      ? '/img/heatmap-background-dark.png'
-                      : '/img/heatmap-background-light.png'
-                  }
-                  className="heatmap-background-loader h-full w-full border-none object-cover object-[54%_60%]"
-                  alt="Heatmap Background"
-                />
-                <div className="heatmap-loader"></div>
-              </div>
-            )}
-          </div>
+      <div className="overflow-hidden rounded-lg bg-white dark:bg-gray-800">
+        <div className="relative">
+          <canvas ref={canvasRef} style={{ display: 'none' }} aria-hidden="true"></canvas>
+          {imgSrc ? (
+            <div className="h-32">
+              <Image
+                width={100}
+                height={100}
+                src={imgSrc}
+                className="h-full w-full object-cover object-[54%_60%]"
+                alt="Contribution Heatmap"
+              />
+            </div>
+          ) : (
+            <div className="relative h-32 items-center justify-center">
+              <Image
+                height={100}
+                width={100}
+                src={
+                  isDarkMode
+                    ? '/img/heatmap-background-dark.png'
+                    : '/img/heatmap-background-light.png'
+                }
+                className="heatmap-background-loader h-full w-full border-none object-cover object-[54%_60%]"
+                alt="Heatmap Background"
+              />
+              <div className="heatmap-loader"></div>
+            </div>
+          )}
         </div>
       </div>
     )
   }
 
   const UserSummary = () => (
-    <div className="mt-4 flex items-center">
-      <Image
-        width={64}
-        height={64}
-        className="mr-4 h-16 w-16 rounded-full border-2 border-white bg-white object-cover shadow-md dark:border-gray-800 dark:bg-gray-600/60"
-        src={user?.avatarUrl || '/placeholder.svg'}
-        alt={user?.name || user?.login || 'User Avatar'}
-      />
-      <div className="w-full">
-        <Link href={user?.url || '#'} className="text-xl font-bold text-blue-400 hover:underline">
-          @{user?.login}
-        </Link>
-        <p className="text-gray-600 dark:text-gray-400">{formattedBio}</p>
+    <div className="flex flex-col lg:flex-row items-start">
+      <div className="mb-4 flex-shrink-0 self-center lg:mr-6 lg:mb-0 lg:self-start">
+        <Image
+          width={200}
+          height={200}
+          className="h-[200px] w-[200px] rounded-full border-2 border-white bg-white object-cover shadow-md dark:border-gray-800 dark:bg-gray-600/60"
+          src={user?.avatarUrl || '/placeholder.svg'}
+          alt={user?.name || user?.login || 'User Avatar'}
+        />
+      </div>
+      <div className="flex flex-1 flex-col w-full">
+        <div className="mb-0 lg:mb-4 lg:ml-[26px] text-center lg:text-left">
+          <Link href={user?.url || '#'} className="text-xl font-bold text-blue-400 hover:underline">
+            @{user?.login}
+          </Link>
+          <p className="text-gray-600 dark:text-gray-400">{formattedBio}</p>
+        </div>
+
+        {!isPrivateContributor && (
+          <div className="hidden lg:block w-full">
+            <Heatmap />
+          </div>
+        )}
       </div>
     </div>
   )
@@ -215,7 +215,6 @@ const UserDetailsPage: React.FC = () => {
   return (
     <DetailsCard
       details={userDetails}
-      heatmap={isPrivateContributor ? undefined : <Heatmap />}
       pullRequests={pullRequests}
       recentIssues={issues}
       recentMilestones={milestones}
