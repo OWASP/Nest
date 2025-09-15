870ee45486825f97f18f6ab940a1ffed2c089458
Ahmed Gouda <ahmedmosad405@gmail.com>
2025-09-15 06:27:46 +0300

    Update tests

--- backend/tests/apps/common/model_fileds_test.py
+++ backend/tests/apps/common/model_fileds_test.py
@@ -17,13 +17,13 @@ class TestKmsEncryptedField:
     @override_settings(IS_AWS_KMS_ENABLED=False)
     def test_get_prep_value_without_kms(self):
         """Test get_prep_value without KMS."""
-        with pytest.raises(ValueError, match="AWS KMS is not enabled."):
+        with pytest.raises(ValueError, match=r"AWS KMS is not enabled."):
             self.field.get_prep_value("test")
 
     @override_settings(IS_AWS_KMS_ENABLED=False)
     def test_to_python_without_kms(self):
         """Test to_python without KMS."""
-        with pytest.raises(ValueError, match="AWS KMS is not enabled."):
+        with pytest.raises(ValueError, match=r"AWS KMS is not enabled."):
             self.field.to_python(b"encrypted_value")
 
     @override_settings(IS_AWS_KMS_ENABLED=True)
@@ -37,7 +37,7 @@ class TestKmsEncryptedField:
     @pytest.mark.parametrize("value", [5, b"rabbit", [6]])
     def test_get_prep_value_with_non_string(self, value):
         """Test get_prep_value with a non-string."""
-        with pytest.raises(TypeError, match="Value must be a string to encrypt."):
+        with pytest.raises(TypeError, match=r"Value must be a string to encrypt."):
             self.field.get_prep_value(value)
 
     @override_settings(IS_AWS_KMS_ENABLED=True)
@@ -50,7 +50,7 @@ class TestKmsEncryptedField:
     @override_settings(IS_AWS_KMS_ENABLED=False)
     def test_from_db_value_without_kms(self):
         """Test from_db_value without KMS."""
-        with pytest.raises(ValueError, match="AWS KMS is not enabled."):
+        with pytest.raises(ValueError, match=r"AWS KMS is not enabled."):
             self.field.from_db_value(b"encrypted_value", None, None)
 
     @override_settings(IS_AWS_KMS_ENABLED=True)
@@ -63,7 +63,7 @@ class TestKmsEncryptedField:
     @pytest.mark.parametrize("value", [55, [], ()])
     def test_from_db_value_with_invalid_type(self, value):
         """Test from_db_value with an invalid type."""
-        with pytest.raises(TypeError, match="Value must be bytes or bytearray to decrypt."):
+        with pytest.raises(TypeError, match=r"Value must be bytes or bytearray to decrypt."):
             self.field.from_db_value(value, None, None)
 
     @override_settings(IS_AWS_KMS_ENABLED=True)
@@ -76,5 +76,5 @@ class TestKmsEncryptedField:
     @pytest.mark.parametrize("value", [55, [], ()])
     def test_to_python_with_invalid_type(self, value):
         """Test to_python with an invalid type."""
-        with pytest.raises(TypeError, match="Value must be bytes or bytearray to decrypt."):
+        with pytest.raises(TypeError, match=r"Value must be bytes or bytearray to decrypt."):
             self.field.to_python(value)
