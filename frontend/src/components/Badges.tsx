81b21d940887967c67eec5a21e15b586c1124e2b
PIYUSH RATHORE <163632958+Piyushrathoree@users.noreply.github.com>
2025-09-16 23:32:23 +0530

    Update frontend/src/components/Badges.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/src/components/Badges.tsx
+++ frontend/src/components/Badges.tsx
@@ -5,11 +5,12 @@ import {
   library,
 } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Tooltip } from '@heroui/tooltip'
 
-library.add(fas)
-
+library.add(fas, far, fab)
 type BadgeProps = {
   name: string
   cssClass: string
