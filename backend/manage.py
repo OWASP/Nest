dcbaeac5d2f44844147fd9595b17713cb5c26c45
Rajgupta36 <raj.gupta369@outlook.com>
2025-09-30 15:54:36 +0530

    fix the migration

--- backend/manage.py
+++ backend/manage.py
@@ -5,10 +5,13 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
-
+    load_dotenv()
     from configurations.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
