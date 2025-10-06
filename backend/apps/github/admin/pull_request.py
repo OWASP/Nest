2c666fe7084bea42ca2282658ebd453e581a8ed9
Rajgupta36 <raj.gupta369@outlook.com>
2025-10-06 17:31:36 +0530

    added pr and issue linking command

--- backend/apps/github/admin/pull_request.py
+++ backend/apps/github/admin/pull_request.py
@@ -14,6 +14,7 @@ class PullRequestAdmin(admin.ModelAdmin):
         "author",
         "labels",
         "repository",
+        "related_issues",
     )
     list_display = (
         "repository",
