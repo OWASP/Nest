7e44059c2d0ab2d61c21f95b5265dd593e160f91
Rajgupta36 <raj.gupta369@outlook.com>
2025-10-09 03:25:51 +0530

    feat: merged contributor-interested branch and resolved conflicts

---
+++ backend/apps/mentorship/models/managers/module.py
@@ -0,0 +1,13 @@
+"""Mentorship app module manager."""
+
+from django.db import models
+
+from apps.mentorship.models.program import Program
+
+
+class PublishedModuleManager(models.Manager):
+    """Published Modules."""
+
+    def get_queryset(self):
+        """Get queryset."""
+        return super().get_queryset().filter(program__status=Program.ProgramStatus.PUBLISHED)
