9aad9fdc7fda1a78e1073dcfb750eaf4e3812a06
Raj gupta <raj.gupta369@outlook.com>
2025-11-17 00:18:47 +0530

    Added participants who have expressed interest in a specific issue in issue model . (#1995)
    * add field in modela and interested-user logic
    
    * update code
    
    * added models , commands
    
    * update suggestion
    
    * update sync_comment
    
    * update code
    
    * clean
    
    * update models
    
    * cleanup
    
    * implemented suggestions
    
    * update code
    
    * update migration
    
    * update code
    
    * update models and code
    
    * simplification
    
    * fix comment update logic
    
    * update code
    
    * update code
    
    * update code
    
    ---------
    
    Co-authored-by: Kate Golovanova <kate@kgthreads.com>

---
+++ backend/apps/mentorship/models/issue_user_interest.py
@@ -0,0 +1,31 @@
+"""Participant interest model."""
+
+from django.db import models
+
+
+class IssueUserInterest(models.Model):
+    """Represents users interested in a specific issue within a module."""
+
+    class Meta:
+        db_table = "mentorship_issue_user_interests"
+        verbose_name = "Issue User Interest"
+        verbose_name_plural = "Issue User Interests"
+        unique_together = ("module", "issue", "user")
+
+    module = models.ForeignKey(
+        "mentorship.Module", on_delete=models.CASCADE, related_name="interests"
+    )
+    issue = models.ForeignKey(
+        "github.Issue", on_delete=models.CASCADE, related_name="participant_interests"
+    )
+    user = models.ForeignKey(
+        "github.User",
+        related_name="mentorship_interests",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        """Return a human-readable representation of the issue user interest."""
+        return (
+            f"User [{self.user.login}] interested in '{self.issue.title}' for {self.module.name}"
+        )
