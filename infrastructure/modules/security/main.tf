b152c40bd4c5760c53d79123a7f54b6939e3f164
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2025-11-18 17:02:11 +0530

    add a security group for ecs tasks

--- infrastructure/modules/security/main.tf
+++ infrastructure/modules/security/main.tf
@@ -13,6 +13,23 @@ terraform {
   }
 }
 
+resource "aws_security_group" "ecs" {
+  description = "Security group for ECS tasks"
+  name        = "${var.project_name}-${var.environment}-ecs-sg"
+  tags = merge(var.common_tags, {
+    Name = "${var.project_name}-${var.environment}-ecs-sg"
+  })
+  vpc_id = var.vpc_id
+
+  egress {
+    cidr_blocks = var.default_egress_cidr_blocks
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+  }
+}
+
 resource "aws_security_group" "lambda" {
   description = "Security group for Lambda functions (Zappa)"
   name        = "${var.project_name}-${var.environment}-lambda-sg"
@@ -90,6 +107,17 @@ resource "aws_security_group" "redis" {
   }
 }
 
+resource "aws_security_group_rule" "rds_from_ecs" {
+  count                    = var.create_rds_proxy ? 0 : 1
+  description              = "PostgreSQL from ECS"
+  from_port                = var.db_port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds.id
+  source_security_group_id = aws_security_group.ecs.id
+  to_port                  = var.db_port
+  type                     = "ingress"
+}
+
 resource "aws_security_group_rule" "rds_from_lambda" {
   count                    = var.create_rds_proxy ? 0 : 1
   description              = "PostgreSQL from Lambda"
@@ -112,6 +140,17 @@ resource "aws_security_group_rule" "rds_from_proxy" {
   source_security_group_id = aws_security_group.rds_proxy[0].id
 }
 
+resource "aws_security_group_rule" "rds_proxy_from_ecs" {
+  count                    = var.create_rds_proxy ? 1 : 0
+  type                     = "ingress"
+  description              = "PostgreSQL from ECS"
+  from_port                = var.db_port
+  to_port                  = var.db_port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_proxy[0].id
+  source_security_group_id = aws_security_group.ecs.id
+}
+
 resource "aws_security_group_rule" "rds_proxy_from_lambda" {
   count                    = var.create_rds_proxy ? 1 : 0
   type                     = "ingress"
@@ -121,4 +160,14 @@ resource "aws_security_group_rule" "rds_proxy_from_lambda" {
   protocol                 = "tcp"
   security_group_id        = aws_security_group.rds_proxy[0].id
   source_security_group_id = aws_security_group.lambda.id
+}
+
+resource "aws_security_group_rule" "redis_from_ecs" {
+  description              = "Redis from ECS"
+  from_port                = var.redis_port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.redis.id
+  source_security_group_id = aws_security_group.ecs.id
+  to_port                  = var.redis_port
+  type                     = "ingress"
 }
