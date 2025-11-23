3bb3ae5423beb8bfaf3efbcd12a1c829fb9172a1
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2025-11-24 02:09:25 +0530

    Integrate AWS Parameter Store for Zappa/Infrastructure deployments (#2551)
    * Use AWS SSM Parameter Store to handle environment variables
    
    * Use focused policy for read access
    
    * Update documentation
    
    * Add flag for create_rds_proxy
    
    * set default value of create_rds_proxy to false
    
    * Populate Zappa/Lambda environment variables from ssm/parameter store
    
    * Update documentation
    
    * Update example
    
    * add default configurations
    
    * add security group db from lambda

--- infrastructure/modules/security/main.tf
+++ infrastructure/modules/security/main.tf
@@ -30,31 +30,6 @@ resource "aws_security_group" "lambda" {
   }
 }
 
-resource "aws_security_group" "rds_proxy" {
-  description = "Security group for RDS Proxy"
-  name        = "${var.project_name}-${var.environment}-rds-proxy-sg"
-  tags = merge(var.common_tags, {
-    Name = "${var.project_name}-${var.environment}-rds-proxy-sg"
-  })
-  vpc_id = var.vpc_id
-
-  egress {
-    cidr_blocks = var.default_egress_cidr_blocks
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
-  }
-
-  ingress {
-    description     = "PostgreSQL from Lambda"
-    from_port       = var.db_port
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lambda.id]
-    to_port         = var.db_port
-  }
-}
-
 resource "aws_security_group" "rds" {
   description = "Security group for RDS PostgreSQL"
   name        = "${var.project_name}-${var.environment}-rds-sg"
@@ -70,13 +45,23 @@ resource "aws_security_group" "rds" {
     protocol    = "-1"
     to_port     = 0
   }
+}
 
-  ingress {
-    description     = "PostgreSQL from RDS Proxy"
-    from_port       = var.db_port
-    protocol        = "tcp"
-    security_groups = [aws_security_group.rds_proxy.id]
-    to_port         = var.db_port
+resource "aws_security_group" "rds_proxy" {
+  count       = var.create_rds_proxy ? 1 : 0
+  description = "Security group for RDS Proxy"
+  name        = "${var.project_name}-${var.environment}-rds-proxy-sg"
+  tags = merge(var.common_tags, {
+    Name = "${var.project_name}-${var.environment}-rds-proxy-sg"
+  })
+  vpc_id = var.vpc_id
+
+  egress {
+    cidr_blocks = var.default_egress_cidr_blocks
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
   }
 }
 
@@ -103,4 +88,37 @@ resource "aws_security_group" "redis" {
     security_groups = [aws_security_group.lambda.id]
     to_port         = var.redis_port
   }
+}
+
+resource "aws_security_group_rule" "rds_from_lambda" {
+  count                    = var.create_rds_proxy ? 0 : 1
+  description              = "PostgreSQL from Lambda"
+  from_port                = var.db_port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds.id
+  source_security_group_id = aws_security_group.lambda.id
+  to_port                  = var.db_port
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "rds_from_proxy" {
+  count                    = var.create_rds_proxy ? 1 : 0
+  type                     = "ingress"
+  description              = "PostgreSQL from RDS Proxy"
+  from_port                = var.db_port
+  to_port                  = var.db_port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds.id
+  source_security_group_id = aws_security_group.rds_proxy[0].id
+}
+
+resource "aws_security_group_rule" "rds_proxy_from_lambda" {
+  count                    = var.create_rds_proxy ? 1 : 0
+  type                     = "ingress"
+  description              = "PostgreSQL from Lambda"
+  from_port                = var.db_port
+  to_port                  = var.db_port
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_proxy[0].id
+  source_security_group_id = aws_security_group.lambda.id
 }
