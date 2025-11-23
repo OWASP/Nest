c15caffa5d70ce7cffc6bd7ee6f78873516888a9
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2025-11-24 02:09:25 +0530

    Integrate AWS Parameter Store for Zappa/Infrastructure deployments (#2551)
    * Use AWS SSM Parameter Store to handle environment variables
    
    * Use focused policy for read access
    
    * Update documentation
    
    * Add flag for create_rds_proxy
    
    * set default value of create_rds_proxy to false
    
    * Populate Zappa/Lambda environment variables from ssm/parameter store
    
    * Update documentation
    
    * Update example
    
    * add default configurations
    
    * add security group db from lambda

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,25 +1,3 @@
-output "database_endpoint" {
-  description = "The endpoint of the RDS proxy"
-  value       = module.database.db_proxy_endpoint
-}
-
-output "redis_endpoint" {
-  description = "The endpoint of the Redis cache"
-  value       = module.cache.redis_primary_endpoint
-}
-
-output "db_password" {
-  description = "The password for the RDS database"
-  value       = module.database.db_password
-  sensitive   = true
-}
-
-output "redis_auth_token" {
-  description = "The auth token for Redis"
-  value       = module.cache.redis_auth_token
-  sensitive   = true
-}
-
 output "private_subnet_ids" {
   description = "A list of private subnet IDs"
   value       = module.networking.private_subnet_ids
