fe7538a1ed6a31922d90d6173c8d54034c38aed8
Kate <kate@kgthreads.com>
2025-11-28 12:56:10 -0800

    Make icon props read-only

--- frontend/src/components/icons/Sun.tsx
+++ frontend/src/components/icons/Sun.tsx
@@ -1,9 +1,9 @@
 interface SunProps {
-  className?: string
-  variant?: 'regular' | 'solid'
+  readonly className?: string
+  readonly variant?: 'regular' | 'solid'
 }
 
-export default function Sun({ className, variant = 'regular' }: SunProps) {
+export default function Sun({ className, variant = 'regular' }: Readonly<SunProps>) {
   const paths = {
     solid:
       'M 11 0 L 11 3 L 13 3 L 13 0 L 11 0 z M 4.2226562 2.8085938 L 2.8085938 4.2226562 L 4.9296875 6.34375 L 6.34375 4.9296875 L 4.2226562 2.8085938 z M 19.777344 2.8085938 L 17.65625 4.9296875 L 19.070312 6.34375 L 21.191406 4.2226562 L 19.777344 2.8085938 z M 12 5 A 7 7 0 0 0 5 12 A 7 7 0 0 0 12 19 A 7 7 0 0 0 19 12 A 7 7 0 0 0 12 5 z M 0 11 L 0 13 L 3 13 L 3 11 L 0 11 z M 21 11 L 21 13 L 24 13 L 24 11 L 21 11 z M 4.9296875 17.65625 L 2.8085938 19.777344 L 4.2226562 21.191406 L 6.34375 19.070312 L 4.9296875 17.65625 z M 19.070312 17.65625 L 17.65625 19.070312 L 19.777344 21.191406 L 21.191406 19.777344 L 19.070312 17.65625 z M 11 21 L 11 24 L 13 24 L 13 21 L 11 21 z',
