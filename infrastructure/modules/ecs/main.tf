6f23a13ebba918bd68c5bdd3091fff19eeebf0a5
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2025-12-05 20:43:01 +0530

    use stricter versioning

--- infrastructure/modules/ecs/main.tf
+++ infrastructure/modules/ecs/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = "1.14.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "6.22.0"
     }
   }
 }
