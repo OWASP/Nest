1c4fda481614075d1e3873601094e0ef5722deae
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2025-12-06 22:15:46 +0530

    add periods

--- infrastructure/modules/ecs/variables.tf
+++ infrastructure/modules/ecs/variables.tf
@@ -16,69 +16,69 @@ variable "container_parameters_arns" {
 }
 
 variable "ecs_sg_id" {
-  description = "The ID of the security group for the ECS tasks"
+  description = "The ID of the security group for the ECS tasks."
   type        = string
 }
 
 variable "environment" {
-  description = "The environment (e.g., staging, production)"
+  description = "The environment (e.g., staging, production)."
   type        = string
 }
 
 variable "fixtures_read_only_policy_arn" {
-  description = "The ARN of the fixtures read-only IAM policy"
+  description = "The ARN of the fixtures read-only IAM policy."
   type        = string
 }
 
 variable "fixtures_bucket_name" {
-  description = "The name of the S3 bucket for fixtures"
+  description = "The name of the S3 bucket for fixtures."
   type        = string
 }
 
 variable "image_tag" {
-  description = "The Docker image tag to use for ECS tasks"
+  description = "The Docker image tag to use for ECS tasks."
   type        = string
   default     = "latest"
 }
 
 variable "index_data_task_cpu" {
-  description = "The CPU for the index-data task"
+  description = "The CPU for the index-data task."
   type        = string
   default     = "256"
 }
 
 variable "index_data_task_memory" {
-  description = "The memory for the index-data task"
+  description = "The memory for the index-data task."
   type        = string
   default     = "2048"
 }
 
 variable "load_data_task_cpu" {
-  description = "The CPU for the load-data task"
+  description = "The CPU for the load-data task."
   type        = string
   default     = "512"
 }
 
 variable "load_data_task_memory" {
-  description = "The memory for the load-data task"
+  description = "The memory for the load-data task."
   type        = string
   default     = "4096"
 }
 
 variable "migrate_task_cpu" {
-  description = "The CPU for the migrate task"
+  description = "The CPU for the migrate task."
   type        = string
   default     = "256"
 }
 
 variable "migrate_task_memory" {
-  description = "The memory for the migrate task"
+  description = "The memory for the migrate task."
   type        = string
   default     = "1024"
 }
 
 variable "private_subnet_ids" {
-  description = "A list of private subnet IDs"
+  description = "A list of private subnet IDs."
   type        = list(string)
 }
 
@@ -88,37 +88,37 @@ variable "project_name" {
 }
 
 variable "sync_data_task_cpu" {
-  description = "The CPU for the sync-data task"
+  description = "The CPU for the sync-data task."
   type        = string
   default     = "256"
 }
 
 variable "sync_data_task_memory" {
-  description = "The memory for the sync-data task"
+  description = "The memory for the sync-data task."
   type        = string
   default     = "1024"
 }
 
 variable "update_project_health_metrics_task_cpu" {
-  description = "The CPU for the update-project-health-metrics task"
+  description = "The CPU for the update-project-health-metrics task."
   type        = string
   default     = "256"
 }
 
 variable "update_project_health_metrics_task_memory" {
-  description = "The memory for the update-project-health-metrics task"
+  description = "The memory for the update-project-health-metrics task."
   type        = string
   default     = "1024"
 }
 
 variable "update_project_health_scores_task_cpu" {
-  description = "The CPU for the update-project-health-scores task"
+  description = "The CPU for the update-project-health-scores task."
   type        = string
   default     = "256"
 }
 
 variable "update_project_health_scores_task_memory" {
-  description = "The memory for the update-project-health-scores task"
+  description = "The memory for the update-project-health-scores task."
   type        = string
   default     = "1024"
 }
