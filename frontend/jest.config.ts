953054701c23dc1c29fa5d91a2eab25619e3af24
kart-u <pandeykartik715@gmail.com>
2025-12-09 06:42:22 +0000

    updated config

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -9,7 +9,6 @@ const config: Config = {
     '!src/app/api/**',
     '!src/app/board/**',
     '!src/components/icons/**',
-    '!src/app/my/**',
     '!src/app/settings/**',
     '!src/components/Mentee*.tsx',
     '!src/hooks/**',
