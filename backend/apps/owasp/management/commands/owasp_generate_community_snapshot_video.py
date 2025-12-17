ed1ce4b6c59ed22fc25d9260d9554c757111cc03
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2025-12-17 20:40:35 +0530

    add snapshot video command with initial slide PoC

---
+++ backend/apps/owasp/management/commands/owasp_generate_community_snapshot_video.py
@@ -0,0 +1,185 @@
+"""A command to generate OWASP community snapshot video."""
+
+import logging
+import os
+import tempfile
+from dataclasses import dataclass
+from pathlib import Path
+
+import ffmpeg
+import pypdfium2 as pdfium
+from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
+from weasyprint import HTML
+
+from apps.common.open_ai import OpenAi
+from apps.owasp.models.snapshot import Snapshot
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+# Required to cache fonts
+os.environ["XDG_CACHE_HOME"] = str(Path(tempfile.gettempdir()) / "cache")
+
+
+@dataclass
+class Slide:
+    """Represents a single video slide."""
+
+    audio_output_path: Path
+    context: dict
+    image_output_path: Path
+    video_output_path: Path
+    input: str
+    name: str
+    template_name: str
+
+    def render_and_save(self):
+        """Render an HTML template as an image."""
+        html_content = render_to_string(self.template_name, self.context)
+
+        print(f"Rendering slide image: {self.name}")
+
+        html = HTML(string=html_content)
+
+        pdf = pdfium.PdfDocument(html.write_pdf())
+        page = pdf[0]
+        bitmap = page.render(scale=2)
+        pil_image = bitmap.to_pil()
+
+        self.image_output_path.parent.mkdir(parents=True, exist_ok=True)
+        pil_image.save(self.image_output_path, "PNG")
+
+        page.close()
+        pdf.close()
+        print(f"Saved {self.name} image to path: {self.image_output_path}")
+
+    def generate_transcript(self, open_ai: OpenAi):
+        """Generate a transcript."""
+        print(f"Prompt: {self.input}")
+        prompt = """
+        You are a scriptwriter for a tech presentation.
+        Your task is to generate a script for a presentation slide.
+        The script should be simple and direct.
+        Do not add any extra words, introduction or conclusion.
+        """
+        open_ai.set_prompt(prompt)
+        open_ai.set_input(self.input)
+
+        self.transcript = open_ai.complete()
+        print(f"Generated slide transcript: {self.transcript}")
+
+    def generate_audio(self):
+        """Generate audio for the transcript."""
+        # TODO(rudransh-shrivastava): Implement code
+        self.audio_output_path = Path(tempfile.gettempdir()) / "test-audio.mp3"
+
+    def generate_video(self):
+        """Generate video for the slide."""
+        image_stream = ffmpeg.input(self.image_output_path, loop=1, framerate=60)
+        audio_stream = ffmpeg.input(self.audio_output_path)
+
+        try:
+            ffmpeg.output(
+                image_stream,
+                audio_stream,
+                filename=self.video_output_path,
+                acodec="aac",
+                vcodec="libx264",
+                pix_fmt="yuv420p",
+                shortest=None,
+            ).run(overwrite_output=True, capture_stdout=True, capture_stderr=True)
+
+            print(f"Generated video for {self.name}")
+
+        except ffmpeg.Error as e:
+            print(f"Error generating video for {self.name}: {e} {e.stderr}")
+
+
+class SlideBuilder:
+    def __init__(self, snapshot, output_dir):
+        """Initialize SlideBuilder."""
+        self.snapshot = snapshot
+        self.output_dir = output_dir
+
+    def create_intro_slide(self) -> Slide:
+        """Create an introduction slide."""
+        month_name = self.snapshot.start_at.strftime("%B")
+        return Slide(
+            audio_output_path=self.output_dir / "01_intro.mp3",
+            context={
+                "formatted_start": self.snapshot.start_at.strftime("%b %d"),
+                "formatted_end": self.snapshot.end_at.strftime("%b %d, %Y"),
+                "month_name": month_name,
+            },
+            image_output_path=self.output_dir / "01_intro.png",
+            input=f"Hello everyone! welcome to the OWASP Nest {month_name} Snapshot",
+            name="Intro Slide",
+            template_name="video/slides/intro.html",
+            video_output_path=self.output_dir / "01_intro.mp4",
+        )
+
+
+class Generator:
+    def __init__(self):
+        """Initialize Video Generator."""
+        self.slides: list[Slide] = []
+        self.open_ai = OpenAi()
+
+    def append_slide(self, slide: Slide):
+        """Append a slide to list."""
+        self.slides.append(slide)
+
+    def generate_video(self):
+        """Generate video."""
+        for slide in self.slides:
+            slide.render_and_save()
+            slide.generate_transcript(self.open_ai)
+            slide.generate_audio()
+            slide.generate_video()
+
+
+class Command(BaseCommand):
+    help = "Generate OWASP community snapshot video."
+
+    def add_arguments(self, parser) -> None:
+        """Add command-line arguments to the parser.
+
+        Args:
+            parser (argparse.ArgumentParser): The argument parser instance.
+
+        """
+        parser.add_argument(
+            "--output-dir",
+            default=Path(tempfile.gettempdir()),
+            type=Path,
+            help="Directory to save generated files",
+        )
+        parser.add_argument(
+            "--snapshot-key",
+            required=True,
+            type=str,
+            help="Snapshot key (e.g., '2025-06' for month or '2025' for year)",
+        )
+
+    def handle(self, *args, **options) -> None:
+        """Handle the command execution."""
+        snapshot_key = options["snapshot_key"]
+        output_dir = options["output_dir"]
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        snapshot = Snapshot.objects.filter(
+            key__iexact=snapshot_key, status=Snapshot.Status.COMPLETED
+        ).first()
+
+        if not snapshot:
+            print(f"No completed snapshot found for key: {snapshot_key}")
+            return
+
+        print(f"Generating video for snapshot: {snapshot_key}")
+
+        slide_builder = SlideBuilder(snapshot, output_dir)
+        generator = Generator()
+
+        generator.append_slide(slide_builder.create_intro_slide())
+
+        generator.generate_video()
