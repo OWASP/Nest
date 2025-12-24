fc9072ab173f987f0534f2ea42f7bbbbbfa3b6f9
Kate <kate@kgthreads.com>
2025-12-24 14:48:02 -0800

    Silence Sonar on using markdownit package

--- frontend/src/components/MarkdownWrapper.tsx
+++ frontend/src/components/MarkdownWrapper.tsx
@@ -3,6 +3,7 @@ import markdownit from 'markdown-it'
 import taskLists from 'markdown-it-task-lists'
 
 export default function Markdown({ content, className }: { content: string; className?: string }) {
+  // #NOSONAR - Safe to use markdown-it as we use DOMPurify to sanitize the content
   const md = markdownit({
     html: true,
     linkify: true,
