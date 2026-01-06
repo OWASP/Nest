7f8472fe4ac8807dafc4b9d9e5aa1bb281e5a4cb
Ahmed Gouda <ahmedmosad405@gmail.com>
2026-01-06 09:56:25 +0200

    Update tests

--- backend/tests/apps/api/rest/v0/event_test.py
+++ backend/tests/apps/api/rest/v0/event_test.py
@@ -1,43 +1,48 @@
 from datetime import datetime
 
 import pytest
+from django.utils import timezone
 
 from apps.api.rest.v0.event import EventDetail
+from apps.owasp.models.event import Event as EventModel
+
+current_timezone = timezone.get_current_timezone()
 
 
 @pytest.mark.parametrize(
-    "event_data",
+    "event_object",
     [
-        {
-            "description": "A test event",
-            "end_date": "2025-03-14T00:00:00Z",
-            "key": "test-event",
-            "latitude": 37.7749,
-            "longitude": -122.4194,
-            "name": "Test Event",
-            "start_date": "2025-03-14T00:00:00Z",
-            "url": "https://github.com/owasp/Nest",
-        },
-        {
-            "description": "this is a biggest event",
-            "end_date": "2023-05-18T00:00:00Z",
-            "key": "biggest-event",
-            "latitude": 59.9139,
-            "longitude": 10.7522,
-            "name": "biggest event",
-            "start_date": "2022-05-19T00:00:00Z",
-            "url": "https://github.com/owasp",
-        },
+        EventModel(
+            description="this is a sample event",
+            end_date=datetime(2023, 6, 15, tzinfo=current_timezone).date(),
+            key="sample-event",
+            latitude=59.9139,
+            longitude=10.7522,
+            name="sample event",
+            start_date=datetime(2023, 6, 14, tzinfo=current_timezone).date(),
+            url="https://github.com/owasp/Nest",
+        ),
+        EventModel(
+            description=None,
+            end_date=None,
+            key="event-without-end-date",
+            latitude=None,
+            longitude=None,
+            name="event without end date",
+            start_date=datetime(2023, 7, 1, tzinfo=current_timezone).date(),
+            url=None,
+        ),
     ],
 )
-def test_event_serializer_validation(event_data):
-    event = EventDetail(**event_data)
+def test_event_serializer_validation(event_object: EventModel):
+    event = EventDetail.from_orm(event_object)
 
-    assert event.description == event_data["description"]
-    assert event.end_date == datetime.fromisoformat(event_data["end_date"])
-    assert event.key == event_data["key"]
-    assert event.latitude == event_data["latitude"]
-    assert event.longitude == event_data["longitude"]
-    assert event.name == event_data["name"]
-    assert event.start_date == datetime.fromisoformat(event_data["start_date"])
-    assert event.url == event_data["url"]
+    assert event.description == event_object.description
+    end_date = event_object.end_date.isoformat() if event_object.end_date else None
+    assert event.end_date == end_date
+    assert event.key == event_object.key
+    assert event.latitude == event_object.latitude
+    assert event.longitude == event_object.longitude
+    assert event.name == event_object.name
+    assert event.start_date == event_object.start_date.isoformat()
+    assert event.url == event_object.url
