295c69d90b1ef5d923f275332fcef97c177efbca
mrkeshav-05 <lcb2023038@iiitl.ac.in>
2026-01-23 01:19:57 +0530

    fix issue

--- backend/apps/mentorship/signals/program.py
+++ backend/apps/mentorship/signals/program.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=Program)
-def program_post_save_clear_algolia_cache(_sender, instance, **_kwargs):
+def program_post_save_clear_algolia_cache(sender, instance, **kwargs):  # noqa: ARG001
     """Signal handler to clear Algolia cache for the Program index."""
     logger.info("Signal received for program '%s'. Clearing 'programs' index.", instance.name)
     clear_index_cache("programs")
