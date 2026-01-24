4b408c39e79e5efc1067afbadbd8fc74f9943e84
Harshit Verma <harshit1092004@gmail.com>
2026-01-24 08:01:45 +0530

    fix: show module as a section when there is only 1 module (#3402)
    * fix: show module as a section when there is only 1 module
    
    * Update icon
    
    * fix: update test
    
    ---------
    
    Co-authored-by: Kate <kate@kgthreads.com>

--- frontend/src/components/CardDetailsPage.tsx
+++ frontend/src/components/CardDetailsPage.tsx
@@ -359,12 +359,15 @@ const DetailsCard = ({
             </SecondaryCard>
           )}
         {type === 'program' && modules.length > 0 && (
-          <SecondaryCard
-            icon={FaFolderOpen}
-            title={<AnchorTitle title={modules.length === 1 ? 'Module' : 'Modules'} />}
-          >
-            <ModuleCard modules={modules} accessLevel={accessLevel} admins={admins} />
-          </SecondaryCard>
+          <>
+            {modules.length === 1 ? (
+              <ModuleCard modules={modules} accessLevel={accessLevel} admins={admins} />
+            ) : (
+              <SecondaryCard icon={FaFolderOpen} title={<AnchorTitle title="Modules" />}>
+                <ModuleCard modules={modules} accessLevel={accessLevel} admins={admins} />
+              </SecondaryCard>
+            )}
+          </>
         )}
         {IS_PROJECT_HEALTH_ENABLED && type === 'project' && healthMetricsData.length > 0 && (
           <HealthMetrics data={healthMetricsData} />
