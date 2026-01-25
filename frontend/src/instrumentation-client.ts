9a1ed15e916f6c2f5527b7a02fdd8995e7ed2378
Shofikul Islam <shofikul.islam23102023@gmail.com>
2026-01-25 10:22:07 +0600

    feat/add-posthog-integration

--- frontend/src/instrumentation-client.ts
+++ frontend/src/instrumentation-client.ts
@@ -1,5 +1,9 @@
 import * as Sentry from '@sentry/nextjs'
-import { ENVIRONMENT, RELEASE_VERSION, SENTRY_DSN } from 'utils/env.client'
+import posthog from 'posthog-js'
+import { ENVIRONMENT, RELEASE_VERSION, SENTRY_DSN, POSTHOG_KEY, POSTHOG_HOST } from 'utils/env.client'
+
+const env = ENVIRONMENT?.toLowerCase() || 'local'
+const isTrackingEnabled = env === 'production' || env === 'staging'
 
 Sentry.init({
   debug: false,
@@ -10,5 +14,15 @@ Sentry.init({
   replaysOnErrorSampleRate: 0.5,
   replaysSessionSampleRate: 0.5,
 })
+
+if (isTrackingEnabled && POSTHOG_KEY) {
+  posthog.init(POSTHOG_KEY!, {
+    api_host: POSTHOG_HOST,
+    defaults: '2025-11-30',
+    person_profiles: 'identified_only',
+    capture_pageview: true,
+  });
+}
+
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
