8cff5a16f7e2bede130ab3ae228a874b5fb05c98
ASIF RAZA <asifr.official9@gmail.com>
2026-01-29 19:26:37 +0500

    fixed sonar issue reductand jsx element

--- frontend/src/components/skeletons/Card.tsx
+++ frontend/src/components/skeletons/Card.tsx
@@ -16,7 +16,6 @@ const CardSkeleton: React.FC<CardSkeletonProps> = ({
   const NUM_CONTRIBUTORS = 8
 
   return (
-    <>
       <div data-testid="card-skeleton" className="flex w-full justify-center">
         <div className="border-border bg-card hover:bg-accent/10 mb-6 w-full rounded-lg border-1 p-6 transition-colors duration-300 ease-linear md:max-w-6xl">
           <div className="flex flex-col gap-6">
@@ -92,7 +91,6 @@ const CardSkeleton: React.FC<CardSkeletonProps> = ({
           </div>
         </div>
       </div>
-    </>
   )
 }
 
