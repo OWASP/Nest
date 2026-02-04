edb8e519f042d3ab8e282fea8f30c4811539298b
Kate <kate@kgthreads.com>
2026-02-03 18:32:05 -0800

    Run make check-test

--- frontend/src/components/skeletons/Card.tsx
+++ frontend/src/components/skeletons/Card.tsx
@@ -16,13 +16,12 @@ const CardSkeleton: React.FC<CardSkeletonProps> = ({
   const NUM_CONTRIBUTORS = 8
 
   return (
-     <output
-   aria-live="polite"
-   aria-busy="true"
-   aria-label="Loading"
-   className="flex w-full justify-center"
- >
-
+    <output
+      aria-live="polite"
+      aria-busy="true"
+      aria-label="Loading"
+      className="flex w-full justify-center"
+    >
       <div className="border-border bg-card hover:bg-accent/10 mb-6 w-full rounded-lg border-1 p-6 transition-colors duration-300 ease-linear md:max-w-6xl">
         <div className="flex flex-col gap-6">
           {/* Header Section */}
