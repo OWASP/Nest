7c43ae4a600f3f236e0e783d8542ea59a8ff95e8
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2026-02-19 04:28:13 +0530

    Automate IAM User Role Policies (#3996)
    * separate backend for each environment
    
    * add bootstrap terraform project
    
    * add initial permissions
    
    * Update CI/CD
    
    * update code
    
    * bot comments
    
    * bot suggestions
    
    * run ci/cd
    
    * run ci/cd
    
    * Run ci/cd
    
    * revert CI/CD changes
    
    * improve s3 bucket and dynamodb table names
    
    * update code
    
    * update code
    
    * run ci/cd
    
    * Revert "run ci/cd"
    
    This reverts commit d43fc1cd52428568c3686e19e70c5c31025571d5.

--- infrastructure/backend/main.tf
+++ infrastructure/backend/main.tf
@@ -18,44 +18,7 @@ locals {
     ManagedBy   = "Terraform"
     Project     = var.project_name
   }
-}
-
-data "aws_iam_policy_document" "logs" {
-  statement {
-    actions   = ["s3:PutObject"]
-    effect    = "Allow"
-    resources = ["${aws_s3_bucket.logs.arn}/*"]
-    sid       = "s3-log-delivery"
-
-    principals {
-      type        = "Service"
-      identifiers = ["logging.s3.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "state_https_only" {
-  policy_id = "ForceHTTPS"
-
-  statement {
-    actions = ["s3:*"]
-    sid     = "HTTPSOnly"
-    effect  = "Deny"
-
-    condition {
-      test     = "Bool"
-      values   = ["false"]
-      variable = "aws:SecureTransport"
-    }
-    principals {
-      identifiers = ["*"]
-      type        = "AWS"
-    }
-    resources = [
-      aws_s3_bucket.state.arn,
-      "${aws_s3_bucket.state.arn}/*",
-    ]
-  }
+  state_environments = toset(var.state_environments)
 }
 
 module "kms" {
@@ -70,12 +33,57 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
+data "aws_iam_policy_document" "logs" {
+  for_each = local.state_environments
+
+  statement {
+    actions   = ["s3:PutObject"]
+    effect    = "Allow"
+    resources = ["${aws_s3_bucket.logs[each.key].arn}/*"]
+    sid       = "s3-log-delivery"
+
+    principals {
+      identifiers = ["logging.s3.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "state_https_only" {
+  for_each  = local.state_environments
+  policy_id = "ForceHTTPS"
+
+  statement {
+    actions = ["s3:*"]
+    effect  = "Deny"
+    sid     = "HTTPSOnly"
+
+    resources = [
+      aws_s3_bucket.state[each.key].arn,
+      "${aws_s3_bucket.state[each.key].arn}/*",
+    ]
+
+    condition {
+      test     = "Bool"
+      values   = ["false"]
+      variable = "aws:SecureTransport"
+    }
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+  }
+}
+
 resource "aws_dynamodb_table" "state_lock" {
-  name         = "${var.project_name}-terraform-state-lock"
+  for_each = local.state_environments
+
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+  name         = "${var.project_name}-${each.key}-terraform-state-lock"
   tags = merge(local.common_tags, {
-    Name = "${var.project_name}-terraform-state-lock"
+    Environment = each.key
+    Name        = "${var.project_name}-${each.key}-terraform-state-lock"
   })
 
   attribute {
@@ -95,30 +103,99 @@ resource "aws_dynamodb_table" "state_lock" {
 }
 
 resource "aws_s3_bucket" "logs" { # NOSONAR
-  bucket = "${var.project_name}-terraform-state-logs-${random_id.suffix.hex}"
+  for_each = local.state_environments
+
+  bucket = "${var.project_name}-${each.key}-terraform-state-logs-${random_id.suffix.hex}"
+  tags = merge(local.common_tags, {
+    Environment = each.key
+    Name        = "${var.project_name}-${each.key}-terraform-state-logs"
+  })
 
   lifecycle {
     prevent_destroy = true
   }
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-terraform-state-logs"
-  })
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "logs" {
+  for_each = local.state_environments
+
+  bucket = aws_s3_bucket.logs[each.key].id
+
+  rule {
+    id     = "expire-logs"
+    status = "Enabled"
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = var.abort_incomplete_multipart_upload_days
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = var.noncurrent_version_expiration_days
+    }
+    expiration {
+      days = var.expire_log_days
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "logs" {
+  for_each = local.state_environments
+
+  bucket = aws_s3_bucket.logs[each.key].id
+  policy = data.aws_iam_policy_document.logs[each.key].json
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  for_each = local.state_environments
+
+  block_public_acls       = true
+  block_public_policy     = true
+  bucket                  = aws_s3_bucket.logs[each.key].id
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+#trivy:ignore:AVD-AWS-0132
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+  for_each = local.state_environments
+
+  bucket = aws_s3_bucket.logs[each.key].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "logs" {
+  for_each = local.state_environments
+
+  bucket = aws_s3_bucket.logs[each.key].id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket" "state" { # NOSONAR
-  bucket              = "${var.project_name}-terraform-state-${random_id.suffix.hex}"
+  for_each = local.state_environments
+
+  bucket              = "${var.project_name}-${each.key}-terraform-state-${random_id.suffix.hex}"
   object_lock_enabled = true
+  tags = merge(local.common_tags, {
+    Environment = each.key
+    Name        = "${var.project_name}-${each.key}-terraform-state"
+  })
 
   lifecycle {
     prevent_destroy = true
   }
-  tags = merge(local.common_tags, {
-    Name = "${var.project_name}-terraform-state"
-  })
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "state" {
-  bucket = aws_s3_bucket.state.id
+  for_each = local.state_environments
+
+  bucket = aws_s3_bucket.state[each.key].id
 
   rule {
     id     = "delete-old-versions"
@@ -133,30 +210,18 @@ resource "aws_s3_bucket_lifecycle_configuration" "state" {
   }
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "logs" {
-  bucket = aws_s3_bucket.logs.id
-
-  rule {
-    id     = "expire-logs"
-    status = "Enabled"
-
-    abort_incomplete_multipart_upload {
-      days_after_initiation = var.abort_incomplete_multipart_upload_days
-    }
-    expiration {
-      days = var.expire_log_days
-    }
-  }
-}
-
 resource "aws_s3_bucket_logging" "state" {
-  bucket        = aws_s3_bucket.state.id
-  target_bucket = aws_s3_bucket.logs.id
+  for_each = local.state_environments
+
+  bucket        = aws_s3_bucket.state[each.key].id
+  target_bucket = aws_s3_bucket.logs[each.key].id
   target_prefix = "s3/"
 }
 
 resource "aws_s3_bucket_object_lock_configuration" "state" {
-  bucket = aws_s3_bucket.state.id
+  for_each = local.state_environments
+
+  bucket = aws_s3_bucket.state[each.key].id
 
   rule {
     default_retention {
@@ -166,28 +231,19 @@ resource "aws_s3_bucket_object_lock_configuration" "state" {
   }
 }
 
-resource "aws_s3_bucket_policy" "logs" {
-  bucket = aws_s3_bucket.logs.id
-  policy = data.aws_iam_policy_document.logs.json
-}
-
 resource "aws_s3_bucket_policy" "state" {
-  bucket = aws_s3_bucket.state.id
-  policy = data.aws_iam_policy_document.state_https_only.json
-}
+  for_each = local.state_environments
 
-resource "aws_s3_bucket_public_access_block" "logs" {
-  block_public_acls       = true
-  block_public_policy     = true
-  bucket                  = aws_s3_bucket.logs.id
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  bucket = aws_s3_bucket.state[each.key].id
+  policy = data.aws_iam_policy_document.state_https_only[each.key].json
 }
 
 resource "aws_s3_bucket_public_access_block" "state" {
+  for_each = local.state_environments
+
   block_public_acls       = true
   block_public_policy     = true
-  bucket                  = aws_s3_bucket.state.id
+  bucket                  = aws_s3_bucket.state[each.key].id
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
@@ -201,8 +257,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
   }
 }
 
+#trivy:ignore:AVD-AWS-0132
 resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
-  bucket = aws_s3_bucket.state.id
+  for_each = local.state_environments
+
+  bucket = aws_s3_bucket.state[each.key].id
+
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -211,14 +271,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
 }
 
 resource "aws_s3_bucket_versioning" "state" {
-  bucket = aws_s3_bucket.state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+  for_each = local.state_environments
 
-resource "aws_s3_bucket_versioning" "logs" {
-  bucket = aws_s3_bucket.logs.id
+  bucket = aws_s3_bucket.state[each.key].id
+
   versioning_configuration {
     status = "Enabled"
   }
---
+++ infrastructure/staging/README.md
@@ -0,0 +1,51 @@
+## Inline Permissions
+Use the following inline permissions for the `nest-staging` IAM User
+*Note*: replace ${...} with appropriate values.
+
+```json
+{
+    "Version": "2012-10-17",
+        "Statement": [
+		{
+			"Sid": "S3StateManagement",
+			"Effect": "Allow",
+			"Action": [
+				"s3:GetObject",
+				"s3:PutObject",
+				"s3:ListBucket"
+			],
+			"Resource": [
+				"arn:aws:s3:::nest-staging-terraform-state-*",
+				"arn:aws:s3:::nest-staging-terraform-state-*/*"
+			]
+		},
+		{
+			"Sid": "DynamoDBStateManagement",
+			"Effect": "Allow",
+			"Action": [
+				"dynamodb:GetItem",
+				"dynamodb:PutItem",
+				"dynamodb:DeleteItem"
+			],
+			"Resource": "arn:aws:dynamodb:${AWS_REGION}:${AWS_ACCOUNT_ID}:table/nest-staging-terraform-state-lock"
+		},
+        {
+            "Sid": "KMSStateManagement",
+            "Effect": "Allow",
+            "Action": [
+                "kms:Decrypt"
+            ],
+            "Resource": "${AWS_BACKEND_KMS_KEY_ARN}"
+        },
+        {
+            "Sid": "STSStateManagement",
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole",
+                "sts:TagSession"
+            ],
+            "Resource": "arn:aws:iam::${AWS_ACCOUNT_ID}:role/nest-staging-terraform"
+        }
+    ]
+}
+```
