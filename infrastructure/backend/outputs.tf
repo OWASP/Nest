311254c5055a330e191e234d960d13f34d045297
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2026-02-19 04:28:13 +0530

    Automate IAM User Role Policies (#3996)
    * separate backend for each environment
    
    * add bootstrap terraform project
    
    * add initial permissions
    
    * Update CI/CD
    
    * update code
    
    * bot comments
    
    * bot suggestions
    
    * run ci/cd
    
    * run ci/cd
    
    * Run ci/cd
    
    * revert CI/CD changes
    
    * improve s3 bucket and dynamodb table names
    
    * update code
    
    * update code
    
    * run ci/cd
    
    * Revert "run ci/cd"
    
    This reverts commit d43fc1cd52428568c3686e19e70c5c31025571d5.

--- infrastructure/backend/outputs.tf
+++ infrastructure/backend/outputs.tf
@@ -1,9 +1,9 @@
-output "dynamodb_table_name" {
-  description = "The name of the DynamoDB table for Terraform state locking"
-  value       = aws_dynamodb_table.state_lock.name
+output "dynamodb_table_names" {
+  description = "The names of the per-environment DynamoDB tables for Terraform state locking."
+  value       = { for env, table in aws_dynamodb_table.state_lock : env => table.name }
 }
 
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket for Terraform state"
-  value       = aws_s3_bucket.state.bucket
+output "state_bucket_names" {
+  description = "The names of the per-environment S3 buckets for Terraform state."
+  value       = { for env, bucket in aws_s3_bucket.state : env => bucket.bucket }
 }
