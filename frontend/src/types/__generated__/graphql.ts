4aa31d7003fc41bd54f09c35369f8037b78e93cb
Harsh <harshit1092004@gmail.com>
2026-02-20 20:50:30 +0000

    feat: implement module ordering with drag-and-drop support

--- frontend/src/types/__generated__/graphql.ts
+++ frontend/src/types/__generated__/graphql.ts
@@ -326,6 +326,7 @@ export type ModuleNode = {
   mentees: Array<UserNode>;
   mentors: Array<MentorNode>;
   name: Scalars['String']['output'];
+  order: Scalars['Int']['output'];
   program?: Maybe<ProgramNode>;
   projectId?: Maybe<Scalars['ID']['output']>;
   projectName?: Maybe<Scalars['String']['output']>;
@@ -388,6 +389,7 @@ export type Mutation = {
   deleteModule: Scalars['String']['output'];
   githubAuth: GitHubAuthResult;
   logoutUser: LogoutResult;
+  reorderModules: Array<ModuleNode>;
   revokeApiKey: RevokeApiKeyResult;
   setTaskDeadline: ModuleNode;
   unassignIssueFromUser: ModuleNode;
@@ -436,6 +438,11 @@ export type MutationDeleteModuleArgs = {
 
 export type MutationGithubAuthArgs = {
   accessToken: Scalars['String']['input'];
+};
+
+
+export type MutationReorderModulesArgs = {
+  inputData: ReorderModulesInput;
 };
 
 
@@ -967,6 +974,11 @@ export type ReleaseNode = Node & {
   repositoryName?: Maybe<Scalars['String']['output']>;
   tagName: Scalars['String']['output'];
   url: Scalars['String']['output'];
+};
+
+export type ReorderModulesInput = {
+  moduleKeys: Array<Scalars['String']['input']>;
+  programKey: Scalars['String']['input'];
 };
 
 export type RepositoryContributorNode = {
