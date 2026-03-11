71702c4290d85b8131fbf06f053609ea27f94fc6
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2026-03-11 12:48:59 +0530

    migrate from AES256 to KMS

--- infrastructure/state/main.tf
+++ infrastructure/state/main.tf
@@ -237,7 +237,6 @@ resource "aws_s3_bucket_public_access_block" "state" {
   restrict_public_buckets = true
 }
 
-#trivy:ignore:AVD-AWS-0132
 resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
   for_each = local.state_environments
 
@@ -245,7 +244,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = module.kms[each.key].key_arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
