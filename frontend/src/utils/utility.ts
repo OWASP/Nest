598ca57e1bb35645dc1e9aa469a7fbef5a879bcb
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2026-03-14 13:07:48 -0700

    Update code

--- frontend/src/utils/utility.ts
+++ frontend/src/utils/utility.ts
@@ -23,12 +23,10 @@ type ProjectType = Project | Issue | Committee | Chapter
 export const getFilteredIcons = (project: ProjectType, params: string[]): Icon => {
   const filteredIcons = params.reduce((acc: Icon, key) => {
     if (ICONS[key as IconKeys] && project[key as keyof typeof project] !== undefined) {
-      if (key === 'createdAt') {
-        const timestamp = project[key as keyof ProjectType]
-        acc[key] = dayjs(timestamp).fromNow()
-      } else {
-        acc[key] = project[key as keyof typeof project] as unknown as number
-      }
+      acc[key] =
+        key === 'createdAt'
+          ? dayjs(project[key as keyof ProjectType]).fromNow()
+          : (project[key as keyof typeof project] as unknown as number)
     }
     return acc
   }, {})
