d607f1f329ea4b45dd216770258d1c18eff897ce
David Wagih <david.wageh@moneyfellows.com>
2026-03-25 20:31:21 +0200

    feat: add threads flag as per trhe comment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -13,6 +13,7 @@ python manage.py clear_cache
 gunicorn wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers "${GUNICORN_WORKERS:-3}" \
+  --threads "${GUNICORN_THREADS:-2}" \
   --timeout "${GUNICORN_TIMEOUT:-30}" \
   --graceful-timeout "${GUNICORN_GRACEFUL_TIMEOUT:-30}" \
   --max-requests "${GUNICORN_MAX_REQUESTS:-1000}" \
