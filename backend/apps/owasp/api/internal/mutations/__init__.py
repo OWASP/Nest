5c919f9c4c629e42cab17fb45dd74ee906b47a73
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2026-06-01 19:44:37 +0530

    fix init

--- backend/apps/owasp/api/internal/mutations/__init__.py
+++ backend/apps/owasp/api/internal/mutations/__init__.py
@@ -3,9 +3,8 @@
 import strawberry
 
 from .board_candidate_claim import BoardCandidateClaimMutations
-from .board_candidate_claim_evidence import BoardCandidateClaimEvidenceMutations
 
 
 @strawberry.type
-class OwaspMutation(BoardCandidateClaimMutations, BoardCandidateClaimEvidenceMutations):
+class OwaspMutation(BoardCandidateClaimMutations):
     """OWASP mutations."""
