5d75cd69cc4b4e11b54067cdfb3393ef1d6caf73
ayushdixit1-av <ayushdixitvsecpanki@gmail.com>
2026-06-10 11:34:41 +0530

    fix: add XFrameOptionsMiddleware to prevent clickjacking

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -100,6 +100,7 @@ class Base(Configuration):
         "apps.common.middlewares.block_null_characters.BlockNullCharactersMiddleware",
         "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
     ]
 
     ROOT_URLCONF = "settings.urls"
