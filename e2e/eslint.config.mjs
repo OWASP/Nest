bd66c27dd9fd6be1f925119475e2baa6e66075c5
Viena <169875752+devnchill@users.noreply.github.com>
2026-06-21 18:14:15 +0530

    feat: prettier setup for e2e along with integration in makefile and ci cd

--- e2e/eslint.config.mjs
+++ e2e/eslint.config.mjs
@@ -1,26 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import playwright from "eslint-plugin-playwright";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import playwright from 'eslint-plugin-playwright'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
-  { ignores: ["node_modules", "playwright-report", "test-results"] },
+  { ignores: ['node_modules', 'playwright-report', 'test-results'] },
   tseslint.configs.recommended,
   js.configs.recommended,
   {
-    files: ["**/*.{js,ts}"],
+    files: ['**/*.{js,ts}'],
     languageOptions: { globals: { ...globals.node } },
     rules: {
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
     },
   },
   {
-    files: ["**/*.{ts}"],
-    extends: [playwright.configs["flat/recommended"]],
+    files: ['**/*.{ts}'],
+    extends: [playwright.configs['flat/recommended']],
   },
-]);
+  eslintConfigPrettier,
+])
