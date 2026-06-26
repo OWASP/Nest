35bef9f6bcfeb41b00c169338d24811df1c153db
Ahmed Gouda <ahmed.gouda@owasp.org>
2026-06-26 04:22:11 +0300

    Optimize mentorship MentorNode (#5031)

--- backend/apps/mentorship/api/internal/nodes/mentor.py
+++ backend/apps/mentorship/api/internal/nodes/mentor.py
@@ -1,6 +1,7 @@
 """GraphQL node for Mentor model."""
 
 import strawberry
+import strawberry_django
 
 
 @strawberry.type
@@ -9,17 +10,17 @@ class MentorNode:
 
     id: strawberry.ID
 
-    @strawberry.field
+    @strawberry_django.field(select_related=["github_user"])
     def avatar_url(self) -> str:
         """Get the GitHub avatar URL of the mentor."""
         return self.github_user.avatar_url if self.github_user else ""
 
-    @strawberry.field
+    @strawberry_django.field(select_related=["github_user"])
     def name(self) -> str:
         """Get the GitHub name of the mentor."""
         return self.github_user.name if self.github_user else ""
 
-    @strawberry.field
+    @strawberry_django.field(select_related=["github_user"])
     def login(self) -> str:
         """Get the GitHub login of the mentor."""
         return self.github_user.login if self.github_user else ""
