1f867071c16e92dbf6f5a6909ac416c6c9bd904e
Mr-Rahul-Paul <179798584+Mr-Rahul-Paul@users.noreply.github.com>
2026-07-06 12:14:13 +0530

    Remove unnecessary local ngrok origin setting
    Signed-off-by: Mr-Rahul-Paul <179798584+Mr-Rahul-Paul@users.noreply.github.com>

--- backend/src/settings/local.py
+++ backend/src/settings/local.py
@@ -1,27 +1,17 @@
 """OWASP Nest local configuration."""
 
-import os
-
 from configurations import values
 
 from settings.base import Base
-
-LOCAL_NGROK_ORIGIN = os.environ.get("DJANGO_LOCAL_NGROK_ORIGIN", "")
-
 
 class Local(Base):
     """Local configuration."""
 
     APP_NAME = "OWASP Nest Local"
 
-    ALLOWED_ORIGINS = tuple(
-        origin
-        for origin in (
-            LOCAL_NGROK_ORIGIN,
-            "http://127.0.0.1:3000",
-            "http://localhost:3000",
-        )
-        if origin
+    ALLOWED_ORIGINS = (
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
     )
     CORS_ALLOWED_ORIGINS = ALLOWED_ORIGINS
     CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS
