ebaf67232ed5a2a7007ecef326d87f3efa5914e5
Ahmed Gouda <ahmed.gouda@owasp.org>
2026-07-22 04:25:48 +0300

    Optimize EntityMemberNode and add optimizations to UserNode (#5257)
    Signed-off-by: Ahmed Gouda <ahmed.gouda@owasp.org>

--- backend/src/apps/github/api/internal/nodes/user.py
+++ backend/src/apps/github/api/internal/nodes/user.py
@@ -52,12 +52,14 @@ class UserNode:
         """Return user badges."""
         return await info.context.github_dataloaders[USER_BADGES_BY_USER_ID_LOADER].load(root.pk)
 
-    @strawberry_django.field
+    @strawberry_django.field(only=["created_at"])
     def created_at(self, root: User) -> str:
         """Resolve created at."""
         return root.idx_created_at
 
-    @strawberry_django.field(select_related=["owasp_profile"])
+    @strawberry_django.field(
+        select_related=["owasp_profile"], only=["owasp_profile__first_contribution_at"]
+    )
     def first_owasp_contribution_at(self, root: User) -> str | None:
         """Resolve first OWASP contribution date."""
         return (
@@ -66,21 +68,27 @@ class UserNode:
             else None
         )
 
-    @strawberry_django.field(select_related=["owasp_profile"])
+    @strawberry_django.field(
+        select_related=["owasp_profile"], only=["owasp_profile__is_owasp_board_member"]
+    )
     def is_owasp_board_member(self, root: User) -> bool:
         """Resolve if member is currently on OWASP Board of Directors."""
         return (
             root.owasp_profile.is_owasp_board_member if hasattr(root, "owasp_profile") else False
         )
 
-    @strawberry_django.field(select_related=["owasp_profile"])
+    @strawberry_django.field(
+        select_related=["owasp_profile"], only=["owasp_profile__is_former_owasp_staff"]
+    )
     def is_former_owasp_staff(self, root: User) -> bool:
         """Resolve if member is a former OWASP staff member."""
         return (
             root.owasp_profile.is_former_owasp_staff if hasattr(root, "owasp_profile") else False
         )
 
-    @strawberry_django.field(select_related=["owasp_profile"])
+    @strawberry_django.field(
+        select_related=["owasp_profile"], only=["owasp_profile__is_gsoc_mentor"]
+    )
     def is_gsoc_mentor(self, root: User) -> bool:
         """Resolve if member is a Google Summer of Code mentor."""
         return root.owasp_profile.is_gsoc_mentor if hasattr(root, "owasp_profile") else False
@@ -90,7 +98,9 @@ class UserNode:
         """Resolve issues count."""
         return await info.context.github_dataloaders[USER_ISSUES_COUNT_LOADER].load(root.pk)
 
-    @strawberry_django.field(select_related=["owasp_profile"])
+    @strawberry_django.field(
+        select_related=["owasp_profile"], only=["owasp_profile__linkedin_page_id"]
+    )
     def linkedin_page_id(self, root: User) -> str:
         """Resolve LinkedIn page ID."""
         return (
@@ -104,12 +114,12 @@ class UserNode:
         """Resolve releases count."""
         return await info.context.github_dataloaders[USER_RELEASES_COUNT_LOADER].load(root.pk)
 
-    @strawberry_django.field
+    @strawberry_django.field(only=["updated_at"])
     def updated_at(self, root: User) -> str:
         """Resolve updated at."""
         return root.idx_updated_at
 
-    @strawberry_django.field
+    @strawberry_django.field(only=["login"])
     def url(self, root: User) -> str:
         """Resolve URL."""
         return root.url
