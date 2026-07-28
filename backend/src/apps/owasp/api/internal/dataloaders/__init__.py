4ce23ff738f01b16b138c1a169e193d09e06919f
Ahmed Gouda <ahmed.gouda@owasp.org>
2026-07-28 06:29:08 +0300

    Run pre-commit
    Signed-off-by: Ahmed Gouda <ahmed.gouda@owasp.org>

--- backend/src/apps/owasp/api/internal/dataloaders/__init__.py
+++ backend/src/apps/owasp/api/internal/dataloaders/__init__.py
@@ -9,6 +9,7 @@ from apps.owasp.api.internal.dataloaders.member_snapshot import (
 )
 from apps.owasp.api.internal.dataloaders.project import get_project_loaders
 
+
 def get_owasp_dataloaders() -> dict[str, object]:
     """Return a dict of dataloader instances for OWASP API resolvers."""
     loaders: dict[str, object] = {}
